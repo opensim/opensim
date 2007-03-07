61c99318300bb2d56b4ea5253deafb354dd0f3a7
gareth <gareth>
2007-03-07 19:30:03 +0000

    fixed genvers.sh

--- genvers.sh
+++ genvers.sh
@@ -3,7 +3,7 @@
 MAJOR=0
 MINOR=1
 BUILD=`date +%s`
-REVISION=`svnversion | sed s/:// | sed s/M//`
+REVISION=`svnversion . | sed s/:// | sed s/M//`
 REALREVISION=`svnversion`
 cat src/VersionInfo.cs.template | sed s/@@VERSION/"$MAJOR.$MINOR, Build $BUILD, Revision $REALREVISION"/g >src/VersionInfo.cs
 echo -n $MAJOR.$MINOR.*.$REVISION >VERSION
