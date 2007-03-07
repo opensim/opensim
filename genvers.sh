b3844c1f7348b4296b5ab1df258935d7f971e412
gareth <gareth>
2007-03-07 18:48:38 +0000

    Deleted old trunk code

--- genvers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MAJOR=0
-MINOR=1
-BUILD=`date +%s`
-REVISION=`svnversion | sed s/:// | sed s/M//`
-REALREVISION=`svnversion`
-cat src/VersionInfo.cs.template | sed s/@@VERSION/"$MAJOR.$MINOR, Build $BUILD, Revision $REALREVISION"/g >src/VersionInfo.cs
-echo -n $MAJOR.$MINOR.*.$REVISION >VERSION
