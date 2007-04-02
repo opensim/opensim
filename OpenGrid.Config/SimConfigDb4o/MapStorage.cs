0c65ac405ba69dbe8b70864106281f390d56c702
gareth <gareth>
2007-04-02 01:40:58 +0000

    Copied OpenSim.Config ready to port for grid server

---
+++ OpenGrid.Config/SimConfigDb4o/MapStorage.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSim.Config.SimConfigDb4o
+{
+    public class MapStorage
+    {
+        public float[] Map;
+
+        public MapStorage()
+        {
+
+        }
+    }
+}
