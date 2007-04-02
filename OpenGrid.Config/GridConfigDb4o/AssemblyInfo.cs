660eaaddb73a9f962d494b1499ae03807fbaccc7
gareth <gareth>
2007-04-02 01:43:06 +0000

    Renamed to GridConfig

--- OpenGrid.Config/GridConfigDb4o/AssemblyInfo.cs
+++ OpenGrid.Config/GridConfigDb4o/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // change them to the information which is associated with the assembly
 // you compile.
 
-[assembly: AssemblyTitle("SimConfig")]
+[assembly: AssemblyTitle("GridConfig")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimConfig")]
+[assembly: AssemblyProduct("GridConfig")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
