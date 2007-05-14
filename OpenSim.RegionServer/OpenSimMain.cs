3cdc219ed64b49fa26faa038408dff7cec734c46
Adam Frisby <adam@gwala.net>
2007-05-14 21:15:26 +0000

    Attempting to add region name to console.

--- OpenSim.RegionServer/OpenSimMain.cs
+++ OpenSim.RegionServer/OpenSimMain.cs
@@ -225,6 +225,7 @@ namespace OpenSim
 
             m_console.WriteLine(OpenSim.Framework.Console.LogPriority.NORMAL,"Main.cs:Startup() - We are " + regionData.RegionName + " at " + regionData.RegionLocX.ToString() + "," + regionData.RegionLocY.ToString());
             m_console.WriteLine(OpenSim.Framework.Console.LogPriority.LOW,"Initialising world");
+            m_console.componentname = "Region " + regionData.RegionName;
             LocalWorld = new World(this._packetServer.ClientThreads, regionData, regionData.RegionHandle, regionData.RegionName);
             LocalWorld.InventoryCache = InventoryCache;
             LocalWorld.AssetCache = AssetCache;
