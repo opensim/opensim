06734abf5f0deda12a2bbc1411b542067004f20d
gareth <gareth>
2007-05-19 00:23:39 +0000

    That shutdown function needs to call this.... Added nice little delay to give time for the XML-RPC response to go back to the grid manager when shutting down

--- OpenGrid.Framework.Manager/GridServerManager.cs
+++ OpenGrid.Framework.Manager/GridServerManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Nwc.XmlRpc;
-
+using System.Threading;
 
 namespace OpenGrid.Framework.Manager {
 
@@ -19,17 +19,20 @@ namespace OpenGrid.Framework.Manager {
             		Hashtable requestData = (Hashtable)request.Params[0];
 	    		Hashtable responseData = new Hashtable();
            
-	    		if((string)requestData["authkey"] != recvkey) {
-	    			responseData["error"]="INVALID KEY";
-	    		} else {
-				responseData["msg"]="Shutdown command accepted";
-	    			responseData["authkey"]=sendkey;
-	    			thecallback("shutdown");	
-	    		} 
-
+			responseData["msg"]="Shutdown command accepted";
+			(new Thread(new ThreadStart(ZOMGServerIsNowTerminallyIll))).Start();
 
 	    		response.Value = responseData;
 	    		return response;
+		}
+
+		// Brought to by late-night coding
+		public static void ZOMGServerIsNowTerminallyIll()
+		{
+			Console.WriteLine("ZOMG! THIS SERVER IS TERMINALLY ILL - WE GOT A SHUTDOWN REQUEST FROM A GRID MANAGER!!!!");
+			Console.WriteLine("We have 3 seconds to live...");
+			Thread.Sleep(3000);
+			thecallback("shutdown");
 		}
 	 }
 }
