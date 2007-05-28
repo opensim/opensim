96b1b955606b7cb5b46c4a0d1f6f5cf404dae5ef
Adam Frisby <adam@gwala.net>
2007-05-28 21:39:13 +0000

    * Added ILogData interface to Grid.Framework.Data - to be used for saving log information to database (region connections, user connections, etc)

---
+++ OpenGridServices/OpenGrid.Framework.Data/ILogData.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenGrid.Framework.Data
+{
+    public enum LogSeverity : int
+    {
+        CRITICAL = 1,
+        MAJOR = 2,
+        MEDIUM = 3,
+        LOW = 4,
+        INFO = 5,
+        VERBOSE = 6
+    }
+    public interface ILogData
+    {
+        void saveLog(string serverDaemon, string methodCall, string arguments, int priority,string logMessage);
+    }
+}
