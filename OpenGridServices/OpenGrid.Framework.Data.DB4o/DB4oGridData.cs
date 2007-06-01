17e422bc673940ffe33b80c29378af65e66501ec
Adam Frisby <adam@gwala.net>
2007-06-01 23:48:52 +0000

    * DB4o Storage provider is now well documented * Added try/catch over user creation (should allow multiple logins on Db4o)

--- OpenGridServices/OpenGrid.Framework.Data.DB4o/DB4oGridData.cs
+++ OpenGridServices/OpenGrid.Framework.Data.DB4o/DB4oGridData.cs
@@ -1,3 +1,30 @@
+/*
+* Copyright (c) OpenSim project, http://sim.opensecondlife.org/
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the <organization> nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,19 +34,41 @@ using libsecondlife;
 
 namespace OpenGrid.Framework.Data.DB4o
 {
+    /// <summary>
+    /// A grid server storage mechanism employing the DB4o database system
+    /// </summary>
     class DB4oGridData : IGridData
     {
+        /// <summary>
+        /// The database manager object
+        /// </summary>
         DB4oGridManager manager;
 
+        /// <summary>
+        /// Called when the plugin is first loaded (as constructors are not called)
+        /// </summary>
         public void Initialise() {
              manager = new DB4oGridManager("gridserver.yap");
         }
 
+        /// <summary>
+        /// Returns a list of regions within the specified ranges
+        /// </summary>
+        /// <param name="a">minimum X coordinate</param>
+        /// <param name="b">minimum Y coordinate</param>
+        /// <param name="c">maximum X coordinate</param>
+        /// <param name="d">maximum Y coordinate</param>
+        /// <returns>An array of region profiles</returns>
         public SimProfileData[] GetProfilesInRange(uint a, uint b, uint c, uint d)
         {
             return null;
         }
 
+        /// <summary>
+        /// Returns a region located at the specified regionHandle (warning multiple regions may occupy the one spot, first found is returned)
+        /// </summary>
+        /// <param name="handle">The handle to search for</param>
+        /// <returns>A region profile</returns>
         public SimProfileData GetProfileByHandle(ulong handle) {
             lock (manager.simProfiles)
             {
@@ -34,6 +83,11 @@ namespace OpenGrid.Framework.Data.DB4o
             throw new Exception("Unable to find profile with handle (" + handle.ToString() + ")");
         }
 
+        /// <summary>
+        /// Returns a specific region
+        /// </summary>
+        /// <param name="uuid">The region ID code</param>
+        /// <returns>A region profile</returns>
         public SimProfileData GetProfileByLLUUID(LLUUID uuid)
         {
             lock (manager.simProfiles)
@@ -44,6 +98,11 @@ namespace OpenGrid.Framework.Data.DB4o
             throw new Exception("Unable to find profile with UUID (" + uuid.ToStringHyphenated() + ")");
         }
 
+        /// <summary>
+        /// Adds a new specified region to the database
+        /// </summary>
+        /// <param name="profile">The profile to add</param>
+        /// <returns>A dataresponse enum indicating success</returns>
         public DataResponse AddProfile(SimProfileData profile)
         {
             lock (manager.simProfiles)
@@ -59,22 +118,40 @@ namespace OpenGrid.Framework.Data.DB4o
             }
         }
 
+        /// <summary>
+        /// Authenticates a new region using the shared secrets. NOT SECURE.
+        /// </summary>
+        /// <param name="uuid">The UUID the region is authenticating with</param>
+        /// <param name="handle">The location the region is logging into (unused in Db4o)</param>
+        /// <param name="key">The shared secret</param>
+        /// <returns>Authenticated?</returns>
         public bool AuthenticateSim(LLUUID uuid, ulong handle, string key) {
             if (manager.simProfiles[uuid].regionRecvKey == key)
                 return true;
             return false;
         }
 
+        /// <summary>
+        /// Shuts down the database
+        /// </summary>
         public void Close()
         {
             manager = null;
         }
 
+        /// <summary>
+        /// Returns the providers name
+        /// </summary>
+        /// <returns>The name of the storage system</returns>
         public string getName()
         {
             return "DB4o Grid Provider";
         }
 
+        /// <summary>
+        /// Returns the providers version
+        /// </summary>
+        /// <returns>The version of the storage system</returns>
         public string getVersion()
         {
             return "0.1";
--- OpenGridServices/OpenGrid.Framework.Data.DB4o/DB4oManager.cs
+++ OpenGridServices/OpenGrid.Framework.Data.DB4o/DB4oManager.cs
@@ -1,3 +1,29 @@
+/*
+* Copyright (c) OpenSim project, http://sim.opensecondlife.org/
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the <organization> nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+*/
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,17 +33,31 @@ using libsecondlife;
 
 namespace OpenGrid.Framework.Data.DB4o
 {
+    /// <summary>
+    /// A Database manager for Db4o
+    /// </summary>
     class DB4oGridManager
     {
+        /// <summary>
+        /// A list of the current regions connected (in-memory cache)
+        /// </summary>
         public Dictionary<LLUUID, SimProfileData> simProfiles = new Dictionary<LLUUID, SimProfileData>();
+        /// <summary>
+        /// Database File Name
+        /// </summary>
         string dbfl;
 
+        /// <summary>
+        /// Creates a new grid storage manager
+        /// </summary>
+        /// <param name="db4odb">Filename to the database file</param>
         public DB4oGridManager(string db4odb)
         {
             dbfl = db4odb;
             IObjectContainer database;
             database = Db4oFactory.OpenFile(dbfl);
             IObjectSet result = database.Get(typeof(SimProfileData));
+            // Loads the file into the in-memory cache
             foreach(SimProfileData row in result) {
                 simProfiles.Add(row.UUID, row);
             }
@@ -57,16 +97,30 @@ namespace OpenGrid.Framework.Data.DB4o
 
     }
 
+    /// <summary>
+    /// A manager for the DB4o database (user profiles)
+    /// </summary>
     class DB4oUserManager
     {
+        /// <summary>
+        /// A list of the user profiles (in memory cache)
+        /// </summary>
         public Dictionary<LLUUID, UserProfileData> userProfiles = new Dictionary<LLUUID, UserProfileData>();
+        /// <summary>
+        /// Database filename
+        /// </summary>
         string dbfl;
 
+        /// <summary>
+        /// Initialises a new DB manager
+        /// </summary>
+        /// <param name="db4odb">The filename to the database</param>
         public DB4oUserManager(string db4odb)
         {
             dbfl = db4odb;
             IObjectContainer database;
             database = Db4oFactory.OpenFile(dbfl);
+            // Load to cache
             IObjectSet result = database.Get(typeof(UserProfileData));
             foreach (UserProfileData row in result)
             {
