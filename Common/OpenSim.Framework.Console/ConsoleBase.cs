61cc2650887e0346f4da305925d27277318bf8a6
MW <Michaelwri22@yahoo.co.uk>
2007-06-08 17:03:49 +0000

    Updated OpenSim.Framework.Console with changes from trunk (also for now have kept the writeline functions).

--- Common/OpenSim.Framework.Console/ConsoleBase.cs
+++ Common/OpenSim.Framework.Console/ConsoleBase.cs
@@ -1,3 +1,30 @@
+/*
+* Copyright (c) Contributors, http://www.openmetaverse.org/
+* See CONTRIBUTORS.TXT for a full list of copyright holders.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the OpenSim Project nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+*/
 using System;
 using System.IO;
 
@@ -41,7 +68,7 @@ namespace OpenSim.Framework.Console
 
         public void Write(string format, params object[] args)
         {
-            WriteLine(LogPriority.NORMAL,format,args);
+            Notice(format,args);
             return;
         }
 
@@ -50,9 +77,9 @@ namespace OpenSim.Framework.Console
         {
             Log.WriteLine(format, args);
             Log.Flush();
-	    if(!m_silent)
+            if (!m_silent)
             {
-            System.Console.WriteLine(format, args);
+                System.Console.WriteLine(format, args);
             }
             return;
         }
@@ -64,6 +91,49 @@ namespace OpenSim.Framework.Console
             if (!m_silent)
             {
                 System.Console.WriteLine(format, args);
+            }
+            return;
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            WriteNewLine(ConsoleColor.Yellow, format, args);
+            return;
+        }
+
+        public void Notice(string format, params object[] args)
+        {
+            WriteNewLine(ConsoleColor.White, format, args);
+            return;
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            WriteNewLine(ConsoleColor.Red, format, args);
+            return;
+        }
+
+        public void Verbose(string format, params object[] args)
+        {
+            WriteNewLine(ConsoleColor.Gray, format, args);
+            return;
+        }
+
+        public void Status(string format, params object[] args)
+        {
+            WriteNewLine(ConsoleColor.Blue, format, args);
+            return;
+        }
+
+        private void WriteNewLine(System.ConsoleColor color, string format, params object[] args)
+        {
+            Log.WriteLine(format, args);
+            Log.Flush();
+            if (!m_silent)
+            {
+                System.Console.ForegroundColor = color;
+                System.Console.WriteLine(format, args);
+                System.Console.ResetColor();
             }
             return;
         }
@@ -130,7 +200,7 @@ namespace OpenSim.Framework.Console
                 }
                 else
                 {
-                    this.WriteLine(LogPriority.MEDIUM,"Valid options are " + OptionA + " or " + OptionB);
+                    Notice("Valid options are " + OptionA + " or " + OptionB);
                     temp = CmdPrompt(prompt, defaultresponse);
                 }
             }
