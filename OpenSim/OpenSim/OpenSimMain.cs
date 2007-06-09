42448ad129afd91cf738ab9249fa2ee2b05d62e5
mingchen <mingchen>
2007-06-09 21:06:34 +0000

    *Fixed casting mishap in last commit - client now starts up without crashing

--- OpenSim/OpenSim/OpenSimMain.cs
+++ OpenSim/OpenSim/OpenSimMain.cs
@@ -131,7 +131,7 @@ namespace OpenSim
                 loginServer = new LoginServer(regionData[0].IPListenAddr, regionData[0].IPListenPort, regionData[0].RegionLocX, regionData[0].RegionLocY, false);
                 loginServer.Startup();
                 //loginServer.SetSessionHandler(((AuthenticateSessionsLocal)this.AuthenticateSessionsHandler[0]).AddNewSession);
-                loginServer.SetSessionHandler(((RegionServerCommsLocal)this.commsManager).gridServer.AddNewSession);
+                loginServer.SetSessionHandler(((RegionServerCommsManager)this.commsManager).gridServer.AddNewSession);
                 //sandbox mode with loginserver not using accounts
                 httpServer.AddXmlRPCHandler("login_to_simulator", loginServer.XmlRpcLoginMethod);
             }
