ffe0fc9bcca35ca8f7e48bd1170dd2a8efa016cd
Adam Frisby <adam@gwala.net>
2007-06-24 17:43:05 +0000

    * Added extensions to trunk/OGS to support Sugilite regions. Anyone running a grid is highly advised to update before 1.18 hits on monday.

--- OpenGridServices/OpenGridServices.GridServer/GridManager.cs
+++ OpenGridServices/OpenGridServices.GridServer/GridManager.cs
@@ -360,6 +360,13 @@ namespace OpenGridServices.GridServer
                     simProfileBlock["agents"] = 1;
                     simProfileBlock["map-image-id"] = aSim.Value.regionMapTextureID.ToString();
 
+                    // For Sugilite compatibility
+                    simProfileBlock["regionhandle"] = aSim.Value.regionHandle.ToString();
+                    simProfileBlock["sim_ip"] = aSim.Value.serverIP.ToString();
+                    simProfileBlock["sim_port"] = aSim.Value.serverPort.ToString();
+                    simProfileBlock["sim_uri"] = aSim.Value.serverURI.ToString();
+                    simProfileBlock["uuid"] = aSim.Value.UUID.ToStringHyphenated();
+
                     simProfileList.Add(simProfileBlock);
                 }
                 OpenSim.Framework.Console.MainConsole.Instance.Verbose("World map request processed, returned " + simProfileList.Count.ToString() + " region(s) in range via FastMode");
