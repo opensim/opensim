4b42bc5311e5856b9688eb84524b29531af7ebf8
MW <Michaelwri22@yahoo.co.uk>
2007-08-25 20:38:57 +0000

    oops , shouldn't have committed my local testing changes

--- OpenSim/Region/ScriptEngine/DotNetEngine/Compiler/LSL/LSL2CSConverter.cs
+++ OpenSim/Region/ScriptEngine/DotNetEngine/Compiler/LSL/LSL2CSConverter.cs
@@ -240,7 +240,6 @@ namespace OpenSim.Region.ScriptEngine.DotNetEngine.Compiler.LSL
             Return += Script;
             Return += "} }\r\n";
 
-            Console.WriteLine("script after conversion: " + Return);
 
             return Return;
         }
