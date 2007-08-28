f41f6daa5750b6c16ffe8239e6011e80c054d299
Brian McBee <heartwide@gmail.com>
2007-08-28 01:57:38 +0000

    reverting to old case sensitive username behavior. As sdague pointed out,we are moving away from db4o to sql, and this won't work.

--- OpenSim/Framework/Data.DB4o/DB4oUserData.cs
+++ OpenSim/Framework/Data.DB4o/DB4oUserData.cs
@@ -82,7 +82,7 @@ namespace OpenSim.Framework.Data.DB4o
         {
             foreach (UserProfileData profile in manager.userProfiles.Values)
             {
-                if (profile.username.ToUpper() == fname.ToUpper() && profile.surname.ToUpper() == lname.ToUpper())
+                if (profile.username == fname && profile.surname == lname)
                     return profile;
             }
             return null;
