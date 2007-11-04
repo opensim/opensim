ef2cea41ce79eefd2f6848b1bbe59c51d3058e52
MW <Michaelwri22@yahoo.co.uk>
2007-11-04 14:38:12 +0000

    Added forgotten IApplicationPlugin file

---
+++ OpenSim/Region/Application/IApplicationPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: AddinRoot("OpenSim", "0.4")]
+namespace OpenSim
+{
+    [TypeExtensionPoint("/OpenSim/Startup")]
+    public interface IApplicationPlugin
+    {
+        void Initialise(OpenSimMain openSim);
+        void Close();
+    }
+}
+
