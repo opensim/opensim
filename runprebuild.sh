3aac47f76909013d075a9e3dcf4b8333f92bfc69
Sean Dague <sean@dague.net>
2007-11-05 14:31:33 +0000

    add a fixup for linux building

--- runprebuild.sh
+++ runprebuild.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 mono bin/Prebuild.exe /target nant
+# needed until we break up OpenSim.exe
+perl -pi -e 's{OpenSim.dll}{OpenSim.exe}' OpenSim/ApplicationPlugins/LoadRegions/OpenSim.ApplicationPlugins.LoadRegions.dll.build
 mono bin/Prebuild.exe /target vs2005
