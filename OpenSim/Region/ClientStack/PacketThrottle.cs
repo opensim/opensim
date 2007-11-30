174a3d2ca1b666affce5ae80b4b0747b88c8a902
Sean Dague <sean@dague.net>
2007-11-30 21:29:21 +0000

    the packet throttle stuff is duped enough that a seperate container for this probably makes the limits easier to understand

---
+++ OpenSim/Region/ClientStack/PacketThrottle.cs
@@ -0,0 +1,95 @@
+/*
+* Copyright (c) Contributors, http://opensimulator.org/
+* See CONTRIBUTORS.TXT for a full list of copyright holders.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the OpenSim Project nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+*/
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using Axiom.Math;
+using libsecondlife;
+using libsecondlife.Packets;
+using OpenSim.Framework;
+using OpenSim.Framework.Communications.Cache;
+using OpenSim.Framework.Console;
+using Timer=System.Timers.Timer;
+
+namespace OpenSim.Region.ClientStack
+{
+    public class PacketThrottle
+    {
+
+        private int max; // max allowable throttle
+        private int min; // min allowable throttle
+        private int throttle; // current throttle setting
+        private static int divisor = 7; // the throttle time divisor, this probably should factor out
+        private int sent; // current number of bytes sent
+
+        public PacketThrottle(int Min, int Max, int Throttle)
+        {
+            max = Max;
+            min = Min;
+            throttle = Throttle;
+            sent = 0;
+        }
+
+        public void Reset()
+        {
+            sent = 0;
+        }
+        
+        public bool UnderLimit()
+        {
+            return (sent < (throttle / divisor));
+        }
+
+        public int Add(int bytes)
+        {
+            sent += bytes;
+            return sent;
+        }
+
+        // Properties
+        public int Max
+        {
+            get {return max;}
+        }
+
+        public int Min
+        {
+            get {return min;}
+        }
+        
+        public int Throttle
+        {
+            get {return throttle;}
+            set {throttle = value;}
+        }
+    }
+}
