53b5fa49745cd5928efac8e77924c99172114396
Jeff Ames <jeffames@gmail.com>
2007-12-15 02:36:31 +0000

    Set svn:eol-style.  Cleaned up set-eol-style.sh a bit.

--- set-eol-style.sh
+++ set-eol-style.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-find OpenSim -name \*\.cs | xargs perl -pi -e 's/\r//' 
-find OpenSim -name \*\.cs | xargs svn propset svn:eol-style native
+set_eol_style()
+{
+    for file in $*; do
+        svn_status=`svn propget svn:eol-style $file`
+        if [ -z "${svn_status}" -o "${svn_status}" != "native" ]; then
+            svn propset svn:eol-style native $file
+        fi
+    done
+}
 
+for file in `find OpenSim -name \*\.cs`; do
+    set_eol_style $file
+done
