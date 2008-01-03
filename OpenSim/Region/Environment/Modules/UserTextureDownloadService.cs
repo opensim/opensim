1337a3973d7d95aee124c622355d686f95ac9269
lbsa71 <lbsa71@hotmail.com>
2008-01-03 14:08:51 +0000

    * changed form Debug to Verbose on Texture sending

--- OpenSim/Region/Environment/Modules/UserTextureDownloadService.cs
+++ OpenSim/Region/Environment/Modules/UserTextureDownloadService.cs
@@ -83,7 +83,7 @@ namespace OpenSim.Region.Environment.Modules
 
         private void EnqueueTextureSender(TextureSender textureSender)
         {
-            MainLog.Instance.Debug( "TEXTUREDOWNLOAD", "Start: ["+textureSender.RequestedAssetID+"] to ["+textureSender.RequestUser.Name+"]");
+            MainLog.Instance.Verbose( "TEXTUREDOWNLOAD", "Start: ["+textureSender.RequestedAssetID+"] to ["+textureSender.RequestUser.Name+"]");
 
             textureSender.Cancel = false;
             textureSender.Sending = true;
