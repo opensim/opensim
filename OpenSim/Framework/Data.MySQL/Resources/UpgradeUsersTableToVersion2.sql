fd2603bcf4e33308e9cc62c2fec7047c9e1b9abc
Teravus Ovares <teravus@gmail.com>
2008-01-09 04:24:33 +0000

    * ok, last mySQL webloginkey related update tonight.

--- OpenSim/Framework/Data.MySQL/Resources/UpgradeUsersTableToVersion2.sql
+++ OpenSim/Framework/Data.MySQL/Resources/UpgradeUsersTableToVersion2.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `users` 
-	ADD COLUMN `webLoginKey` varchar(36) default '00000000-0000-0000-0000-000000000000',
+	ADD COLUMN `webLoginKey` varchar(36) default '00000000-0000-0000-0000-000000000000' NOT NULL,
 COMMENT='Rev. 2';
