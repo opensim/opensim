8b6d29ff2ed7dde3abef0b1defabed3960e0d959
lbsa71 <lbsa71@hotmail.com>
2008-03-13 19:55:18 +0000

    * SimpleApp is dead, long live OpenSim.Region.Examples.SimpleModule * This module more or less crashes every region in the instance if you enable it by moving it from local /bin to global /bin * But hey, it crashes in lots of interesting ways.

---
+++ OpenSim/Region/Examples/SimpleModule/ComplexObject.cs
@@ -0,0 +1,137 @@
+/*
+* Copyright (c) Contributors, http://opensimulator.org/
+* See CONTRIBUTORS.TXT for a full list of copyright holders.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the OpenSim Project nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+*/
+
+using libsecondlife;
+using OpenSim.Framework;
+using OpenSim.Region.Environment.Scenes;
+
+namespace OpenSim.Region.Examples.SimpleModule
+{
+    public class ComplexObject : SceneObjectGroup
+    {
+        private readonly LLQuaternion m_rotationDirection;
+
+        protected override bool InSceneBackup
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        private class RotatingWheel : SceneObjectPart
+        {
+            private readonly LLQuaternion m_rotationDirection;
+
+            public RotatingWheel()
+            {
+
+            }
+
+            public RotatingWheel(ulong regionHandle, SceneObjectGroup parent, LLUUID ownerID, uint localID,
+                                 LLVector3 groupPosition, LLVector3 offsetPosition, LLQuaternion rotationDirection)
+                : base(
+                    regionHandle, parent, ownerID, localID, PrimitiveBaseShape.Default, groupPosition, offsetPosition
+                    )
+            {
+                m_rotationDirection = rotationDirection;
+
+                Flags |= LLObject.ObjectFlags.Touch;
+            }
+
+            public override void UpdateMovement()
+            {
+                UpdateRotation(RotationOffset*m_rotationDirection);
+            }
+        }
+
+        public override void UpdateMovement()
+        {
+            UpdateGroupRotation(GroupRotation*m_rotationDirection);
+
+            base.UpdateMovement();
+        }
+
+        public ComplexObject()
+        {
+            
+        }
+
+        public ComplexObject(Scene scene, ulong regionHandle, LLUUID ownerID, uint localID, LLVector3 pos)
+            : base(scene, regionHandle, ownerID, localID, pos, PrimitiveBaseShape.Default)
+        {
+            m_rotationDirection = new LLQuaternion(0.05f, 0.1f, 0.15f);
+
+            AddPart(
+                new RotatingWheel(regionHandle, this, ownerID, scene.PrimIDAllocate(), pos, new LLVector3(0, 0, 0.75f),
+                                  new LLQuaternion(0.05f, 0, 0)));
+            AddPart(
+                new RotatingWheel(regionHandle, this, ownerID, scene.PrimIDAllocate(), pos, new LLVector3(0, 0, -0.75f),
+                                  new LLQuaternion(-0.05f, 0, 0)));
+
+            AddPart(
+                new RotatingWheel(regionHandle, this, ownerID, scene.PrimIDAllocate(), pos, new LLVector3(0, 0.75f, 0),
+                                  new LLQuaternion(0.5f, 0, 0.05f)));
+            AddPart(
+                new RotatingWheel(regionHandle, this, ownerID, scene.PrimIDAllocate(), pos, new LLVector3(0, -0.75f, 0),
+                                  new LLQuaternion(-0.5f, 0, -0.05f)));
+
+            AddPart(
+                new RotatingWheel(regionHandle, this, ownerID, scene.PrimIDAllocate(), pos, new LLVector3(0.75f, 0, 0),
+                                  new LLQuaternion(0, 0.5f, 0.05f)));
+            AddPart(
+                new RotatingWheel(regionHandle, this, ownerID, scene.PrimIDAllocate(), pos, new LLVector3(-0.75f, 0, 0),
+                                  new LLQuaternion(0, -0.5f, -0.05f)));
+
+            RootPart.Flags |= LLObject.ObjectFlags.Touch;
+
+            UpdateParentIDs();
+        }
+
+        public override void OnGrabPart(SceneObjectPart part, LLVector3 offsetPos, IClientAPI remoteClient)
+        {
+            m_parts.Remove(part.UUID);
+
+            remoteClient.SendKillObject(m_regionHandle, part.LocalId);
+            remoteClient.AddMoney(1);
+            remoteClient.SendChatMessage("Poof!", 1, AbsolutePosition, "Party Party", LLUUID.Zero);
+        }
+
+        public override void OnGrabGroup(LLVector3 offsetPos, IClientAPI remoteClient)
+        {
+            if (m_parts.Count == 1)
+            {
+                m_parts.Remove(m_rootPart.UUID);
+                m_scene.RemoveEntity(this);
+                remoteClient.SendKillObject(m_regionHandle, m_rootPart.LocalId);
+                remoteClient.AddMoney(50);
+                remoteClient.SendChatMessage("KABLAM!!!", 1, AbsolutePosition, "Groupie Groupie", LLUUID.Zero);
+            }
+        }
+    }
+}
---
+++ OpenSim/Region/Examples/SimpleModule/RegionModule.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using libsecondlife;
+using Nini.Config;
+using OpenSim.Framework;
+using OpenSim.Region.Environment.Interfaces;
+using OpenSim.Region.Environment.Scenes;
+
+namespace OpenSim.Region.Examples.SimpleModule
+{
+    public class RegionModule : IRegionModule
+    {
+        #region IRegionModule Members
+
+        private Scene m_scene;
+
+
+        public void Initialise(Scene scene, IConfigSource source)
+        {
+            m_scene = scene;
+        }
+
+        public void PostInitialise()
+        {
+            RegionInfo regionInfo = m_scene.RegionInfo;
+
+            LLVector3 pos = new LLVector3(110, 129, 27);
+
+            AddCpuCounter(regionInfo, pos);
+            AddComplexObjects(regionInfo, pos);
+            AddAvatars();
+            AddFileSystemObjects();
+        }
+
+        private void AddFileSystemObjects()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(".");
+
+            float x = 0;
+            float z = 0;
+
+            foreach (FileInfo fileInfo in dirInfo.GetFiles())
+            {
+                LLVector3 filePos = new LLVector3(100 + x, 129, 27 + z);
+                x = x + 2;
+                if (x > 50)
+                {
+                    x = 0;
+                    z = z + 2;
+                }
+
+                FileSystemObject fileObject = new FileSystemObject(m_scene, fileInfo, filePos);
+                m_scene.AddEntity(fileObject);
+            }
+        }
+
+        private void AddAvatars()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                MyNpcCharacter m_character = new MyNpcCharacter(m_scene.EventManager);
+                m_scene.AddNewClient(m_character, false);
+            }
+
+            List<ScenePresence> avatars = m_scene.GetAvatars();
+            foreach (ScenePresence avatar in avatars)
+            {
+                avatar.AbsolutePosition =
+                    new LLVector3((float)Util.RandomClass.Next(100, 200), (float)Util.RandomClass.Next(30, 200), 2);
+            }
+        }
+
+        private void AddComplexObjects(RegionInfo regionInfo, LLVector3 pos)
+        {
+            int objs = 3;
+
+            for (int i = 0; i < (objs*objs*objs); i++)
+            {
+                LLVector3 posOffset = new LLVector3((i % objs) * 4, (i % (objs*objs)) / ( objs ) * 4, (i / (objs*objs)) * 4);
+                ComplexObject complexObject =
+                    new ComplexObject(m_scene, regionInfo.RegionHandle, LLUUID.Zero, m_scene.PrimIDAllocate(),
+                                      pos + posOffset);
+                m_scene.AddEntity(complexObject);
+            }
+        }
+
+        private void AddCpuCounter(RegionInfo regionInfo, LLVector3 pos)
+        {
+            SceneObjectGroup sceneObject =
+                new CpuCounterObject(m_scene, regionInfo.RegionHandle, LLUUID.Zero, m_scene.PrimIDAllocate(),
+                                     pos + new LLVector3(1f, 1f, 1f));
+            m_scene.AddEntity(sceneObject);
+        }
+
+        public void Close()
+        {
+            m_scene = null;
+        }
+
+        public string Name
+        {
+            get { return GetType().AssemblyQualifiedName; }
+        }
+
+        public bool IsSharedModule
+        {
+            get { return false; }
+        }
+
+        #endregion
+    }
+}
