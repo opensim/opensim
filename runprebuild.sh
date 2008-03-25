a2daccad5efcd8094a114281331dce7f01727f23
Jeff Ames <jeffames@gmail.com>
2008-03-25 02:10:54 +0000

    Added monodevelop support to runprebuild.sh

--- runprebuild.sh
+++ runprebuild.sh
@@ -3,4 +3,5 @@
 mono bin/Prebuild.exe /target nant
 # needed until we break up OpenSim.exe
 perl -pi -e 's{OpenSim.dll}{OpenSim.exe}' OpenSim/ApplicationPlugins/LoadRegions/OpenSim.ApplicationPlugins.LoadRegions.dll.build
+mono bin/Prebuild.exe /target monodev
 mono bin/Prebuild.exe /target vs2005
