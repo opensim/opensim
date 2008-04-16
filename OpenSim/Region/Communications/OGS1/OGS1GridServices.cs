54eaadeb8276f77c597044f2eb537db05fbbdb84
MW <Michaelwri22@yahoo.co.uk>
2008-04-16 15:49:28 +0000

    added a thread.Sleep(500) to OGS1GridServices.CheckRegion() , to try to help avoid a potential race condition. As IAsyncResult.AsyncWaitHandle.WaitOne before a AsyncCallback is called. So in this case, its a race to see who sets or reads the bool m_bAvailable first.

--- OpenSim/Region/Communications/OGS1/OGS1GridServices.cs
+++ OpenSim/Region/Communications/OGS1/OGS1GridServices.cs
@@ -1460,6 +1460,7 @@ namespace OpenSim.Region.Communications.OGS1
             AsyncCallback ConnectedMethodCallback = new AsyncCallback(ConnectedMethod);
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IAsyncResult ar = socket.BeginConnect(m_EndPoint, ConnectedMethodCallback, socket);
+            System.Threading.Thread.Sleep(500);
             ar.AsyncWaitHandle.WaitOne(timeOut*1000, false);
         }
 
