664d4fc8a081ddd6c92d5d1c507f99951b83e75e
Adam Frisby <adam@gwala.net>
2008-04-17 12:07:54 +0000

    * Missed a file while committing.

---
+++ OpenSim/Region/Environment/Modules/ExportSerialiser/IRegionSerialiser.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace OpenSim.Region.Environment.Modules.ExportSerialiser
+{
+    public interface IRegionSerialiser
+    {
+        System.Collections.Generic.List<string> SerialiseRegion(OpenSim.Region.Environment.Scenes.Scene scene, string saveDir);
+    }
+}
