ede5f5d4cb474a6e9d370637beaae250846f421f
Adam Frisby <adam@gwala.net>
2008-04-23 05:43:18 +0000

    * Tweaked autobuild.sh

--- autobuild.sh
+++ autobuild.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
 mono bin/Prebuild.exe /target nant
-# needed until we break up OpenSim.exe
 perl -pi -e 's{OpenSim.dll}{OpenSim.exe}' OpenSim/ApplicationPlugins/LoadRegions/OpenSim.ApplicationPlugins.LoadRegions.dll.build
 nant
+
