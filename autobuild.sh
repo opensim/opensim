f2213782190e4c462346f07d1cb7085e45a2e6ae
Adam Frisby <adam@gwala.net>
2008-04-23 06:44:22 +0000

    * Removing autobuild.sh * Adding bamboo.build NAnt target for bamboo CI server.

--- autobuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mono bin/Prebuild.exe /target nant
-perl -pi -e 's{OpenSim.dll}{OpenSim.exe}' OpenSim/ApplicationPlugins/LoadRegions/OpenSim.ApplicationPlugins.LoadRegions.dll.build
-nant
-
