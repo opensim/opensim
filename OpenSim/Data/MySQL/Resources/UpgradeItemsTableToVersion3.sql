6e9042c3d34f5e7169eac146b48eec8fe0b406cb
Justin Clarke Casey <jjustincc@googlemail.com>
2008-05-01 12:33:05 +0000

    * Change mysql inventory items table version 3 upgrade sql to provide explicit default values

--- OpenSim/Data/MySQL/Resources/UpgradeItemsTableToVersion3.sql
+++ OpenSim/Data/MySQL/Resources/UpgradeItemsTableToVersion3.sql
@@ -1,8 +1,8 @@
 ALTER TABLE `inventoryitems` 
-	ADD COLUMN `salePrice` int(11) NOT NULL,
-	ADD COLUMN `saleType` tinyint(4) NOT NULL,
-	ADD COLUMN `creationDate` int(11) NOT NULL,
+	ADD COLUMN `salePrice` int(11) NOT NULL default 0,
+	ADD COLUMN `saleType` tinyint(4) NOT NULL default 0,
+	ADD COLUMN `creationDate` int(11) NOT NULL default 3000,
 	ADD COLUMN `groupID` varchar(36) NOT NULL default '00000000-0000-0000-0000-000000000000',
-	ADD COLUMN `groupOwned` tinyint(4) NOT NULL,
-	ADD COLUMN `flags` int(11) unsigned NOT NULL,
+	ADD COLUMN `groupOwned` tinyint(4) NOT NULL default 0,
+	ADD COLUMN `flags` int(11) unsigned NOT NULL default 0,
 COMMENT='Rev. 3';
