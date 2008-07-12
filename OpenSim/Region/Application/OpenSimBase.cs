451c3d1dd7bd7fc64e741882040ae44f8c47518f
Justin Clarke Casey <jjustincc@googlemail.com>
2008-07-12 19:29:49 +0000

    * Hive off ConsolePluginCommand into its own framework class

--- OpenSim/Region/Application/OpenSimBase.cs
+++ OpenSim/Region/Application/OpenSimBase.cs
@@ -38,6 +38,7 @@ using Mono.Addins;
 using Nini.Config;
 using OpenSim.Framework;
 using OpenSim.Framework.Communications.Cache;
+using OpenSim.Framework.Console;
 using OpenSim.Framework.Servers;
 using OpenSim.Framework.Statistics;
 using OpenSim.Region.ClientStack;
