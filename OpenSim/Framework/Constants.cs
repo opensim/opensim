a580d7ee35604892941b8e360567540f455b883a
Sean Dague <sean@dague.net>
2008-07-17 19:58:41 +0000

    add plywood texture as a constant, as we need it in a couple of places and it's super cryptic to have random UUIDs hanging out.

--- OpenSim/Framework/Constants.cs
+++ OpenSim/Framework/Constants.cs
@@ -31,6 +31,7 @@ namespace OpenSim.Framework
     {
         public const uint RegionSize = 256;
         public const byte TerrainPatchSize = 16;
+        public const string DefaultTexture = "89556747-24cb-43ed-920b-47caed15465f";
 
         public enum EstateAccessCodex : uint
         {
