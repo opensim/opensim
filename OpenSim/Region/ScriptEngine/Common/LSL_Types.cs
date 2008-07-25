6554c7bac53f60caad21623670ae9e595e38acb3
mingchen <mingchen>
2008-07-25 03:39:38 +0000

    *Fixes #0001818: typecast string to float

--- OpenSim/Region/ScriptEngine/Common/LSL_Types.cs
+++ OpenSim/Region/ScriptEngine/Common/LSL_Types.cs
@@ -1189,6 +1189,10 @@ namespace OpenSim.Region.ScriptEngine.Common
             {
                 return new Vector3(s.m_string);
             }
+            public static implicit operator LSLFloat(LSLString s)
+            {
+                return new LSLFloat(Convert.ToDouble(s.m_string));
+            }
 
             #endregion
 
