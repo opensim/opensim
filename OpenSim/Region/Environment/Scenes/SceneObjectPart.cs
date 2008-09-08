41a68ca5db07d6730f3f04fd0401b01c388a235e
Melanie Thielker <melanie@t-data.com>
2008-09-08 19:57:28 +0000

    Fix component order on a quaternion for the sit target. This caused sit positions to be upside down until the script was reset.

--- OpenSim/Region/Environment/Scenes/SceneObjectPart.cs
+++ OpenSim/Region/Environment/Scenes/SceneObjectPart.cs
@@ -821,7 +821,7 @@ namespace OpenSim.Region.Environment.Scenes
                                         );
             }
 
-            set { m_sitTargetOrientation = new Quaternion(value.W, value.X, value.Y, value.Z); }
+            set { m_sitTargetOrientation = new Quaternion(value.X, value.Y, value.Z, value.W); }
         }
 
         public bool Stopped
