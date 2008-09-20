e4bead4edc50c669fc291a8235e6f6850ade9d8f
Melanie Thielker <melanie@t-data.com>
2008-09-20 22:04:59 +0000

    Remove CAPS notecard updating, as LL isn't supporting it and the viewer is not playing nice

--- OpenSim/Framework/Communications/Capabilities/Caps.cs
+++ OpenSim/Framework/Communications/Capabilities/Caps.cs
@@ -170,7 +170,8 @@ namespace OpenSim.Framework.Communications.Capabilities
                 m_capsHandlers["UpdateScriptAgentInventory"] = m_capsHandlers["UpdateNotecardAgentInventory"];
                 m_capsHandlers["UpdateNotecardTaskInventory"] =
                     new RestStreamHandler("POST", capsBase + m_notecardTaskUpdatePath, ScriptTaskInventory);
-                m_capsHandlers["UpdateScriptTaskInventory"] = m_capsHandlers["UpdateNotecardTaskInventory"];
+                // LL doesn't offer this
+                // m_capsHandlers["UpdateScriptTaskInventory"] = m_capsHandlers["UpdateNotecardTaskInventory"];
 
                 // justincc: I've disabled the CAPS service for now to fix problems with selecting textures, and
                 // subsequent inventory breakage, in the edit object pane (such as mantis 1085).  This requires
