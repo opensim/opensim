232aa783adc6449425c627666c472b98b77081b8
Dahlia Trimble <dahliatrimble@gmail.com>
2008-09-22 02:33:48 +0000

    Disabled use of ODE internal geometry to see if it affects the "waves finger" error

--- OpenSim/Region/Physics/OdePlugin/OdePlugin.cs
+++ OpenSim/Region/Physics/OdePlugin/OdePlugin.cs
@@ -1527,18 +1527,18 @@ namespace OpenSim.Region.Physics.OdePlugin
             }
 
             // if it's a standard box or sphere with no cuts or hollows or twist, return false since ODE can use an internal representation for the prim
-            if ((pbs.ProfileShape == ProfileShape.Square && pbs.PathCurve == (byte)Extrusion.Straight)
-                || (pbs.ProfileShape == ProfileShape.HalfCircle && pbs.PathCurve == (byte)Extrusion.Curve1
-                && pbs.Scale.X == pbs.Scale.Y && pbs.Scale.Y == pbs.Scale.Z))
-            {
-                if (pbs.ProfileBegin == 0 && pbs.ProfileEnd == 0
-                    && pbs.ProfileHollow == 0
-                    && pbs.PathTwist == 0 && pbs.PathTwistBegin == 0
-                    && pbs.PathBegin == 0 && pbs.PathEnd == 0
-                    //&& pbs.PathTaperX == 0 && pbs.PathTaperY == 0
-                    && pbs.PathScaleX == 100 && pbs.PathScaleY == 100)
-                    return false;
-            }
+            //if ((pbs.ProfileShape == ProfileShape.Square && pbs.PathCurve == (byte)Extrusion.Straight)
+            //    || (pbs.ProfileShape == ProfileShape.HalfCircle && pbs.PathCurve == (byte)Extrusion.Curve1
+            //    && pbs.Scale.X == pbs.Scale.Y && pbs.Scale.Y == pbs.Scale.Z))
+            //{
+            //    if (pbs.ProfileBegin == 0 && pbs.ProfileEnd == 0
+            //        && pbs.ProfileHollow == 0
+            //        && pbs.PathTwist == 0 && pbs.PathTwistBegin == 0
+            //        && pbs.PathBegin == 0 && pbs.PathEnd == 0
+            //        //&& pbs.PathTaperX == 0 && pbs.PathTaperY == 0
+            //        && pbs.PathScaleX == 100 && pbs.PathScaleY == 100)
+            //        return false;
+            //}
 
         //    if (pbs.ProfileHollow != 0)
         //        return true;
