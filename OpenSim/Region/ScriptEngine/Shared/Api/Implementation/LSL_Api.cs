4b78aa50e65b7c4bb42fcff9bfd6866d71cfad98
Justin Clarke Casey <jjustincc@googlemail.com>
2008-09-26 15:18:22 +0000

    * Apply http://opensimulator.org/mantis/view.php?id=2274 * Reapply homer's click action implementation, which accidentally got blatted * Thanks tyre

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -1143,7 +1143,9 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
         public void llSetClickAction(int action)
         {
             m_host.AddScriptLPS(1);
-            NotImplemented("llSetClickAction");
+            m_host.ClickAction = (byte)action;
+            if (m_host.ParentGroup != null) m_host.ParentGroup.HasGroupChanged = true;
+            m_host.ScheduleFullUpdate();
             return;
         }
 
