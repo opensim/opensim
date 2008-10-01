e7cd583c1edcea7f600064fac89a94a52a84a553
Dr Scofield <drscofield@xyzzyxyzzy.net>
2008-10-01 20:18:57 +0000

    removing asterisk: it's now living at http://forge.opensimulator.org/gf/project/asteriskvoice/

--- share/python/asterisk/create-region.sql
+++
@@ -1,4 +0,0 @@
-USE %(database)s;
-REPLACE INTO `extensions_table` (context,exten,priority,app,appdata) VALUES ('avatare', '%(regionname)s', 1, 'Answer', '');
-REPLACE INTO `extensions_table` (context,exten,priority,app,appdata) VALUES ('avatare', '%(regionname)s', 2, 'Wait', '1');
-REPLACE INTO `extensions_table` (context,exten,priority,app,appdata) VALUES ('avatare', '%(regionname)s', 3, 'Meetme', '%(regionname)s|Acdi');
--- share/python/asterisk/create-user.sql
+++
@@ -1,5 +0,0 @@
-USE %(database)s;
-REPLACE INTO ast_sipfriends (port,context,disallow,allow,type,secret,host,name) VALUES ('5060','avatare','all','ulaw','friend','%(password)s','dynamic','%(username)s');
-REPLACE INTO `extensions_table` (context,exten,priority,app,appdata) VALUES ('avatare', '%(username)s', 1, 'Answer', '');
-REPLACE INTO `extensions_table` (context,exten,priority,app,appdata) VALUES ('avatare', '%(username)s', 2, 'Wait', '1');
-REPLACE INTO `extensions_table` (context,exten,priority,app,appdata) VALUES ('avatare', '%(username)s', 3, 'Dial', 'SIP/%(username)s,60');
