f1aa849dff3f287686c635510d165941eda8039a
Adam Frisby <adam@gwala.net>
2008-11-08 19:37:08 +0000

    * Missed a file from previous checkin.

---
+++ OpenSim/Framework/Client/IClientChat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+
+namespace OpenSim.Framework.Client
+{
+    public interface IClientChat
+    {
+        event ChatMessage OnChatFromClient;
+
+        void SendChatMessage(string message, byte type, Vector3 fromPos, string fromName, UUID fromAgentID, byte source,
+                     byte audible);
+    }
+}
