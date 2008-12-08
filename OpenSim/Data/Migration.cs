7e65697142b386f853076289ece35786dc2bc51a
idb <idb>
2008-12-08 18:55:19 +0000

    Reinstate setting timeout to never, the default 30 second timeout was causing problems with big migrations

--- OpenSim/Data/Migration.cs
+++ OpenSim/Data/Migration.cs
@@ -137,7 +137,7 @@ namespace OpenSim.Data
                 int newversion = kvp.Key;
                 cmd.CommandText = kvp.Value;
                 // we need to up the command timeout to infinite as we might be doing long migrations.
-                //cmd.CommandTimeout = 0;
+                cmd.CommandTimeout = 0;
                 cmd.ExecuteNonQuery();
 
                 if (version == 0)
