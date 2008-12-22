a7a0b5e1aeeddd87aca1158cc5189e717263b38e
Melanie Thielker <melanie@t-data.com>
2008-12-22 14:41:18 +0000

    Fix the error i got sidetracked from :/

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -3644,7 +3644,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                     {
 
                         partItemID = item.ItemID;
-                        linkNumber = m_host.LinkNum;
+                        int linkNumber = m_host.LinkNum;
                         if(m_host.ParentGroup.Children.Count == 1)
                             linkNumber = 0;
 
