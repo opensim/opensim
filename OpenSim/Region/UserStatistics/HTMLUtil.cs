694e37f529b13e509139cb98f82b61423279339b
Jeff Ames <jeffames@gmail.com>
2009-01-05 04:34:17 +0000

    Update svn properties, minor formatting cleanup.

--- OpenSim/Region/UserStatistics/HTMLUtil.cs
+++ OpenSim/Region/UserStatistics/HTMLUtil.cs
@@ -1,4 +1,31 @@
-﻿using System;
+/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -141,13 +168,13 @@ namespace OpenSim.Region.UserStatistics
             o.Append("<script type=\"text/javascript\">\n");
             o.Append(
                 @"
-				// <![CDATA[
-				document.observe('dom:loaded', function() {
-					/*
-					first arg	: div to update
-					second arg	: interval to poll in seconds
-					third arg	: file to get data
-					*/
+                // <![CDATA[
+                document.observe('dom:loaded', function() {
+                    /*
+                    first arg    : div to update
+                    second arg    : interval to poll in seconds
+                    third arg    : file to get data
+                    */
 ");
             for (int i = 0; i < divID.Length; i++)
             {
@@ -162,16 +189,16 @@ namespace OpenSim.Region.UserStatistics
             }
 
             o.Append(@"
-				});
-				// ]]>
-				</script>");
+                });
+                // ]]>
+                </script>");
 
         }
 
         public static void HtmlHeaders_O ( ref StringBuilder o)
         {
             o.Append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n");
-		    o.Append("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"nl\">");
+            o.Append("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"nl\">");
             o.Append("<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />");
 
 
