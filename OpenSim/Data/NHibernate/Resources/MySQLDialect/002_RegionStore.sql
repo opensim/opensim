c8ea09f3253c8a7f6f03c8894df09d442c366b25
Justin Clarke Casey <jjustincc@googlemail.com>
2009-01-13 17:04:52 +0000

    * Apply http://opensimulator.org/mantis/view.php?id=2992 * Removes now unnecessary revision 2 and 3 files that should have been removed in an earlier patch * Thanks tlaukkan!

--- OpenSim/Data/NHibernate/Resources/MySQLDialect/002_RegionStore.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-CREATE TABLE `Terrain` (
-       `RegionID` char(36) not null,
-       `MapData` longblob,
-        PRIMARY KEY  (`RegionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-COMMIT;
