3b866df07800a412fc174fe43204bffbd1bb2933
Dr Scofield <drscofield@xyzzyxyzzy.net>
2009-01-26 21:32:59 +0000

    + completed python test server

--- OpenSim/Region/Environment/Modules/Avatar/Concierge/testserver/ConciergeServer.py
+++ OpenSim/Region/Environment/Modules/Avatar/Concierge/testserver/ConciergeServer.py
@@ -4,19 +4,54 @@
 import logging
 import BaseHTTPServer
 
+# enable debug level logging
+logging.basicConfig(level = logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+# subclassed HTTPRequestHandler
 class ConciergeHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_HEAD(req):
-        logging.info('[Concierge] %(command)s request: %(host)s:%(port)d --- %(path)s',
+    def logRequest(self):
+        logging.info('[ConciergeHandler] %(command)s request: %(host)s:%(port)d --- %(path)s',
                      dict(command = self.command,
                           host = self.client_address[0],
                           port = self.client_address[1],
                           path = self.path))
-        
-        req.send_response(200)
-        req.send_header('Content-type', 'text/html')
-        req.send_headers()
 
-        logging.info('[Concierge] %(command)s returned 200', dict(command = self.command))
+    def logResponse(self, status):
+        logging.info('[ConciergeHandler] %(command)s returned %(status)d',
+                     dict(command = self.command,
+                          status = status))
+        
+
+    def do_HEAD(self):
+        self.logRequest()
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        self.logResponse(200)
+
+    def do_POST(self):
+        self.logRequest()
+        hdrs = {}
+        for hdr in self.headers.headers:
+            logging.debug('[ConciergeHandler] POST: header:  %s', hdr.rstrip())
+
+        length = int(self.headers.getheader('Content-Length'))
+        content = self.rfile.read(length)
+        self.rfile.close()
+        
+        logging.debug('[ConciergeHandler] POST: content: %s', content)
+            
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        self.logResponse(200)
+
+    def log_request(code, size):
+        pass
 
 if __name__ == '__main__':
 
