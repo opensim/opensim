7b1c42836d58bd0b2f03ad45575520d8ecf8119d
Melanie Thielker <melanie@t-data.com>
2009-02-20 12:15:40 +0000

    Committing interface and stubs for IM interception

--- OpenSim/Region/CoreModules/Avatar/InstantMessage/MessageTransferModule.cs
+++ OpenSim/Region/CoreModules/Avatar/InstantMessage/MessageTransferModule.cs
@@ -647,5 +647,12 @@ namespace OpenSim.Region.CoreModules.Avatar.InstantMessage
             return gim;
         }
 
+        public void RegisterMessageFilter(Byte dialog, bool always, MessageFilterDelegate fn)
+        {
+        }
+
+        public void DeregisterMessageFilter(Byte dialog, MessageFilterDelegate fn)
+        {
+        }
     }
 }
