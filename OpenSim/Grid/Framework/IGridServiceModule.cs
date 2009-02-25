0f62b2bacb00da2c3ab3733059cf613c8bebd0c5
MW <Michaelwri22@yahoo.co.uk>
2009-02-25 18:47:19 +0000

    Added IGridServiceModule to be the base interface for the Service Modules for the Grid, User and Messaging servers.

---
+++ OpenSim/Grid/Framework/IGridServiceModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenSim.Framework.Servers;
+
+namespace OpenSim.Grid.Framework
+{
+    public interface IGridServiceModule
+    {
+        void Close();
+        void Initialise(IUGAIMCore core);
+        void PostInitialise();
+        void RegisterHandlers(BaseHttpServer httpServer);
+    }
+}
