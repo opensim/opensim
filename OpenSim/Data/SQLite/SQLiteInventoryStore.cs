16a6d60a21860d8ae49715f9077c22a3f1bc54bc
Melanie Thielker <melanie@t-data.com>
2009-03-22 18:35:16 +0000

    Make offline gives work in SQLite standalones

--- OpenSim/Data/SQLite/SQLiteInventoryStore.cs
+++ OpenSim/Data/SQLite/SQLiteInventoryStore.cs
@@ -295,7 +295,15 @@ namespace OpenSim.Data.SQLite
 
                     fillItemRow(inventoryRow, item);
                 }
+
                 invItemsDa.Update(ds, "inventoryitems");
+
+                DataTable inventoryFolderTable = ds.Tables["inventoryfolders"];
+
+                inventoryRow = inventoryFolderTable.Rows.Find(Util.ToRawUuidString(item.Folder));
+                inventoryRow["version"] = (int)inventoryRow["version"] + 1;
+
+                invFoldersDa.Update(ds, "inventoryfolders");
             }
         }
 
