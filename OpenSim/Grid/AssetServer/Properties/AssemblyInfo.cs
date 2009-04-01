958d764172fcbaa5d3a33b787b01426caa300a8a
lbsa71 <lbsa71@hotmail.com>
2009-04-01 19:44:46 +0000

    * Upped trunk version number to 0.6.4 as we just tagged 0.6.4-release

--- OpenSim/Grid/AssetServer/Properties/AssemblyInfo.cs
+++ OpenSim/Grid/AssetServer/Properties/AssemblyInfo.cs
@@ -59,5 +59,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly : AssemblyVersion("0.6.3.*")]
+[assembly : AssemblyVersion("0.6.4.*")]
 [assembly : AssemblyFileVersion("1.0.0.0")]
