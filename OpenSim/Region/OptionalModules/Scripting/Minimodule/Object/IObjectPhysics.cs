39c3ccb93a62de270c2f625d396daf18c805bf51
Adam Frisby <adam@gwala.net>
2009-04-04 23:16:19 +0000

    * Implements IObjectPhysics, this collects Physics attributes into one interface, will be referenced on IObject as IObject.Physics.* * Eg; IObject.Physics.Torque = new Vector3(30.0,0.4,0.5); * Maps loosely to internal PhysicsActor class.

---
+++ OpenSim/Region/OptionalModules/Scripting/Minimodule/Object/IObjectPhysics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+
+namespace OpenSim.Region.OptionalModules.Scripting.Minimodule.Object
+{
+    interface IObjectPhysics
+    {
+        bool Enabled { get; set; }
+        bool Phantom { get; set; }
+        bool PhantomCollisions { get; set; }
+
+        double Density { get; set; }
+        double Mass { get; set; }
+
+        double Buoyancy { get; set; }
+
+        Vector3 GeometricCenter { get; }
+        Vector3 CenterOfMass { get; }
+        Vector3 Velocity { get; set; }
+        Vector3 Torque { get; set; }
+
+        Vector3 Acceleration { get; }
+        Quaternion Orientation { get; set; }
+        Vector3 RotationalVelocity { get; set; }
+
+        void AddForce(Vector3 force, bool pushforce);
+        void AddAngularForce(Vector3 force, bool pushforce);
+        void SetMomentum(Vector3 momentum);
+    }
+}
