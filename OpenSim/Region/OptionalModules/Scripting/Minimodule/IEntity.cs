0af0399198f6b12cc8f7728ecf5dbf59ec66d0b3
Adam Frisby <adam@gwala.net>
2009-04-09 11:25:50 +0000

    * Forgot to commit IEntity in last commit. * Added "DrunkenTextAppreciationModule" Demo MRM - behaves very similarly to the sobriety filter in WoW. ;)

---
+++ OpenSim/Region/OptionalModules/Scripting/Minimodule/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+
+namespace OpenSim.Region.OptionalModules.Scripting.Minimodule
+{
+    public interface IEntity
+    {
+        string Name { get; set; }
+        UUID GlobalID { get; }
+        Vector3 WorldPosition { get; set; }
+    }
+}
