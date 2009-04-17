opensim/opensim

commit 8f1553e7e9ebd61ca4db87b333b43a76933a4b18

Adding a script event, changed(CHANGED_ANIMATION) This is sent to all root prims of all attachments of an avatar when the animation state changes. llGetAnimation() can thenbe used to find the new movement animation. This eliminates the need for fast timers in AOs
git-svn-id: svn+ssh://opensimulator.org/var/svn/opensim/trunk@9110 e9a5c24e-b13f-11db-933b-17aa6df88f97

OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -223,6 +223,8 @@ namespace OpenSim.Region.Framework.Scenes
         string m_callbackURI;
         ulong m_rootRegionHandle;
 
+        private IScriptModule[] m_scriptEngines;
+
         #region Properties
 
         /// <summary>
@@ -584,6 +586,8 @@ namespace OpenSim.Region.Framework.Scenes
             IGroupsModule gm = m_scene.RequestModuleInterface<IGroupsModule>();
             if (gm != null)
                 m_grouptitle = gm.GetGroupTitle(m_uuid);
+
+            m_scriptEngines = m_scene.RequestModuleInterfaces<IScriptModule>();
 
             AbsolutePosition = m_controllingClient.StartPos;
             AdjustKnownSeeds();
@@ -1943,7 +1947,24 @@ namespace OpenSim.Region.Framework.Scenes
             
             if (m_animations.TrySetDefaultAnimation(anim, m_controllingClient.NextAnimationSequenceNumber, UUID.Zero))
             {
-                SendAnimPack();
+                if (m_scriptEngines != null)
+                {
+                    lock (m_attachments)
+                    {
+                        foreach (SceneObjectGroup grp in m_attachments)
+                        {
+                            // 16384 is CHANGED_ANIMATION
+                            //
+                            // Send this to all attachment root prims
+                            //
+                            foreach (IScriptModule m in m_scriptEngines)
+                            {
+                                m.PostObjectEvent(grp.RootPart.UUID, "changed", new Object[] {16384});
+                            }
+                            SendAnimPack();
+                        }
+                    }
+                }
             }
         }
 

OpenSim/Region/Framework/Interfaces/IScriptModule.cs
@@ -38,5 +38,6 @@ namespace OpenSim.Region.Framework.Interfaces
         string GetXMLState(UUID itemID);
 
         bool PostScriptEvent(UUID itemID, string name, Object[] args);
+        bool PostObjectEvent(UUID itemID, string name, Object[] args);
     }
 }

OpenSim/Region/ScriptEngine/DotNetEngine/ScriptEngine.cs
@@ -222,6 +222,15 @@ namespace OpenSim.Region.ScriptEngine.DotNetEngine
             return PostScriptEvent(itemID, new EventParams(name, p, new DetectParams[0]));
         }
 
+        public bool PostObjectEvent(UUID itemID, string name, Object[] p)
+        {
+            SceneObjectPart part = m_Scene.GetSceneObjectPart(itemID);
+            if (part == null)
+                return false;
+
+            return PostObjectEvent(part.LocalId, new EventParams(name, p, new DetectParams[0]));
+        }
+
         public DetectParams GetDetectParams(UUID itemID, int number)
         {
             uint localID = m_ScriptManager.GetLocalID(itemID);

OpenSim/Region/ScriptEngine/Shared/Api/Runtime/LSL_Constants.cs
@@ -268,6 +268,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.ScriptBase
         public const int CHANGED_REGION_RESTART = 256;
         public const int CHANGED_REGION = 512;
         public const int CHANGED_TELEPORT = 1024;
+        public const int CHANGED_ANIMATION = 16384;
         public const int TYPE_INVALID = 0;
         public const int TYPE_INTEGER = 1;
         public const int TYPE_FLOAT = 2;

OpenSim/Region/ScriptEngine/XEngine/XEngine.cs
@@ -938,6 +938,15 @@ namespace OpenSim.Region.ScriptEngine.XEngine
             return PostScriptEvent(itemID, new EventParams(name, p, new DetectParams[0]));
         }
 
+        public bool PostObjectEvent(UUID itemID, string name, Object[] p)
+        {
+            SceneObjectPart part = m_Scene.GetSceneObjectPart(itemID);
+            if (part == null)
+                return false;
+
+            return PostObjectEvent(part.LocalId, new EventParams(name, p, new DetectParams[0]));
+        }
+
         public Assembly OnAssemblyResolve(object sender,
                                           ResolveEventArgs args)
         {

commit 722be8f13258b285b322d1ec8609714f7059fb62

Thank you M1sha for diagnosing and patching a lock bug affecting region crossings introduced in r9110. Fixes mantis #3456.

OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -1948,29 +1948,32 @@ namespace OpenSim.Region.Framework.Scenes
         {
             //m_log.DebugFormat("Updating movement animation to {0}", anim);
             
-            if (m_animations.TrySetDefaultAnimation(anim, m_controllingClient.NextAnimationSequenceNumber, UUID.Zero))
+            if (!m_isChildAgent)
             {
-                if (m_scriptEngines != null)
+                if (m_animations.TrySetDefaultAnimation(anim, m_controllingClient.NextAnimationSequenceNumber, UUID.Zero))
                 {
-                    lock (m_attachments)
+                    if (m_scriptEngines != null)
                     {
-                        foreach (SceneObjectGroup grp in m_attachments)
+                        lock (m_attachments)
                         {
-                            // 16384 is CHANGED_ANIMATION
-                            //
-                            // Send this to all attachment root prims
-                            //
-                            foreach (IScriptModule m in m_scriptEngines)
+                            foreach (SceneObjectGroup grp in m_attachments)
                             {
-                                if (m == null) // No script engine loaded
-                                    continue;
+                                // 16384 is CHANGED_ANIMATION
+                                //
+                                // Send this to all attachment root prims
+                                //
+                                foreach (IScriptModule m in m_scriptEngines)
+                                {
+                                    if (m == null) // No script engine loaded
+                                        continue;
 
-                                m.PostObjectEvent(grp.RootPart.UUID, "changed", new Object[] {16384});
+                                    m.PostObjectEvent(grp.RootPart.UUID, "changed", new Object[] { 16384 });
+                                }
                             }
                         }
                     }
+                    SendAnimPack();
                 }
-                SendAnimPack();
             }
         }
 