a582d1b1a201224df97d1e4030ae7d471aa7f120
Melanie Thielker <melanie@t-data.com>
2009-05-09 00:39:01 +0000

    Add the /data and /metadata retrieval modes to the new asset server. Not functional yet.

--- OpenSim/Servers/Asset/AssetServerGetHandler.cs
+++ OpenSim/Servers/Asset/AssetServerGetHandler.cs
@@ -33,6 +33,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Serialization;
+using OpenSim.Servers.Base;
 using OpenSim.Services.Interfaces;
 using OpenSim.Services.AssetService;
 using OpenSim.Framework;
@@ -60,77 +61,60 @@ namespace OpenSim.Servers.AssetServer
             if (p.Length == 0)
                 return result;
 
-            AssetBase asset = m_AssetService.Get(p[0]);
-
-            if (asset != null)
+            if (p.Length > 1 && p[1] == "data")
             {
-                if (p.Length > 1 && p[1] == "data")
+                result = m_AssetService.GetData(p[0]);
+                if (result == null)
+                    result = new byte[0];
+
+                httpResponse.StatusCode = (int)HttpStatusCode.OK;
+                httpResponse.ContentType = "application/octet-stream";
+            }
+            else if(p.Length > 1 && p[1] == "metadata")
+            {
+                AssetMetadata metadata = m_AssetService.GetMetadata(p[0]);
+
+                if (metadata != null)
                 {
+                    XmlSerializer xs =
+                            new XmlSerializer(typeof(AssetMetadata));
+                    result = SerializeResult(xs, metadata);
+
                     httpResponse.StatusCode = (int)HttpStatusCode.OK;
                     httpResponse.ContentType =
-                            SLAssetTypeToContentType(asset.Type);
-
-                    result = asset.Data;
+                            ServerUtils.SLAssetTypeToContentType(metadata.Type);
                 }
-                else
+            }
+            else
+            {
+                AssetBase asset = m_AssetService.Get(p[0]);
+
+                if (asset != null)
                 {
                     XmlSerializer xs = new XmlSerializer(typeof(AssetBase));
-                    MemoryStream ms = new MemoryStream();
-                    XmlTextWriter xw = new XmlTextWriter(ms, Encoding.UTF8);
-                    xw.Formatting = Formatting.Indented;
-                    xs.Serialize(xw, asset);
-                    xw.Flush();
+                    result = SerializeResult(xs, asset);
 
-                    ms.Seek(0, SeekOrigin.Begin);
-                    result = ms.GetBuffer();
-
-                    Array.Resize<byte>(ref result, (int)ms.Length);
+                    httpResponse.StatusCode = (int)HttpStatusCode.OK;
+                    httpResponse.ContentType =
+                            ServerUtils.SLAssetTypeToContentType(asset.Type);
                 }
             }
             return result;
         }
 
-        private string SLAssetTypeToContentType(int assetType)
+        private byte[] SerializeResult(XmlSerializer xs, object data)
         {
-            switch (assetType)
-            {
-                case 0:
-                    return "image/jp2";
-                case 1:
-                    return "application/ogg";
-                case 2:
-                    return "application/x-metaverse-callingcard";
-                case 3:
-                    return "application/x-metaverse-landmark";
-                case 5:
-                    return "application/x-metaverse-clothing";
-                case 6:
-                    return "application/x-metaverse-primitive";
-                case 7:
-                    return "application/x-metaverse-notecard";
-                case 8:
-                    return "application/x-metaverse-folder";
-                case 10:
-                    return "application/x-metaverse-lsl";
-                case 11:
-                    return "application/x-metaverse-lso";
-                case 12:
-                    return "image/tga";
-                case 13:
-                    return "application/x-metaverse-bodypart";
-                case 17:
-                    return "audio/x-wav";
-                case 19:
-                    return "image/jpeg";
-                case 20:
-                    return "application/x-metaverse-animation";
-                case 21:
-                    return "application/x-metaverse-gesture";
-                case 22:
-                    return "application/x-metaverse-simstate";
-                default:
-                    return "application/octet-stream";
-            }
+            MemoryStream ms = new MemoryStream();
+            XmlTextWriter xw = new XmlTextWriter(ms, Encoding.UTF8);
+            xw.Formatting = Formatting.Indented;
+            xs.Serialize(xw, data);
+            xw.Flush();
+
+            ms.Seek(0, SeekOrigin.Begin);
+            byte[] ret = ms.GetBuffer();
+            Array.Resize<byte>(ref ret, (int)ms.Length);
+
+            return ret;
         }
     }
 }
