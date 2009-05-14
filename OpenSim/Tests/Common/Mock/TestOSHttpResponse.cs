c6e2fc947b72e30113e439579e8b9a52fb81e203
lbsa71 <lbsa71@hotmail.com>
2009-05-14 08:12:23 +0000

    * Changed auto-properties to properties with backing field * This fixes mantis #3650

--- OpenSim/Tests/Common/Mock/TestOSHttpResponse.cs
+++ OpenSim/Tests/Common/Mock/TestOSHttpResponse.cs
@@ -7,7 +7,18 @@ namespace OpenSim.Tests.Common.Mock
 {
     public class TestOSHttpResponse : OSHttpResponse
     {
-        public override int StatusCode { get; set; }
-        public override string ContentType { get; set; }
+        private int m_statusCode;
+        public override int StatusCode
+        {
+            get { return m_statusCode; }
+            set { m_statusCode = value; }
+        }
+
+        private string m_contentType;
+        public override string ContentType
+        {
+            get { return m_contentType; }
+            set { m_contentType = value; }
+        }
     }
 }
