95d87244b0c52bc0755af936809945f30a7d78ff
lbsa71 <lbsa71@hotmail.com>
2009-05-14 08:21:14 +0000

    * Moved BaseRequestHandlerTestHelper to OpenSim.Tests.Common.Setup for great justice.

--- OpenSim/Framework/Servers/HttpServer/Tests/BaseRequestHandlerTests.cs
+++ OpenSim/Framework/Servers/HttpServer/Tests/BaseRequestHandlerTests.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
-using OpenSim.Tests.Common;
+using OpenSim.Tests.Common.Setup;
 
 namespace OpenSim.Framework.Servers.HttpServer.Tests
 {
--- OpenSim/Tests/Common/Setup/BaseRequestHandlerTestHelper.cs
+++ OpenSim/Tests/Common/Setup/BaseRequestHandlerTestHelper.cs
@@ -7,7 +7,7 @@ using OpenSim.Framework.Servers;
 using OpenSim.Framework.Servers.HttpServer;
 using OpenSim.Tests.Common.Mock;
 
-namespace OpenSim.Tests.Common
+namespace OpenSim.Tests.Common.Setup
 {
     public class BaseRequestHandlerTestHelper
     {
