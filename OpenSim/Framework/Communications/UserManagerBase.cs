6ddf70b1284a1f5096a49abc77936a5f813e9f56
diva <diva@metaverseink.com>
2009-06-10 20:31:02 +0000

    Bug fix for creating users in standalone: the newly-created root folder was not being set in the cached profile. I suspect this bug has been around for a while.

--- OpenSim/Framework/Communications/UserManagerBase.cs
+++ OpenSim/Framework/Communications/UserManagerBase.cs
@@ -694,6 +694,9 @@ namespace OpenSim.Framework.Communications
                     // local service (standalone)
                     m_log.Debug("[USERSTORAGE]: using IInventoryService to create user's inventory");
                     m_InventoryService.CreateUserInventory(userProf.ID);
+                    InventoryFolderBase rootfolder = m_InventoryService.RequestRootFolder(userProf.ID);
+                    if (rootfolder != null)
+                        userProf.RootInventoryFolderID = rootfolder.ID;
                 }
                 else
                 {
