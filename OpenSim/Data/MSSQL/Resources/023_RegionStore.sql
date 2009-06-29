dc9900d73fff6e8f9caaa81e19966f608692b9b8
Arthur Valadares <arthursv@linux.vnet.ibm.com>
2009-06-29 19:24:43 +0000

    Thanks StrawberryFride, for a patch that fixes MSSQL migration: Minor tweak to a region migration SQL script for the OAR DateTime field - SQL Server syntax slightly different from MySQL (and there is no unsigned in MSSQL, sadly)

--- OpenSim/Data/MSSQL/Resources/023_RegionStore.sql
+++ OpenSim/Data/MSSQL/Resources/023_RegionStore.sql
@@ -1,7 +1,7 @@
 ﻿BEGIN TRANSACTION
 
-ALTER TABLE regionsettings DROP COLUMN loaded_creation_date;
-ALTER TABLE regionsettings DROP COLUMN loaded_creation_time;
-ALTER TABLE regionsettings ADD COLUMN loaded_creation_datetime int unsigned NOT NULL default 0;
+ALTER TABLE regionsettings DROP COLUMN loaded_creation_date
+ALTER TABLE regionsettings DROP COLUMN loaded_creation_time
+ALTER TABLE regionsettings ADD loaded_creation_datetime int NOT NULL default 0
 
 COMMIT
