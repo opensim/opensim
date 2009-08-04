21e62e87c779da839941a1f7e19c519ced7643c8
dahlia <dahliaTrimble at gmail dot com>
2009-08-03 22:59:47 -0700

    starting to refactor path generation out of prim extrusion

--- OpenSim/Region/Physics/Meshing/PrimMesher.cs
+++ OpenSim/Region/Physics/Meshing/PrimMesher.cs
@@ -1116,7 +1116,7 @@ namespace PrimMesher
             if (path == null)
                 return;
             String fileName = name + "_" + title + ".raw";
-            String completePath = Path.Combine(path, fileName);
+            String completePath = System.IO.Path.Combine(path, fileName);
             StreamWriter sw = new StreamWriter(completePath);
 
             for (int i = 0; i < this.faces.Count; i++)
@@ -1129,6 +1129,22 @@ namespace PrimMesher
             }
 
             sw.Close();
+        }
+    }
+
+    public struct PathNode
+    {
+        public float position;
+        public Quat rotation;
+        public float xScale;
+        public float yScale;
+
+        public PathNode(float position, Quat rotation, float xScale, float yScale)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.xScale = xScale;
+            this.yScale = yScale;
         }
     }
 
@@ -2209,7 +2225,7 @@ namespace PrimMesher
             if (path == null)
                 return;
             String fileName = name + "_" + title + ".raw";
-            String completePath = Path.Combine(path, fileName);
+            String completePath = System.IO.Path.Combine(path, fileName);
             StreamWriter sw = new StreamWriter(completePath);
 
             for (int i = 0; i < this.faces.Count; i++)
