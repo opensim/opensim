02f937b0dcc3150774e423478377246f7b4744bf
Jeff Ames <jeffames@gmail.com>
2009-08-26 13:03:18 +0900

    Fix some compile warnings.

--- OpenSim/Framework/Console/RemoteConsole.cs
+++ OpenSim/Framework/Console/RemoteConsole.cs
@@ -50,8 +50,6 @@ namespace OpenSim.Framework.Console
     //
     public class RemoteConsole : CommandConsole
     {
-        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
         private IHttpServer m_Server = null;
         private IConfigSource m_Config = null;
 
