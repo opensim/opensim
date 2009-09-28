7cd03abab54c697fc24dc871f96ddfea78daeb9c
Melanie <melanie@t-data.com>
2009-09-28 18:41:21 +0100

    Bump main version to 0.6.8-Dev

--- OpenSim/Framework/Servers/VersionInfo.cs
+++ OpenSim/Framework/Servers/VersionInfo.cs
@@ -29,7 +29,7 @@ namespace OpenSim
 {
     public class VersionInfo
     {
-        private const string VERSION_NUMBER = "0.6.6";
+        private const string VERSION_NUMBER = "0.6.8";
         private const Flavour VERSION_FLAVOUR = Flavour.Dev;
 
         public enum Flavour
