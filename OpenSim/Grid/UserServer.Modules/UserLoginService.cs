73a61a8a32f18f4a003cbf0f4d6e876badea69ce
Diva Canto <diva@metaverseink.com>
2009-09-27 21:14:31 -0700

    Fixed small bug in having to deal with RegionInfo's ServerURI, which wasn't being set.

--- OpenSim/Grid/UserServer.Modules/UserLoginService.cs
+++ OpenSim/Grid/UserServer.Modules/UserLoginService.cs
@@ -240,6 +240,7 @@ namespace OpenSim.Grid.UserServer.Modules
             rinfo.RegionLocY = (uint)(gregion.RegionLocY / Constants.RegionSize);
             rinfo.RegionName = gregion.RegionName;
             rinfo.ScopeID = gregion.ScopeID;
+            rinfo.ServerURI = "http://" + gregion.ExternalHostName + ":" + gregion.HttpPort.ToString();
 
             return rinfo;
         }
