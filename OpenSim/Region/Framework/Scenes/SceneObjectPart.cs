d43f739cef2c31200ab297d596b073da29972118
Kitto Flora <kittoflora@nomail.com>
2009-09-30 19:05:39 +0200

    And another merge issue

--- OpenSim/Region/Framework/Scenes/SceneObjectPart.cs
+++ OpenSim/Region/Framework/Scenes/SceneObjectPart.cs
@@ -3485,6 +3485,7 @@ if (m_shape != null) {
             else
             {   // Remove VolumeDetect in any case. Note, it's safe to call SetVolumeDetect as often as you like
                 // (mumbles, well, at least if you have infinte CPU powers :-))
+                PhysicsActor pa = this.PhysActor;
                 if (pa != null)
                 {
                     PhysActor.SetVolumeDetect(0);
