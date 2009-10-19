182693628ca1b81c90f3f0296418437eda406bb5
Snowcrash <Snowcrash.short@gmail.com>
2009-10-19 13:03:14 +0200

    Fix for index error in llList2String

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -4660,7 +4660,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 index = src.Length + index;
             }
-            if (index >= src.Length)
+            if (index >= src.Length || index < 0)
             {
                 return 0;
             }
@@ -4685,7 +4685,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 index = src.Length + index;
             }
-            if (index >= src.Length)
+            if (index >= src.Length || index < 0)
             {
                 return 0.0;
             }
@@ -4712,7 +4712,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 index = src.Length + index;
             }
-            if (index >= src.Length)
+            if (index >= src.Length || index < 0)
             {
                 return String.Empty;
             }
@@ -4726,7 +4726,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 index = src.Length + index;
             }
-            if (index >= src.Length)
+            if (index >= src.Length || index < 0)
             {
                 return "";
             }
@@ -4740,7 +4740,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 index = src.Length + index;
             }
-            if (index >= src.Length)
+            if (index >= src.Length || index < 0)
             {
                 return new LSL_Vector(0, 0, 0);
             }
@@ -4761,7 +4761,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 index = src.Length + index;
             }
-            if (index >= src.Length)
+            if (index >= src.Length || index < 0)
             {
                 return new LSL_Rotation(0, 0, 0, 1);
             }
