120c731a3b3e055cd6786deef2bf3a1ca0fceb8c
Teravus Ovares (Dan Olivares) <teravus@gmail.com>
2009-10-30 17:04:10 -0400

    * Moving A test from the OpenSim.Framework.ACL object to the OpenSim.Framework.Tests assembly.   Fixing the test.

---
+++ OpenSim/Framework/Tests/ACLTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace OpenSim.Framework.Tests
+{
+    [TestFixture]
+    public class ACLTest
+    {
+        #region Tests
+
+        /// <summary>
+        /// ACL Test class
+        /// </summary>
+        [Test]
+        public void ACLTest01()
+        {
+            ACL acl = new ACL();
+
+            Role Guests = new Role("Guests");
+            acl.AddRole(Guests);
+
+            Role[] parents = new Role[1];
+            parents[0] = Guests;
+
+            Role JoeGuest = new Role("JoeGuest", parents);
+            acl.AddRole(JoeGuest);
+
+            Resource CanBuild = new Resource("CanBuild");
+            acl.AddResource(CanBuild);
+
+
+            acl.GrantPermission("Guests", "CanBuild");
+
+            acl.HasPermission("JoeGuest", "CanBuild");
+
+        }
+
+        #endregion
+    }
+}
