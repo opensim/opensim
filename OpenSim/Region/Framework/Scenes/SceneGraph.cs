e7d0aa75ba2ef1982ffd74a14b963d4838a89e5e
dahlia <dahliaTrimble@gmailDotCom>
2009-11-13 20:52:03 -0800

    expose SceneGraph.GetGroupByPrim(UUID)

--- OpenSim/Region/Framework/Scenes/SceneGraph.cs
+++ OpenSim/Region/Framework/Scenes/SceneGraph.cs
@@ -928,7 +928,7 @@ namespace OpenSim.Region.Framework.Scenes
         /// </summary>
         /// <param name="fullID"></param>
         /// <returns>null if no scene object group containing that prim is found</returns>
-        private SceneObjectGroup GetGroupByPrim(UUID fullID)
+        public SceneObjectGroup GetGroupByPrim(UUID fullID)
         {
             SceneObjectGroup sog;
             lock (SceneObjectGroupsByFullID)
