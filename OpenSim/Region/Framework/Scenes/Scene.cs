2a6dbd068ef48c7605f44153dcfea6b215749bed
CasperW <tom@streamsense.net>
2009-12-09 03:58:56 +0100

    Last bits and bobs to clean up the Meta7WindlightModule

--- OpenSim/Region/Framework/Scenes/Scene.cs
+++ OpenSim/Region/Framework/Scenes/Scene.cs
@@ -1504,11 +1504,17 @@ namespace OpenSim.Region.Framework.Scenes
         {
             m_storageManager.DataStore.StoreTerrain(Heightmap.GetDoubles(), RegionInfo.RegionID);
         }
-
+        
         public void StoreWindlightProfile(RegionMeta7WindlightData wl)
         {
             m_regInfo.WindlightSettings = wl;
             m_storageManager.DataStore.StoreRegionWindlightSettings(wl);
+            m_eventManager.TriggerOnSaveNewWindlightProfile();
+        }
+
+        public void LoadWindlightProfile()
+        {
+            m_regInfo.WindlightSettings = m_storageManager.DataStore.LoadRegionWindlightSettings(RegionInfo.RegionID);
             m_eventManager.TriggerOnSaveNewWindlightProfile();
         }
 
