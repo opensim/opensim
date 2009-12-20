37b07aa9ec1d4bb9c84eebc5f9d99b4d70d6d05e
Melanie <melanie@t-data.com>
2009-12-20 16:44:31 +0000

    Fix a case where an idle sim can eat 100% of a core

--- OpenSim/Region/Framework/Scenes/Scene.cs
+++ OpenSim/Region/Framework/Scenes/Scene.cs
@@ -1314,7 +1314,7 @@ namespace OpenSim.Region.Framework.Scenes
                 maintc = Util.EnvironmentTickCountSubtract(maintc);
                 maintc = (int)(m_timespan * 1000) - maintc;
 
-                if ((maintc < (m_timespan * 1000)) && maintc > 0)
+                if (maintc > 0)
                     Thread.Sleep(maintc);
 
                 // Tell the watchdog that this thread is still alive
