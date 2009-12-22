c11f259e154eb7e42bbe522681a5561e626622a2
Melanie <melanie@t-data.com>
2009-12-22 15:46:55 +0000

    Remove GetState. It is really unused and was reinstated by the revert

--- OpenSim/Region/ScriptEngine/XEngine/XEngine.cs
+++ OpenSim/Region/ScriptEngine/XEngine/XEngine.cs
@@ -1189,13 +1189,6 @@ namespace OpenSim.Region.ScriptEngine.XEngine
                 return;
             instance.SetState(newState);
         }
-        public string GetState(UUID itemID)
-        {
-            IScriptInstance instance = GetInstance(itemID);
-            if (instance == null)
-                return "default";
-            return instance.State;
-        }
 
         public int GetStartParameter(UUID itemID)
         {
