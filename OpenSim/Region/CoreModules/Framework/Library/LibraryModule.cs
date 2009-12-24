1fa938aab0b748768ff3528e41ba76dd6baa1bf3
Diva Canto <diva@metaverseink.com>
2009-12-23 17:31:30 -0800

    Library Module: allows adding folders/items to the Library from IAR files placed under bin/Library. This works only for standalones.

---
+++ OpenSim/Region/CoreModules/Framework/Library/LibraryModule.cs
@@ -0,0 +1,219 @@
+﻿/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+using OpenSim.Framework;
+using OpenSim.Framework.Communications;
+using OpenSim.Framework.Communications.Cache;
+using OpenSim.Region.CoreModules.Avatar.Inventory.Archiver;
+using OpenSim.Region.Framework;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scenes;
+using OpenSim.Services.Interfaces;
+
+using OpenMetaverse;
+using log4net;
+using Nini.Config;
+
+namespace OpenSim.Region.CoreModules.Framework.Library
+{
+    public class LibraryModule : ISharedRegionModule
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private static bool m_HasRunOnce = false;
+
+        private bool m_Enabled = false;
+        private string m_LibraryName = "OpenSim Library";
+        private Scene m_Scene;
+
+        #region ISharedRegionModule
+
+        public void Initialise(IConfigSource config)
+        {
+            m_Enabled = config.Configs["Modules"].GetBoolean("LibraryModule", m_Enabled);
+            if (m_Enabled)
+            {
+                IConfig libConfig = config.Configs["LibraryModule"];
+                if (libConfig != null)
+                    m_LibraryName = libConfig.GetString("LibraryName", m_LibraryName);
+            }
+        }
+
+        public bool IsSharedModule
+        {
+            get { return true; }
+        }
+
+        public string Name
+        {
+            get { return "Library Module"; }
+        }
+
+        public Type ReplaceableInterface
+        {
+            get { return null; }
+        }
+
+        public void AddRegion(Scene scene)
+        {
+            if (!m_Enabled)
+                return;
+
+            // Store only the first scene
+            if (m_Scene == null)
+            {
+                m_Scene = scene;
+            }
+        }
+
+        public void RemoveRegion(Scene scene)
+        {
+        }
+
+        public void RegionLoaded(Scene scene)
+        {
+            if (!m_Enabled)
+                return;
+
+            // This will never run more than once, even if the region is restarted
+            if (!m_HasRunOnce) 
+            {
+                LoadLibrariesFromArchives();
+                //DumpLibrary();
+                m_HasRunOnce = true;
+            }
+        }
+
+        public void PostInitialise()
+        {
+        }
+
+        public void Close()
+        {
+            m_Scene = null;
+        }
+
+        #endregion ISharedRegionModule
+
+        #region LoadLibraries
+        private string pathToLibraries = "Library";
+
+        protected void LoadLibrariesFromArchives()
+        {
+            InventoryFolderImpl lib = m_Scene.CommsManager.UserProfileCacheService.LibraryRoot;
+            if (lib == null)
+            {
+                m_log.Debug("[LIBRARY MODULE]: No library.");
+                return;
+            }
+
+            lib.Name = m_LibraryName;
+
+            RegionInfo regInfo = new RegionInfo();
+            Scene m_MockScene = new Scene(regInfo);
+            m_MockScene.CommsManager = m_Scene.CommsManager;
+            LocalInventoryService invService = new LocalInventoryService((LibraryRootFolder)lib);
+            m_MockScene.RegisterModuleInterface<IInventoryService>(invService);
+            m_MockScene.RegisterModuleInterface<IAssetService>(m_Scene.AssetService);
+
+            UserProfileData profile = new UserProfileData();
+            profile.FirstName = "OpenSim";
+            profile.ID = lib.Owner;
+            profile.SurName = "Library";
+            CachedUserInfo uinfo = new CachedUserInfo(invService, profile);
+
+            foreach (string iarFileName in Directory.GetFiles(pathToLibraries, "*.iar"))
+            {
+                string simpleName = Path.GetFileNameWithoutExtension(iarFileName);
+
+                m_log.InfoFormat("[LIBRARY MODULE]: Loading library archive {0} ({1})...", iarFileName, simpleName);
+                simpleName = GetInventoryPathFromName(simpleName);
+
+                try
+                {
+                    InventoryArchiveReadRequest archread = new InventoryArchiveReadRequest(m_MockScene, uinfo, simpleName, iarFileName);
+                    List<InventoryNodeBase> nodes = archread.Execute();
+                    if (nodes.Count == 0)
+                    {
+                        // didn't find the subfolder with the given name; place it on the top
+                        m_log.InfoFormat("[LIBRARY MODULE]: Didn't find {0} in library. Placing archive on the top level", simpleName);
+                        archread.Close();
+                        archread = new InventoryArchiveReadRequest(m_MockScene, uinfo, "/", iarFileName);
+                        archread.Execute();
+                    }
+                    archread.Close();
+                }
+                catch (Exception e)
+                {
+                    m_log.DebugFormat("[LIBRARY MODULE]: Exception when processing archive {0}: {1}", iarFileName, e.Message);
+                }
+
+            }
+
+        }
+
+        private void DumpLibrary()
+        {
+            InventoryFolderImpl lib = m_Scene.CommsManager.UserProfileCacheService.LibraryRoot;
+
+            m_log.DebugFormat(" - folder {0}", lib.Name);
+            DumpFolder(lib);
+        }
+
+        private void DumpFolder(InventoryFolderImpl folder)
+        {
+            foreach (InventoryItemBase item in folder.Items.Values)
+            {
+                m_log.DebugFormat("   --> item {0}", item.Name);
+            }
+            foreach (InventoryFolderImpl f in folder.RequestListOfFolderImpls())
+            {
+                m_log.DebugFormat(" - folder {0}", f.Name);
+                DumpFolder(f);
+            }
+        }
+
+        private string GetInventoryPathFromName(string name)
+        {
+            string[] parts = name.Split(new char[] { ' ' });
+            if (parts.Length == 3)
+            {
+                name = string.Empty;
+                // cut the last part
+                for (int i = 0; i < parts.Length - 1; i++)
+                    name = name + ' ' + parts[i];
+            }
+
+            return name;
+        }
+
+        #endregion LoadLibraries
+    }
+}
---
+++ OpenSim/Region/CoreModules/Framework/Library/LocalInventoryService.cs
@@ -0,0 +1,263 @@
+﻿/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using OpenSim.Framework;
+using OpenSim.Framework.Communications.Cache;
+using OpenSim.Services.Interfaces;
+
+using OpenMetaverse;
+using log4net;
+
+namespace OpenSim.Region.CoreModules.Framework.Library
+{
+    public class LocalInventoryService : IInventoryService
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private LibraryRootFolder m_Library;
+
+        public LocalInventoryService(LibraryRootFolder lib)
+        {
+            m_Library = lib;
+        }
+
+        /// <summary>
+        /// Retrieve the root inventory folder for the given user.
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns>null if no root folder was found</returns>
+        public InventoryFolderBase GetRootFolder(UUID userID) { return m_Library; }
+
+        /// <summary>
+        /// Gets everything (folders and items) inside a folder
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="folderID"></param>
+        /// <returns></returns>
+        public InventoryCollection GetFolderContent(UUID userID, UUID folderID)
+        {
+            InventoryFolderImpl folder = null;
+            InventoryCollection inv = new InventoryCollection();
+            inv.UserID = m_Library.Owner;
+
+            if (folderID != m_Library.ID)
+            {
+                folder = m_Library.FindFolder(folderID);
+                if (folder == null)
+                {
+                    inv.Folders = new List<InventoryFolderBase>();
+                    inv.Items = new List<InventoryItemBase>();
+                    return inv;
+                }
+            }
+            else
+                folder = m_Library;
+
+            inv.Folders = folder.RequestListOfFolders();
+            inv.Items = folder.RequestListOfItems();
+
+            m_log.DebugFormat("[LIBRARY MODULE]: Got content for folder {0}", folder.Name);
+            return inv;
+        }
+
+        /// <summary>
+        /// Add a new folder to the user's inventory
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns>true if the folder was successfully added</returns>
+        public bool AddFolder(InventoryFolderBase folder)
+        {
+            //m_log.DebugFormat("[LIBRARY MODULE]: Adding folder {0} ({1}) to {2}", folder.Name, folder.ID, folder.ParentID);
+            InventoryFolderImpl parent = m_Library;
+            if (m_Library.ID != folder.ParentID)
+                parent = m_Library.FindFolder(folder.ParentID);
+
+            if (parent == null)
+            {
+                m_log.DebugFormat("[LIBRARY MODULE]: could not add folder {0} because parent folder {1} not found", folder.Name, folder.ParentID);
+                return false;
+            }
+
+            parent.CreateChildFolder(folder.ID, folder.Name, (ushort)folder.Type);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Add a new item to the user's inventory
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>true if the item was successfully added</returns>
+        public bool AddItem(InventoryItemBase item)
+        {
+            //m_log.DebugFormat("[LIBRARY MODULE]: Adding item {0} to {1}", item.Name, item.Folder);
+            InventoryFolderImpl folder = m_Library;
+            if (m_Library.ID != item.Folder)
+                folder = m_Library.FindFolder(item.Folder);
+
+            if (folder == null)
+            {
+                m_log.DebugFormat("[LIBRARY MODULE]: could not add item {0} because folder {1} not found", item.Name, item.Folder);
+                return false;
+            }
+
+            folder.Items.Add(item.ID, item);
+            return true;
+        }
+
+        public bool CreateUserInventory(UUID user) { return false; }
+
+        /// <summary>
+        /// Gets the skeleton of the inventory -- folders only
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public List<InventoryFolderBase> GetInventorySkeleton(UUID userId) { return null; }
+
+        /// <summary>
+        /// Synchronous inventory fetch.
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns></returns>
+        public InventoryCollection GetUserInventory(UUID userID) { return null; }
+
+        /// <summary>
+        /// Request the inventory for a user.  This is an asynchronous operation that will call the callback when the
+        /// inventory has been received
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="callback"></param>
+        public void GetUserInventory(UUID userID, InventoryReceiptCallback callback) { }
+
+
+        /// <summary>
+        /// Gets the user folder for the given folder-type
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public InventoryFolderBase GetFolderForType(UUID userID, AssetType type) { return null; }
+
+
+        /// <summary>
+        /// Gets the items inside a folder
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="folderID"></param>
+        /// <returns></returns>
+        public List<InventoryItemBase> GetFolderItems(UUID userID, UUID folderID) { return null; }
+
+
+        /// <summary>
+        /// Update a folder in the user's inventory
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns>true if the folder was successfully updated</returns>
+        public bool UpdateFolder(InventoryFolderBase folder) { return false; }
+
+        /// <summary>
+        /// Move an inventory folder to a new location
+        /// </summary>
+        /// <param name="folder">A folder containing the details of the new location</param>
+        /// <returns>true if the folder was successfully moved</returns>
+        public bool MoveFolder(InventoryFolderBase folder) { return false; }
+
+        /// <summary>
+        /// Delete an item from the user's inventory
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>true if the item was successfully deleted</returns>
+        //bool DeleteItem(InventoryItemBase item);
+        public bool DeleteFolders(UUID userID, List<UUID> folderIDs) { return false; }
+
+        /// <summary>
+        /// Purge an inventory folder of all its items and subfolders.
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns>true if the folder was successfully purged</returns>
+        public bool PurgeFolder(InventoryFolderBase folder) { return false; }
+
+
+        /// <summary>
+        /// Update an item in the user's inventory
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>true if the item was successfully updated</returns>
+        public bool UpdateItem(InventoryItemBase item) { return false; }
+
+        public bool MoveItems(UUID ownerID, List<InventoryItemBase> items) { return false; }
+
+        /// <summary>
+        /// Delete an item from the user's inventory
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>true if the item was successfully deleted</returns>
+        //bool DeleteItem(InventoryItemBase item);
+        public bool DeleteItems(UUID userID, List<UUID> itemIDs) { return false; }
+
+        /// <summary>
+        /// Get an item, given by its UUID
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public InventoryItemBase GetItem(InventoryItemBase item) { return null; }
+
+        /// <summary>
+        /// Get a folder, given by its UUID
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns></returns>
+        public InventoryFolderBase GetFolder(InventoryFolderBase folder) { return null; }
+
+        /// <summary>
+        /// Does the given user have an inventory structure?
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns></returns>
+        public bool HasInventoryForUser(UUID userID) { return false; }
+
+        /// <summary>
+        /// Get the active gestures of the agent.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public List<InventoryItemBase> GetActiveGestures(UUID userId) { return null; }
+
+        /// <summary>
+        /// Get the union of permissions of all inventory items
+        /// that hold the given assetID. 
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="assetID"></param>
+        /// <returns>The permissions or 0 if no such asset is found in 
+        /// the user's inventory</returns>
+        public int GetAssetPermissions(UUID userID, UUID assetID) { return 0; }
+    }
+}
