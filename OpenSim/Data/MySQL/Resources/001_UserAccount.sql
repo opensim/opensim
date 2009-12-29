e9df86a6d6e79c7c370a454a6c2b5cdd3c3f3641
Diva Canto <diva@metaverseink.com>
2009-12-29 09:22:52 -0800

    * Added useraccount table

---
+++ OpenSim/Data/MySQL/Resources/001_UserAccount.sql
@@ -0,0 +1,13 @@
+﻿BEGIN;
+
+CREATE TABLE `useraccount` (
+    `UserID` CHAR(36) NOT NULL,
+    `ScopeID` CHAR(36) NOT NULL,
+    `FirstName` VARCHAR(64) NOT NULL,
+    `LastName` VARCHAR(64) NOT NULL,
+    `Email` VARCHAR(64),
+    `ServiceURLs` TEXT,
+    `Created` DATETIME
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+COMMIT;
