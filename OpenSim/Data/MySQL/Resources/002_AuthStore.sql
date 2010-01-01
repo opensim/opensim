c540c93b543358988b2c66c2c35d1616c41ee8d2
Diva Canto <diva@metaverseink.com>
2010-01-01 08:45:41 -0800

    Auth data migration.

---
+++ OpenSim/Data/MySQL/Resources/002_AuthStore.sql
@@ -0,0 +1,5 @@
+﻿BEGIN;
+
+INSERT INTO auth (UUID, passwordHash, passwordSalt, webLoginKey) SELECT `UUID` AS UUID, `passwordHash` AS passwordHash, `passwordSalt` AS passwordSalt, `webLoginKey` AS webLoginKey FROM users;
+
+COMMIT;
