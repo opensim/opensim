58dac970afb09bc003c7089f35fe592516fb209e
lcc <lcc@Mac-Pro.local>
2010-04-10 18:05:08 +0200

    re-fixing this darn file

---
+++ OpenSim/Region/Framework/Interfaces/ISnmpModule.cs
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////
+//
+// (c) Careminster LImited, Melanie Thielker and the Meta7 Team
+//
+// This file is not open source. All rights reserved
+//
+public interface ISnmpModule
+{
+    void Alert(string message);
+    void  Trap(int code,string simname,string Message);
+}
