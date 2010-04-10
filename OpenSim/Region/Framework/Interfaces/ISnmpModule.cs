c4eaf195b53be3f19fe25bd7ac4a41bd002bbc8d
lcc <lcc@Mac-Pro.local>
2010-04-10 17:59:08 +0200

    dding this deleted file...

--- OpenSim/Region/Framework/Interfaces/ISnmpModule.cs
+++ OpenSim/Region/Framework/Interfaces/ISnmpModule.cs
@@ -3,9 +3,9 @@
 // (c) Careminster LImited, Melanie Thielker and the Meta7 Team
 //
 // This file is not open source. All rights reserved
-//
+// Mod 2
 public interface ISnmpModule
 {
     void Alert(string message);
-    void Trap(string message);
+    void  Trap(int code,string simname,string Message);
 }
