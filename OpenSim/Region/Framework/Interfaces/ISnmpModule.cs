fac55bc78b70a1f8a3a53b222d9df515e32e82ae
Melanie <melanie@t-data.com>
2010-04-14 01:17:27 +0100

    Change ISnmpModule to use scene references

--- OpenSim/Region/Framework/Interfaces/ISnmpModule.cs
+++ OpenSim/Region/Framework/Interfaces/ISnmpModule.cs
@@ -4,8 +4,15 @@
 //
 // This file is not open source. All rights reserved
 // Mod 2
+
+using OpenSim.Region.Framework.Scenes;
+
 public interface ISnmpModule
 {
-    void Trap(int code,string simname,string Message);
-    void ColdStart(int step , string simname);
+    void Trap(int code, string Message, Scene scene);
+    void Critcal(string Message, Scene scene);
+    void Warning(string Message, Scene scene);
+    void Major(string Message, Scene scene);
+    void ColdStart(int step , Scene scene);
+    void Shutdown(int step , Scene scene);
 }
