27b3f23ff3c7b040885784b46a90e9523e633e93
Melanie Thielker <melanie@t-data.com>
2010-04-24 16:53:47 +0200

    Remove some usings that stopped compilation

--- OpenSim/Services/Interfaces/IAttachmentsService.cs
+++ OpenSim/Services/Interfaces/IAttachmentsService.cs
@@ -5,23 +5,9 @@
 // All rights reserved
 //
 using System;
-using System.Diagnostics;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Threading;
-using System.Reflection;
-using OpenSim.Framework;
-using OpenSim.Framework.Console;
-using OpenSim.Server.Base;
-using OpenSim.Services.Base;
-using OpenSim.Services.Interfaces;
 using Nini.Config;
-using log4net;
-using Careminster;
-using OpenMetaverse;
 
-namespace Careminster
+namespace OpenSim.Services.Interfaces
 {
     public interface IAttachmentsService
     {
