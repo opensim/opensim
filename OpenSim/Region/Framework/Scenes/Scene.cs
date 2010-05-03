7a8ad1ceb23b768863a05997d3c0397dc301a323
Melanie <melanie@t-data.com>
2010-05-03 01:30:57 +0100

    Make in-place sale send CHANGED_OWNER again

--- OpenSim/Region/Framework/Scenes/Scene.cs
+++ OpenSim/Region/Framework/Scenes/Scene.cs
@@ -4522,6 +4522,7 @@ namespace OpenSim.Region.Framework.Scenes
                     foreach (SceneObjectPart child in partList)
                     {
                         child.Inventory.ChangeInventoryOwner(remoteClient.AgentId);
+                        child.TriggerScriptChangedEvent(Changed.OWNER);
                         child.ApplyNextOwnerPermissions();
                     }
                 }
@@ -4531,6 +4532,8 @@ namespace OpenSim.Region.Framework.Scenes
 
                 group.HasGroupChanged = true;
                 part.GetProperties(remoteClient);
+                part.TriggerScriptChangedEvent(Changed.OWNER);
+                group.ResumeScripts();
                 part.ScheduleFullUpdate();
 
                 break;
