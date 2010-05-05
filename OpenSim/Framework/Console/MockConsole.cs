f005e570aa85fba4e2f82094f9d795db52895eeb
Teravus Ovares (Dan Olivares) <teravus@gmail.com>
2010-05-04 22:13:25 -0400

    * This should fix the tests failing because of a MainConsole.Instance null reference * Added a MockConsole that doesn't require a handle to System.Console

---
+++ OpenSim/Framework/Console/MockConsole.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSim.Framework.Console
+{
+    public class MockConsole : CommandConsole
+    {
+        public MockConsole(string defaultPrompt) : base(defaultPrompt)
+        {
+        }
+        public override void Output(string text)
+        {
+        }
+        public override void Output(string text, string level)
+        {
+        }
+
+        public override string ReadLine(string p, bool isCommand, bool e)
+        {
+            //Thread.CurrentThread.Join(1000);
+            return string.Empty;
+        }
+        public override void UnlockOutput()
+        {
+        }
+        public override void LockOutput()
+        {
+        }
+    }
+}
