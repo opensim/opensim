2b48ed60ecf45e4ec07af1cadbb6325c3e678a13
Melanie Thielker <melanie@t-data.com>
2010-05-06 02:02:12 +0200

    Remove the m_Viewer variable and make the property a shortcut to the proper field in AgentCircuitData instead

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -218,8 +218,6 @@ namespace OpenSim.Region.Framework.Scenes
         private bool m_followCamAuto;
 
         private int m_movementUpdateCount;
-        private string m_Viewer = String.Empty;
-
         private const int NumMovementsBetweenRayCast = 5;
 
         private bool CameraConstraintActive;
@@ -654,8 +652,7 @@ namespace OpenSim.Region.Framework.Scenes
 
         public string Viewer
         {
-            get { return m_Viewer; }
-            set { m_Viewer = value; }
+            get { return m_scene.AuthenticateHandler.GetAgentCircuitData(ControllingClient.CircuitCode).Viewer; }
         }
 
         #endregion
