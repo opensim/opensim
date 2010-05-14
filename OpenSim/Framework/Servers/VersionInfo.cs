e79eac238ed54d1e0d41ad1ff970473226cede50
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2010-05-14 21:30:53 +0100

    switch 0.6.9 flavour to release

--- OpenSim/Framework/Servers/VersionInfo.cs
+++ OpenSim/Framework/Servers/VersionInfo.cs
@@ -30,7 +30,7 @@ namespace OpenSim
     public class VersionInfo
     {
         private const string VERSION_NUMBER = "0.6.9";
-        private const Flavour VERSION_FLAVOUR = Flavour.RC1;
+        private const Flavour VERSION_FLAVOUR = Flavour.Release;
 
         public enum Flavour
         {
