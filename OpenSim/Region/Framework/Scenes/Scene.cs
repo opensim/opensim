c7d812802fcb2531c49f0fe4f4971571f332183d
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2010-05-28 18:49:32 +0100

    Adjust Scene.DeleteAllSceneObjects() to not delete objects attached to avatars.
    This is going to be the right behaviour in all cases, I should think.
    This means that avatars in region when an oar is loaded do not lose their attachments

--- OpenSim/Region/Framework/Scenes/Scene.cs
+++ OpenSim/Region/Framework/Scenes/Scene.cs
@@ -1908,7 +1908,7 @@ namespace OpenSim.Region.Framework.Scenes
         }        
 
         /// <summary>
-        /// Delete every object from the scene
+        /// Delete every object from the scene.  This does not include attachments worn by avatars.
         /// </summary>
         public void DeleteAllSceneObjects()
         {
@@ -1919,7 +1919,11 @@ namespace OpenSim.Region.Framework.Scenes
                 foreach (EntityBase e in entities)
                 {
                     if (e is SceneObjectGroup)
-                        DeleteSceneObject((SceneObjectGroup)e, false);
+                    {
+                        SceneObjectGroup sog = (SceneObjectGroup)e;
+                        if (!sog.IsAttachment)
+                            DeleteSceneObject((SceneObjectGroup)e, false);
+                    }
                 }
             }
         }
