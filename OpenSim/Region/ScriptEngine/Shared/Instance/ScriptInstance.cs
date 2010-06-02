241ed5734ee3c5b1417484dfa22af6cf6ae60b82
Melanie <melanie@t-data.com>
2010-06-02 17:14:37 +0100

    After aborting a thread, wait for it to actually finish. EXPERIMENTAL for OSG testing. DO NOT UPDATE TO THIS UNLESS YOU KNOW WHAT YOU'RE DOING!

--- OpenSim/Region/ScriptEngine/Shared/Instance/ScriptInstance.cs
+++ OpenSim/Region/ScriptEngine/Shared/Instance/ScriptInstance.cs
@@ -536,6 +536,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Instance
             if (!m_InSelfDelete)
                 result.Abort();
 
+            result.Wait(TimeSpan.FromSeconds(10));
             lock (m_EventQueue)
             {
                 m_CurrentResult = null;
