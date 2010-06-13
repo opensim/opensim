3d319d6c3ff2bded0f4b6c730f227a490dd2fb2e
Melanie Thielker <melanie@t-data.com>
2010-06-13 22:23:07 +0200

    Actually update the SP. EXPERIMENTAL

--- OpenSim/Region/Framework/Scenes/SceneObjectPart.cs
+++ OpenSim/Region/Framework/Scenes/SceneObjectPart.cs
@@ -4726,7 +4726,13 @@ namespace OpenSim.Region.Framework.Scenes
             if (m_sitTargetAvatar != UUID.Zero)
                 return;
 
+            ScenePresence p = m_parentGroup.Scene.GetScenePresence(m_sitTargetAvatar);
+            if (p == null)
+                return;
+
             // Update sitting avatar chat position
+
+            p.AbsolutePosition = AbsolutePosition;
         }
     }
 }
