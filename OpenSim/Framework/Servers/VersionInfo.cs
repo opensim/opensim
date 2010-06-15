cf5427e8100aa398fc268c983244d177a1f871fa
Diva Canto <diva@metaverseink.com>
2010-06-14 19:13:46 -0700

    Fixed version info again.

--- OpenSim/Framework/Servers/VersionInfo.cs
+++ OpenSim/Framework/Servers/VersionInfo.cs
@@ -29,8 +29,8 @@ namespace OpenSim
 {
     public class VersionInfo
     {
-        private const string VERSION_NUMBER = "0.7.1";
-        private const Flavour VERSION_FLAVOUR = Flavour.Dev;
+        private const string VERSION_NUMBER = "0.7";
+        private const Flavour VERSION_FLAVOUR = Flavour.RC1;
 
         public enum Flavour
         {
