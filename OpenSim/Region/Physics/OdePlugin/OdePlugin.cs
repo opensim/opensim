1032e1142a30f2fbeab85bd6827212c8b168c7e5
Melanie <melanie@t-data.com>
2010-06-18 19:49:14 +0100

    Remove an error in meshing error reporting. Now doesn't double-bomb anymore on a bad sculpt

--- OpenSim/Region/Physics/OdePlugin/OdePlugin.cs
+++ OpenSim/Region/Physics/OdePlugin/OdePlugin.cs
@@ -1778,7 +1778,7 @@ namespace OpenSim.Region.Physics.OdePlugin
                 }
                 catch(Exception e)
                 {
-                    m_log.ErrorFormat("[PHYSICS]: Exception while meshing prim {0}.");
+                    m_log.ErrorFormat("[PHYSICS]: Exception while meshing prim {0}.", primName);
                     m_log.Debug(e.ToString());
                     mesh = null;
                 }
