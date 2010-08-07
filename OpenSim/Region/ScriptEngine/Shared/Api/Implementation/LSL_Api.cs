7bac069976b31ecb39c2d667b51aaa555f8207e3
Melanie Thielker <melanie@t-data.com>
2010-08-07 22:10:46 +0200

    Mantis #229. Fix Global sim ccordinates

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -9145,8 +9145,8 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                             return UUID.Zero.ToString();
                         }
                         reply = new LSL_Vector(
-                            info.RegionLocX / Constants.RegionSize,
-                            info.RegionLocY / Constants.RegionSize,
+                            info.RegionLocX;
+                            info.RegionLocY;
                             0).ToString();
                         break;
                     case 6: // DATA_SIM_STATUS
