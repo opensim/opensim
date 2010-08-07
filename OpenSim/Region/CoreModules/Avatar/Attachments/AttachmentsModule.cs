fa11ac8c851d4471640dfb0381ca285d0e5299d7
Melanie Thielker <melanie@t-data.com>
2010-08-07 05:42:30 +0200

    Change the (hackish) constant to match the changed enum for attachs

--- OpenSim/Region/CoreModules/Avatar/Attachments/AttachmentsModule.cs
+++ OpenSim/Region/CoreModules/Avatar/Attachments/AttachmentsModule.cs
@@ -293,8 +293,8 @@ namespace OpenSim.Region.CoreModules.Avatar.Attachments
                     }
 
                     // Fire after attach, so we don't get messy perms dialogs
-                    // 3 == AttachedRez
-                    objatt.CreateScriptInstances(0, true, m_scene.DefaultScriptEngine, 3);
+                    // 4 == AttachedRez
+                    objatt.CreateScriptInstances(0, true, m_scene.DefaultScriptEngine, 4);
                     objatt.ResumeScripts();
 
                     // Do this last so that event listeners have access to all the effects of the attachment
