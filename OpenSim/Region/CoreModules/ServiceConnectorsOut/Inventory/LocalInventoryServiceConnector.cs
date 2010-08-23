opensim/opensim

commit bd49985afa0a30cf9338730807a42eff3d508bee

Switched everything to XInventory by default. The old Inventory is still there for now, in case bugs pop up with XInventory.

OpenSim/Region/CoreModules/ServiceConnectorsOut/Inventory/LocalInventoryServiceConnector.cs
@@ -41,7 +41,7 @@ using OpenMetaverse;
 
 namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
 {
-    public class LocalInventoryServicesConnector : BaseInventoryConnector, ISharedRegionModule, IInventoryService
+    public class LocalInventoryServicesConnector : ISharedRegionModule, IInventoryService
     {
         private static readonly ILog m_log =
                 LogManager.GetLogger(
@@ -50,7 +50,6 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         private IInventoryService m_InventoryService;
 
         private bool m_Enabled = false;
-        private bool m_Initialized = false;
 
         public Type ReplaceableInterface 
         {
@@ -93,22 +92,8 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
                     if (m_InventoryService == null)
                     {
                         m_log.Error("[LOCAL INVENTORY SERVICES CONNECTOR]: Can't load inventory service");
-                        //return;
                         throw new Exception("Unable to proceed. Please make sure your ini files in config-include are updated according to .example's");
                     }
-
-                    //List<IInventoryDataPlugin> plugins
-                    //    = DataPluginFactory.LoadDataPlugins<IInventoryDataPlugin>(
-                    //        configSettings.StandaloneInventoryPlugin,
-                    //        configSettings.StandaloneInventorySource);
-
-                    //foreach (IInventoryDataPlugin plugin in plugins)
-                    //{
-                    //    // Using the OSP wrapper plugin for database plugins should be made configurable at some point
-                    //    m_InventoryService.AddPlugin(new OspInventoryWrapperPlugin(plugin, this));
-                    //}
-
-                    Init(source);
 
                     m_Enabled = true;
                     m_log.Info("[LOCAL INVENTORY SERVICES CONNECTOR]: Local inventory connector enabled");
@@ -128,99 +113,60 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         {
             if (!m_Enabled)
                 return;
-
-            if (!m_Initialized)
-            {
-                m_Initialized = true;
-            }
-
-//            m_log.DebugFormat(
-//                "[LOCAL INVENTORY SERVICES CONNECTOR]: Registering IInventoryService to scene {0}", scene.RegionInfo.RegionName);
             
             scene.RegisterModuleInterface<IInventoryService>(this);
-            m_cache.AddRegion(scene);
         }
 
         public void RemoveRegion(Scene scene)
         {
             if (!m_Enabled)
                 return;
-
-            m_cache.RemoveRegion(scene);
         }
 
         public void RegionLoaded(Scene scene)
         {
             if (!m_Enabled)
                 return;
-
-            m_log.InfoFormat(
-                "[LOCAL INVENTORY SERVICES CONNECTOR]: Enabled local inventory for region {0}", scene.RegionInfo.RegionName);
         }
 
         #region IInventoryService
 
-        public override bool CreateUserInventory(UUID user)
+        public bool CreateUserInventory(UUID user)
         {
             return m_InventoryService.CreateUserInventory(user);
         }
 
-        public override List<InventoryFolderBase> GetInventorySkeleton(UUID userId)
+        public List<InventoryFolderBase> GetInventorySkeleton(UUID userId)
         {
             return m_InventoryService.GetInventorySkeleton(userId);
         }
 
-        public override InventoryCollection GetUserInventory(UUID id)
+        public InventoryCollection GetUserInventory(UUID id)
         {
             return m_InventoryService.GetUserInventory(id);
         }
 
-        public override void GetUserInventory(UUID userID, InventoryReceiptCallback callback)
+        public void GetUserInventory(UUID userID, InventoryReceiptCallback callback)
         {
             m_InventoryService.GetUserInventory(userID, callback);
         }
 
-        // Inherited. See base
-        //public InventoryFolderBase GetFolderForType(UUID userID, AssetType type)
-        //{
-        //    return m_InventoryService.GetFolderForType(userID, type);
-        //}
-
-        public override Dictionary<AssetType, InventoryFolderBase> GetSystemFolders(UUID userID)
+        public InventoryFolderBase GetRootFolder(UUID userID)
         {
-            InventoryFolderBase root = m_InventoryService.GetRootFolder(userID);
-            if (root != null)
-            {
-                InventoryCollection content = GetFolderContent(userID, root.ID);
-                if (content != null)
-                {
-                    Dictionary<AssetType, InventoryFolderBase> folders = new Dictionary<AssetType, InventoryFolderBase>();
-                    foreach (InventoryFolderBase folder in content.Folders)
-                    {
-                        if ((folder.Type != (short)AssetType.Folder) && (folder.Type != (short)AssetType.Unknown))
-                        {
-                            //m_log.InfoFormat("[INVENTORY CONNECTOR]: folder type {0} ", folder.Type);
-                            folders[(AssetType)folder.Type] = folder;
-                        }
-                    }
-                    // Put the root folder there, as type Folder
-                    folders[AssetType.Folder] = root;
-                    //m_log.InfoFormat("[INVENTORY CONNECTOR]: root folder is type {0} ", root.Type);
-
-                    return folders;
-                }
-            }
-            m_log.WarnFormat("[LOCAL INVENTORY SERVICES CONNECTOR]: System folders for {0} not found", userID);
-            return new Dictionary<AssetType, InventoryFolderBase>();
+            return m_InventoryService.GetRootFolder(userID);
         }
 
-        public override InventoryCollection GetFolderContent(UUID userID, UUID folderID)
+        public InventoryFolderBase GetFolderForType(UUID userID, AssetType type)
+        {
+            return m_InventoryService.GetFolderForType(userID, type);
+        }
+
+        public InventoryCollection GetFolderContent(UUID userID, UUID folderID)
         {
             return m_InventoryService.GetFolderContent(userID, folderID);
         }
 
-
-        public override List<InventoryItemBase> GetFolderItems(UUID userID, UUID folderID)
+        public List<InventoryItemBase> GetFolderItems(UUID userID, UUID folderID)
         {
             return m_InventoryService.GetFolderItems(userID, folderID);
         }
@@ -230,7 +176,7 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         /// </summary>
         /// <param name="folder"></param>
         /// <returns>true if the folder was successfully added</returns>
-        public override bool AddFolder(InventoryFolderBase folder)
+        public bool AddFolder(InventoryFolderBase folder)
         {
             return m_InventoryService.AddFolder(folder);
         }
@@ -240,7 +186,7 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         /// </summary>
         /// <param name="folder"></param>
         /// <returns>true if the folder was successfully updated</returns>
-        public override bool UpdateFolder(InventoryFolderBase folder)
+        public bool UpdateFolder(InventoryFolderBase folder)
         {
             return m_InventoryService.UpdateFolder(folder);
         }
@@ -250,12 +196,12 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         /// </summary>
         /// <param name="folder">A folder containing the details of the new location</param>
         /// <returns>true if the folder was successfully moved</returns>
-        public override bool MoveFolder(InventoryFolderBase folder)
+        public bool MoveFolder(InventoryFolderBase folder)
         {
             return m_InventoryService.MoveFolder(folder);
         }
 
-        public override bool DeleteFolders(UUID ownerID, List<UUID> folderIDs)
+        public bool DeleteFolders(UUID ownerID, List<UUID> folderIDs)
         {
             return m_InventoryService.DeleteFolders(ownerID, folderIDs);
         }
@@ -265,18 +211,17 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         /// </summary>
         /// <param name="folder"></param>
         /// <returns>true if the folder was successfully purged</returns>
-        public override bool PurgeFolder(InventoryFolderBase folder)
+        public bool PurgeFolder(InventoryFolderBase folder)
         {
             return m_InventoryService.PurgeFolder(folder);
         }
 
         /// <summary>
-        /// Add a new item to the user's inventory, plain
-        /// Called by base class AddItem
+        /// Add a new item to the user's inventory
         /// </summary>
         /// <param name="item"></param>
         /// <returns>true if the item was successfully added</returns>
-        protected override bool AddItemPlain(InventoryItemBase item)
+        public bool AddItem(InventoryItemBase item)
         {
             return m_InventoryService.AddItem(item);
         }
@@ -286,13 +231,13 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         /// </summary>
         /// <param name="item"></param>
         /// <returns>true if the item was successfully updated</returns>
-        public override bool UpdateItem(InventoryItemBase item)
+        public bool UpdateItem(InventoryItemBase item)
         {
             return m_InventoryService.UpdateItem(item);
         }
 
 
-        public override bool MoveItems(UUID ownerID, List<InventoryItemBase> items)
+        public bool MoveItems(UUID ownerID, List<InventoryItemBase> items)
         {
             return m_InventoryService.MoveItems(ownerID, items);
         }
@@ -302,12 +247,12 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         /// </summary>
         /// <param name="item"></param>
         /// <returns>true if the item was successfully deleted</returns>
-        public override bool DeleteItems(UUID ownerID, List<UUID> itemIDs)
+        public bool DeleteItems(UUID ownerID, List<UUID> itemIDs)
         {
             return m_InventoryService.DeleteItems(ownerID, itemIDs);
         }
 
-        public override InventoryItemBase GetItem(InventoryItemBase item)
+        public InventoryItemBase GetItem(InventoryItemBase item)
         {
 //            m_log.DebugFormat("[LOCAL INVENTORY SERVICES CONNECTOR]: Requesting inventory item {0}", item.ID);
 
@@ -322,7 +267,7 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
             return item;
         }
 
-        public override InventoryFolderBase GetFolder(InventoryFolderBase folder)
+        public InventoryFolderBase GetFolder(InventoryFolderBase folder)
         {
             return m_InventoryService.GetFolder(folder);
         }
@@ -332,17 +277,17 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
         /// </summary>
         /// <param name="userID"></param>
         /// <returns></returns>
-        public override bool HasInventoryForUser(UUID userID)
+        public bool HasInventoryForUser(UUID userID)
         {
             return m_InventoryService.HasInventoryForUser(userID);
         }
 
-        public override List<InventoryItemBase> GetActiveGestures(UUID userId)
+        public List<InventoryItemBase> GetActiveGestures(UUID userId)
         {
             return m_InventoryService.GetActiveGestures(userId);
         }
 
-        public override int GetAssetPermissions(UUID userID, UUID assetID)
+        public int GetAssetPermissions(UUID userID, UUID assetID)
         {
             return m_InventoryService.GetAssetPermissions(userID, assetID);
         }

OpenSim/Data/SQLite/Resources/001_XInventoryStore.sql
@@ -1,0 +1,38 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE inventoryfolders(
+       folderName varchar(255),
+       type integer,
+       version integer,
+       folderID varchar(255) primary key,
+       agentID varchar(255) not null default '00000000-0000-0000-0000-000000000000',
+       parentFolderID varchar(255) not null default '00000000-0000-0000-0000-000000000000');
+
+CREATE TABLE inventoryitems(
+       assetID varchar(255),
+       assetType integer,
+       inventoryName varchar(255),
+       inventoryDescription varchar(255),
+       inventoryNextPermissions integer,
+       inventoryCurrentPermissions integer,
+       invType integer,
+       creatorID varchar(255),
+       inventoryBasePermissions integer,
+       inventoryEveryOnePermissions integer, 
+       salePrice integer default 99, 
+       saleType integer default 0, 
+       creationDate integer default 2000, 
+       groupID varchar(255) default '00000000-0000-0000-0000-000000000000', 
+       groupOwned integer default 0, 
+       flags integer default 0,
+       inventoryID varchar(255) primary key,
+       parentFolderID varchar(255) not null default '00000000-0000-0000-0000-000000000000',
+       avatarID varchar(255) not null default '00000000-0000-0000-0000-000000000000',
+       inventoryGroupPermissions integer not null default 0);
+
+create index inventoryfolders_agentid on inventoryfolders(agentID);
+create index inventoryfolders_parentid on inventoryfolders(parentFolderID);
+create index inventoryitems_parentfolderid on inventoryitems(parentFolderID);
+create index inventoryitems_avatarid on inventoryitems(avatarID);
+
+COMMIT;

OpenSim/Data/SQLite/Resources/002_XInventoryStore.sql
@@ -1,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+
+ATTACH 'inventoryStore.db' AS old;
+
+INSERT INTO inventoryfolders (folderName, type, version, folderID, agentID, parentFolderID) SELECT `name` AS folderName, `type` AS type, `version` AS version, `UUID` AS folderID, `agentID` AS agentID, `parentID` AS parentFolderID from old.inventoryfolders;
+
+INSERT INTO inventoryitems (assetID, assetType, inventoryName, inventoryDescription, inventoryNextPermissions, inventoryCurrentPermissions, invType, creatorID, inventoryBasePermissions, inventoryEveryOnePermissions, salePrice, saleType, creationDate, groupID, groupOwned, flags, inventoryID, parentFolderID, avatarID, inventoryGroupPermissions) SELECT `assetID`, `assetType` AS assetType, `inventoryName` AS inventoryName, `inventoryDescription` AS inventoryDescription, `inventoryNextPermissions` AS inventoryNextPermissions, `inventoryCurrentPermissions` AS inventoryCurrentPermissions, `invType` AS invType, `creatorsID` AS creatorID, `inventoryBasePermissions` AS inventoryBasePermissions, `inventoryEveryOnePermissions` AS inventoryEveryOnePermissions, `salePrice` AS salePrice, `saleType` AS saleType, `creationDate` AS creationDate, `groupID` AS groupID, `groupOwned` AS groupOwned, `flags` AS flags, `UUID` AS inventoryID, `parentFolderID` AS parentFolderID, `avatarID` AS avatarID, `inventoryGroupPermissions` AS inventoryGroupPermissions FROM old.inventoryitems;
+
+COMMIT;

OpenSim/Data/SQLite/SQLiteXInventoryData.cs
@@ -49,7 +49,7 @@ namespace OpenSim.Data.SQLite
         public SQLiteXInventoryData(string conn, string realm)
         {
             m_Folders = new SQLiteGenericTableHandler<XInventoryFolder>(
-                    conn, "inventoryfolders", "InventoryStore");
+                    conn, "inventoryfolders", "XInventoryStore");
             m_Items = new SqliteItemHandler(
                     conn, "inventoryitems", String.Empty);
         }

bin/Robust.HG.ini.example
@@ -11,7 +11,7 @@
 ;;
 
 [Startup]
-ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003/OpenSim.Server.Handlers.dll:InventoryServiceInConnector,8002/OpenSim.Server.Handlers.dll:FreeswitchServerConnector,8003/OpenSim.Server.Handlers.dll:GridServiceConnector,8003/OpenSim.Server.Handlers.dll:GridInfoServerInConnector,8003/OpenSim.Server.Handlers.dll:AuthenticationServiceConnector,8002/OpenSim.Server.Handlers.dll:OpenIdServerConnector,8003/OpenSim.Server.Handlers.dll:AvatarServiceConnector,8002/OpenSim.Server.Handlers.dll:LLLoginServiceInConnector,8003/OpenSim.Server.Handlers.dll:PresenceServiceConnector,8003/OpenSim.Server.Handlers.dll:UserAccountServiceConnector,8003/OpenSim.Server.Handlers.dll:FriendsServiceConnector,8002/OpenSim.Server.Handlers.dll:GatekeeperServiceInConnector,8002/OpenSim.Server.Handlers.dll:UserAgentServerConnector,HGInventoryService@8002/OpenSim.Server.Handlers.dll:HGInventoryServiceInConnector,8002/OpenSim.Server.Handlers.dll:AssetServiceConnector"
+ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003/OpenSim.Server.Handlers.dll:XInventoryServiceInConnector,8002/OpenSim.Server.Handlers.dll:FreeswitchServerConnector,8003/OpenSim.Server.Handlers.dll:GridServiceConnector,8003/OpenSim.Server.Handlers.dll:GridInfoServerInConnector,8003/OpenSim.Server.Handlers.dll:AuthenticationServiceConnector,8002/OpenSim.Server.Handlers.dll:OpenIdServerConnector,8003/OpenSim.Server.Handlers.dll:AvatarServiceConnector,8002/OpenSim.Server.Handlers.dll:LLLoginServiceInConnector,8003/OpenSim.Server.Handlers.dll:PresenceServiceConnector,8003/OpenSim.Server.Handlers.dll:UserAccountServiceConnector,8003/OpenSim.Server.Handlers.dll:FriendsServiceConnector,8002/OpenSim.Server.Handlers.dll:GatekeeperServiceInConnector,8002/OpenSim.Server.Handlers.dll:UserAgentServerConnector,HGInventoryService@8002/OpenSim.Server.Handlers.dll:XInventoryInConnector,8002/OpenSim.Server.Handlers.dll:AssetServiceConnector"
 
 ; * This is common for all services, it's the network setup for the entire
 ; * server instance, if none if specified above
@@ -44,8 +44,7 @@ ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003
 ; * the function of the legacy inventory server
 ; *
 [InventoryService]
-    LocalServiceModule = "OpenSim.Services.InventoryService.dll:InventoryService"
-    SessionAuthentication = "false"
+    LocalServiceModule = "OpenSim.Services.InventoryService.dll:XInventoryService"
 
 ; * This is the new style grid service.
 ; * "Realm" is the table that is used for user lookup.
@@ -92,7 +91,7 @@ ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003
     AuthenticationService = "OpenSim.Services.AuthenticationService.dll:PasswordAuthenticationService"
     PresenceService = "OpenSim.Services.PresenceService.dll:PresenceService"
     GridService = "OpenSim.Services.GridService.dll:GridService"
-    InventoryService = "OpenSim.Services.InventoryService.dll:InventoryService"
+    InventoryService = "OpenSim.Services.InventoryService.dll:XInventoryService"
 
 [PresenceService]
     ; for the server connector
@@ -116,7 +115,7 @@ ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003
     ; for the service
     UserAccountService = "OpenSim.Services.UserAccountService.dll:UserAccountService"
     AuthenticationService = "OpenSim.Services.AuthenticationService.dll:PasswordAuthenticationService"
-    InventoryService = "OpenSim.Services.InventoryService.dll:InventoryService"
+    InventoryService = "OpenSim.Services.InventoryService.dll:XInventoryService"
     AvatarService = "OpenSim.Services.AvatarService.dll:AvatarService"
     PresenceService = "OpenSim.Services.PresenceService.dll:PresenceService"
     GridService = "OpenSim.Services.GridService.dll:GridService"

bin/Robust.ini.example
@@ -11,7 +11,7 @@
 ; *
 [Startup]
 
-ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003/OpenSim.Server.Handlers.dll:InventoryServiceInConnector,8002/OpenSim.Server.Handlers.dll:FreeswitchServerConnector,8003/OpenSim.Server.Handlers.dll:GridServiceConnector,8002/OpenSim.Server.Handlers.dll:GridInfoServerInConnector,8003/OpenSim.Server.Handlers.dll:AuthenticationServiceConnector,8002/OpenSim.Server.Handlers.dll:OpenIdServerConnector,8003/OpenSim.Server.Handlers.dll:AvatarServiceConnector,8002/OpenSim.Server.Handlers.dll:LLLoginServiceInConnector,8003/OpenSim.Server.Handlers.dll:PresenceServiceConnector,8003/OpenSim.Server.Handlers.dll:UserAccountServiceConnector,8003/OpenSim.Server.Handlers.dll:FriendsServiceConnector"
+ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003/OpenSim.Server.Handlers.dll:XInventoryInConnector,8002/OpenSim.Server.Handlers.dll:FreeswitchServerConnector,8003/OpenSim.Server.Handlers.dll:GridServiceConnector,8002/OpenSim.Server.Handlers.dll:GridInfoServerInConnector,8003/OpenSim.Server.Handlers.dll:AuthenticationServiceConnector,8002/OpenSim.Server.Handlers.dll:OpenIdServerConnector,8003/OpenSim.Server.Handlers.dll:AvatarServiceConnector,8002/OpenSim.Server.Handlers.dll:LLLoginServiceInConnector,8003/OpenSim.Server.Handlers.dll:PresenceServiceConnector,8003/OpenSim.Server.Handlers.dll:UserAccountServiceConnector,8003/OpenSim.Server.Handlers.dll:FriendsServiceConnector"
 
 ; * This is common for all services, it's the network setup for the entire
 ; * server instance, if none if specified above
@@ -45,8 +45,7 @@ ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003
 ; * the function of the legacy inventory server
 ; *
 [InventoryService]
-    LocalServiceModule = "OpenSim.Services.InventoryService.dll:InventoryService"
-    SessionAuthentication = "false"
+    LocalServiceModule = "OpenSim.Services.InventoryService.dll:XInventoryService"
 
 ; * This is the new style grid service.
 ; * "Realm" is the table that is used for user lookup.
@@ -94,7 +93,7 @@ ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003
     AuthenticationService = "OpenSim.Services.AuthenticationService.dll:PasswordAuthenticationService"
     PresenceService = "OpenSim.Services.PresenceService.dll:PresenceService"
     GridService = "OpenSim.Services.GridService.dll:GridService"
-    InventoryService = "OpenSim.Services.InventoryService.dll:InventoryService"
+    InventoryService = "OpenSim.Services.InventoryService.dll:XInventoryService"
 
 [PresenceService]
     ; for the server connector
@@ -118,7 +117,7 @@ ServiceConnectors = "8003/OpenSim.Server.Handlers.dll:AssetServiceConnector,8003
     ; for the service
     UserAccountService = "OpenSim.Services.UserAccountService.dll:UserAccountService"
     AuthenticationService = "OpenSim.Services.AuthenticationService.dll:PasswordAuthenticationService"
-    InventoryService = "OpenSim.Services.InventoryService.dll:InventoryService"
+    InventoryService = "OpenSim.Services.InventoryService.dll:XInventoryService"
     AvatarService = "OpenSim.Services.AvatarService.dll:AvatarService"
     PresenceService = "OpenSim.Services.PresenceService.dll:PresenceService"
     GridService = "OpenSim.Services.GridService.dll:GridService"

bin/config-include/Grid.ini
@@ -9,7 +9,7 @@
 
 [Modules]
     AssetServices = "RemoteAssetServicesConnector"
-    InventoryServices = "RemoteInventoryServicesConnector"
+    InventoryServices = "RemoteXInventoryServicesConnector"
     GridServices = "RemoteGridServicesConnector"
     AvatarServices = "RemoteAvatarServicesConnector"
     NeighbourServices = "RemoteNeighbourServicesConnector"

bin/config-include/GridHypergrid.ini
@@ -30,8 +30,7 @@
     HypergridAssetService = "OpenSim.Services.Connectors.dll:HGAssetServiceConnector"
 
 [InventoryService]
-    LocalGridInventoryService = "OpenSim.Region.CoreModules.dll:RemoteInventoryServicesConnector"
-    HypergridInventoryService = "OpenSim.Services.Connectors.dll:HGInventoryServiceConnector"
+    LocalGridInventoryService = "OpenSim.Region.CoreModules.dll:RemoteXInventoryServicesConnector"
 
 [GridService]
 	; RemoteGridServicesConnector instantiates a LocalGridServicesConnector,

commit 704a53fb9bfeb945fa25f7367c631698dea5ae89

Fix bug in llGiveInventory() where items were disappearing on relog
This was a regression - the code to look up the correct type folder was no longer being called if items were added without a parent folder set
This may have been broken since commit bd49985a on 2010-05-02

OpenSim/Region/CoreModules/ServiceConnectorsOut/Inventory/LocalInventoryServiceConnector.cs
@@ -216,16 +216,28 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
             return m_InventoryService.PurgeFolder(folder);
         }
 
-        /// <summary>
-        /// Add a new item to the user's inventory
-        /// </summary>
-        /// <param name="item"></param>
-        /// <returns>true if the item was successfully added</returns>
         public bool AddItem(InventoryItemBase item)
         {
             m_log.DebugFormat(
                 "[LOCAL INVENTORY SERVICES CONNECTOR]: Adding inventory item {0} to user {1} folder {2}", 
                 item.Name, item.Owner, item.Folder);            
+
+            if (UUID.Zero == item.Folder)
+            {
+                InventoryFolderBase f = m_InventoryService.GetFolderForType(item.Owner, (AssetType)item.AssetType);
+                if (f != null)
+                {
+                    item.Folder = f.ID;
+                }
+                else
+                {
+                    f = m_InventoryService.GetRootFolder(item.Owner);
+                    if (f != null)
+                        item.Folder = f.ID;
+                    else
+                        return false;
+                }
+            }
             
             return m_InventoryService.AddItem(item);
         }

OpenSim/Region/CoreModules/ServiceConnectorsOut/Inventory/RemoteXInventoryServiceConnector.cs
@@ -294,9 +294,6 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Inventory
             return m_RemoteConnector.GetAssetPermissions(userID, assetID);
         }
 
-
         #endregion
-
-
     }
 }

OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -3844,18 +3844,16 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             if (World.GetScenePresence(destId) != null)
             {
                 // destination is an avatar
-                InventoryItemBase agentItem =
-                        World.MoveTaskInventoryItem(destId, UUID.Zero, m_host, objId);
+                InventoryItemBase agentItem = World.MoveTaskInventoryItem(destId, UUID.Zero, m_host, objId);
 
                 if (agentItem == null)
                     return;
 
                 byte[] bucket = new byte[17];
                 bucket[0] = (byte)assetType;
-                byte[] objBytes = objId.GetBytes();
+                byte[] objBytes = agentItem.ID.GetBytes();
                 Array.Copy(objBytes, 0, bucket, 1, 16);
 
-                Console.WriteLine("Giving inventory");
                 GridInstantMessage msg = new GridInstantMessage(World,
                         m_host.UUID, m_host.Name+", an object owned by "+
                         resolveName(m_host.OwnerID)+",", destId,

OpenSim/Services/Interfaces/IInventoryService.cs
@@ -141,8 +141,11 @@ namespace OpenSim.Services.Interfaces
         /// <summary>
         /// Add a new item to the user's inventory
         /// </summary>
-        /// <param name="item"></param>
-        /// <returns>true if the item was successfully added</returns>
+        /// <param name="item">
+        /// The item to be added.  If item.FolderID == UUID.Zero then the item is added to the most suitable system
+        /// folder.  If there is no suitable folder then the item is added to the user's root inventory folder.
+        /// </param>
+        /// <returns>true if the item was successfully added, false if it was not</returns>
         bool AddItem(InventoryItemBase item);
 
         /// <summary>

OpenSim/Services/InventoryService/XInventoryService.cs
@@ -219,12 +219,21 @@ namespace OpenSim.Services.InventoryService
 
         public virtual InventoryFolderBase GetFolderForType(UUID principalID, AssetType type)
         {
+//            m_log.DebugFormat("[XINVENTORY SERVICE]: Getting folder type {0} for user {1}", type, principalID);
+            
             XInventoryFolder[] folders = m_Database.GetFolders(
                     new string[] { "agentID", "type"},
                     new string[] { principalID.ToString(), ((int)type).ToString() });
 
             if (folders.Length == 0)
+            {
+//                m_log.WarnFormat("[XINVENTORY SERVICE]: Found no folder for type {0} for user {1}", type, principalID);
                 return null;
+            }
+            
+//            m_log.DebugFormat(
+//                "[XINVENTORY SERVICE]: Found folder {0} {1} for type {2} for user {3}", 
+//                folders[0].folderName, folders[0].folderID, type, principalID);
 
             return ConvertToOpenSim(folders[0]);
         }
@@ -235,7 +244,7 @@ namespace OpenSim.Services.InventoryService
             // connector. So we disregard the principal and look
             // by ID.
             //
-            m_log.DebugFormat("[XINVENTORY]: Fetch contents for folder {0}", folderID.ToString());
+            m_log.DebugFormat("[XINVENTORY SERVICE]: Fetch contents for folder {0}", folderID.ToString());
             InventoryCollection inventory = new InventoryCollection();
             inventory.UserID = principalID;
             inventory.Folders = new List<InventoryFolderBase>();
@@ -354,6 +363,9 @@ namespace OpenSim.Services.InventoryService
 
         public virtual bool AddItem(InventoryItemBase item)
         {
+//            m_log.DebugFormat(
+//                "[XINVENTORY SERVICE]: Adding item {0} to folder {1} for {2}", item.ID, item.Folder, item.Owner);
+            
             return m_Database.StoreItem(ConvertFromOpenSim(item));
         }
 