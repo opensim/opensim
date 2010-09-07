0bebe94231c3b5aeaa24a0eeb38272a08b73473b
John Hurliman <john.hurliman@intel.com>
2010-09-07 15:45:33 -0700

    Rolling back the recent libomv update but keeping the ExpiringCache cleanups (as much as is possible). There was a report of non-finite avatar positions after the update

--- OpenSim/Services/Connectors/SimianGrid/SimianUserAccountServiceConnector.cs
+++ OpenSim/Services/Connectors/SimianGrid/SimianUserAccountServiceConnector.cs
@@ -148,7 +148,7 @@ namespace OpenSim.Services.Connectors.SimianGrid
             if (account == null)
             {
                 // Store null responses too, to avoid repeated lookups for missing accounts
-                m_accountCache.AddOrUpdate(userID, null, CACHE_EXPIRATION_SECONDS);
+                m_accountCache.AddOrUpdate(userID, null, DateTime.Now + TimeSpan.FromSeconds(CACHE_EXPIRATION_SECONDS));
             }
 
             return account;
@@ -226,7 +226,7 @@ namespace OpenSim.Services.Connectors.SimianGrid
                 if (success)
                 {
                     // Cache the user account info
-                    m_accountCache.AddOrUpdate(data.PrincipalID, data, CACHE_EXPIRATION_SECONDS);
+                    m_accountCache.AddOrUpdate(data.PrincipalID, data, DateTime.Now + TimeSpan.FromSeconds(CACHE_EXPIRATION_SECONDS));
                 }
                 else
                 {
@@ -291,7 +291,7 @@ namespace OpenSim.Services.Connectors.SimianGrid
             GetFirstLastName(response["Name"].AsString(), out account.FirstName, out account.LastName);
 
             // Cache the user account info
-            m_accountCache.AddOrUpdate(account.PrincipalID, account, CACHE_EXPIRATION_SECONDS);
+            m_accountCache.AddOrUpdate(account.PrincipalID, account, DateTime.Now + TimeSpan.FromSeconds(CACHE_EXPIRATION_SECONDS));
 
             return account;
         }
