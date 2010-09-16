opensim/opensim

commit f7b28dd32155eac2d55ccf0757b059794d6b5f67

If a scene object part UUID is changed (only possible when not in a scene), then adjust the inventory items to point to the new uuid as well

OpenSim/Region/Framework/Scenes/SceneObjectPart.cs
@@ -494,7 +494,14 @@ namespace OpenSim.Region.Framework.Scenes
         public UUID UUID
         {
             get { return m_uuid; }
-            set { m_uuid = value; }
+            set 
+            { 
+                m_uuid = value; 
+                
+                // This is necessary so that TaskInventoryItem parent ids correctly reference the new uuid of this part
+                if (Inventory != null)
+                    Inventory.ResetInventoryIDs();
+            }
         }
 
         public uint LocalId
@@ -2756,7 +2763,6 @@ namespace OpenSim.Region.Framework.Scenes
             UUID = UUID.Random();
             LinkNum = linkNum;
             LocalId = 0;
-            Inventory.ResetInventoryIDs();
         }
 
         /// <summary>

OpenSim/Region/Framework/Scenes/SceneObjectPartInventory.cs
@@ -119,6 +119,9 @@ namespace OpenSim.Region.Framework.Scenes
         /// <param name="linkNum">Link number for the part</param>
         public void ResetInventoryIDs()
         {
+            if (null == m_part || null == m_part.ParentGroup)
+                return;
+            
             lock (m_items)
             {
                 if (0 == m_items.Count)

OpenSim/Framework/TaskInventoryItem.cs
@@ -348,15 +348,15 @@ namespace OpenSim.Framework
         /// <param name="partID">The new part ID to which this item belongs</param>
         public void ResetIDs(UUID partID)
         {
-            _oldID  = _itemID;
-            _itemID = UUID.Random();
-            _parentPartID = partID;
-            _parentID = partID;
+            OldItemID = ItemID;
+            ItemID = UUID.Random();
+            ParentPartID = partID;
+            ParentID = partID;
         }
 
         public TaskInventoryItem()
         {
-            _creationDate = (uint)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+            CreationDate = (uint)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
         }
     }
 }

OpenSim/Region/CoreModules/World/Archiver/Tests/ArchiverTests.cs
@@ -215,7 +215,7 @@ namespace OpenSim.Region.CoreModules.World.Archiver.Tests
         public void TestLoadOarV0_2()
         {
             TestHelper.InMethod();
-            //log4net.Config.XmlConfigurator.Configure();
+//            log4net.Config.XmlConfigurator.Configure();
 
             MemoryStream archiveWriteStream = new MemoryStream();
             TarArchiveWriter tar = new TarArchiveWriter(archiveWriteStream);

commit a0c87b5af6bba104fe50de3006b5dd515a8eb833

JustinCC is evil. f7b28dd3 broke script persistence. This fixes it.

OpenSim/Region/Framework/Interfaces/IEntityInventory.cs
@@ -58,6 +58,15 @@ namespace OpenSim.Region.Framework.Interfaces
         void ResetInventoryIDs();
 
         /// <summary>
+        /// Reset parent object UUID for all the items in the prim's inventory.
+        /// </summary>
+        /// 
+        /// If this method is called and there are inventory items, then we regard the inventory as having changed.
+        /// 
+        /// <param name="linkNum">Link number for the part</param>
+        void ResetObjectID();
+
+        /// <summary>
         /// Change every item in this inventory to a new owner.
         /// </summary>
         /// <param name="ownerId"></param>

OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
@@ -696,6 +696,7 @@ namespace OpenSim.Region.Framework.Scenes
                     if (node.Attributes["UUID"] != null)
                     {
                         UUID itemid = new UUID(node.Attributes["UUID"].Value);
+						m_log.DebugFormat("[SCRIPT STATE]: Adding state for oldID {0}", itemid);
                         m_savedScriptState.Add(itemid, node.InnerXml);
                     }
                 } 

OpenSim/Region/Framework/Scenes/SceneObjectPart.cs
@@ -514,7 +514,7 @@ namespace OpenSim.Region.Framework.Scenes
                 
                 // This is necessary so that TaskInventoryItem parent ids correctly reference the new uuid of this part
                 if (Inventory != null)
-                    Inventory.ResetInventoryIDs();
+                    Inventory.ResetObjectID();
             }
         }
 
@@ -2800,6 +2800,7 @@ namespace OpenSim.Region.Framework.Scenes
             UUID = UUID.Random();
             LinkNum = linkNum;
             LocalId = 0;
+			Inventory.ResetInventoryIDs();
         }
 
         /// <summary>

OpenSim/Region/Framework/Scenes/SceneObjectPartInventory.cs
@@ -148,6 +148,34 @@ namespace OpenSim.Region.Framework.Scenes
             m_items.LockItemsForWrite(false);
         }
 
+        public void ResetObjectID()
+        {
+            m_items.LockItemsForWrite(true);
+
+            if (Items.Count == 0)
+            {
+                m_items.LockItemsForWrite(false);
+                return;
+            }
+
+            HasInventoryChanged = true;
+            if (m_part.ParentGroup != null)
+            {
+                m_part.ParentGroup.HasGroupChanged = true;
+            }
+            
+            IList<TaskInventoryItem> items = new List<TaskInventoryItem>(Items.Values);
+            Items.Clear();
+
+            foreach (TaskInventoryItem item in items)
+            {
+                item.ParentPartID = m_part.UUID;
+                item.ParentID = m_part.UUID;
+                Items.Add(item.ItemID, item);
+            }
+            m_items.LockItemsForWrite(false);
+        }
+
         /// <summary>
         /// Change every item in this inventory to a new owner.
         /// </summary>