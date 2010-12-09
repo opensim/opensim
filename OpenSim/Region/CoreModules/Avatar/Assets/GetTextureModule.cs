90bcfefe093889914523743ff10388c0b39fa7d9
Diva Canto <diva@metaverseink.com>
2010-12-08 19:37:35 -0800

    Fixed previous compile error -- I haven't pushed the CreatorID in AssetBase yet...

--- OpenSim/Region/CoreModules/Avatar/Assets/GetTextureModule.cs
+++ OpenSim/Region/CoreModules/Avatar/Assets/GetTextureModule.cs
@@ -226,7 +226,7 @@ namespace OpenSim.Region.CoreModules.Avatar.ObjectCaps
                         }
                         else
                         {
-                            AssetBase newTexture = new AssetBase(texture.ID + "-" + format, texture.Name, (sbyte)AssetType.Texture, texture.CreatorID);
+                            AssetBase newTexture = new AssetBase(texture.ID + "-" + format, texture.Name, (sbyte)AssetType.Texture, texture.Metadata.CreatorID);
                             newTexture.Data = ConvertTextureData(texture, format);
                             if (newTexture.Data.Length == 0)
                                 return false; // !!! Caller try another codec, please!
