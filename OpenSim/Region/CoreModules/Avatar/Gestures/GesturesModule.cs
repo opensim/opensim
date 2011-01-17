31144a62b34cf84cbc2c5508924294334fb76979
Melanie <melanie@t-data.com>
2011-01-17 21:22:32 +0100

    Change gesture activation to not quash any other flags

--- OpenSim/Region/CoreModules/Avatar/Gestures/GesturesModule.cs
+++ OpenSim/Region/CoreModules/Avatar/Gestures/GesturesModule.cs
@@ -69,7 +69,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Gestures
             item = invService.GetItem(item);
             if (item != null)
             {
-                item.Flags = 1;
+                item.Flags |= 1;
                 invService.UpdateItem(item);
             }
             else 
@@ -85,7 +85,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Gestures
             item = invService.GetItem(item);
             if (item != null)
             {
-                item.Flags = 0;
+                item.Flags &= ~1;
                 invService.UpdateItem(item);
             }
             else 
