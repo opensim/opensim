8eb2789ae1d1f94ea9dfe4bf25b1c69065f0f960
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2011-01-26 21:19:22 +0000

    Add some comments on ILandObject.StartPoint and EndPoint

--- OpenSim/Region/Framework/Interfaces/ILandObject.cs
+++ OpenSim/Region/Framework/Interfaces/ILandObject.cs
@@ -43,7 +43,17 @@ namespace OpenSim.Region.Framework.Interfaces
         LandData LandData { get; set; }
         bool[,] LandBitmap { get; set; }
         UUID RegionUUID { get; }
+        
+        /// <summary>
+        /// The start point for the land object.  This is the western-most point as one scans land working from 
+        /// north to south.
+        /// </summary>
         Vector3 StartPoint { get; }
+        
+        /// <summary>
+        /// The end point for the land object.  This is the eastern-most point as one scans land working from 
+        /// south to north.
+        /// </summary>        
         Vector3 EndPoint { get; }
         
         bool ContainsPoint(int x, int y);
