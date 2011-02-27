cea47491de6723230a49b1ac99cecc5c32758f40
Melanie <melanie@t-data.com>
2011-02-27 18:55:17 +0100

    Fix a few little things

--- OpenSim/Region/CoreModules/Framework/InventoryAccess/InventoryAccessModule.cs
+++ OpenSim/Region/CoreModules/Framework/InventoryAccess/InventoryAccessModule.cs
@@ -148,6 +148,9 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
             InventoryItemBase item = new InventoryItemBase(itemID, remoteClient.AgentId);
             item = m_Scene.InventoryService.GetItem(item);
 
+            if (item.Owner != remoteClient.AgentId)
+                return UUID.Zero;
+
             if (item != null)
             {
                 if ((InventoryType)item.InvType == InventoryType.Notecard)
