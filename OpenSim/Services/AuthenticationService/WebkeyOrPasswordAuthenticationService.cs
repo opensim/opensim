3bc859a834c50634281df3d52166fc68d7c3c3ba
SignpostMarv Martin <me@signpostmarv.name>
2011-03-21 15:42:57 +0000

    Making combined auth service re-use the existing auth services instead of duplicating code
    Signed-off-by: SignpostMarv Martin <me@signpostmarv.name>

--- OpenSim/Services/AuthenticationService/WebkeyOrPasswordAuthenticationService.cs
+++ OpenSim/Services/AuthenticationService/WebkeyOrPasswordAuthenticationService.cs
@@ -8,58 +8,56 @@ using System.Reflection;
 using OpenSim.Data;
 using OpenSim.Framework;
 using OpenSim.Framework.Console;
+using OpenSim.Server.Base;
 
 namespace OpenSim.Services.AuthenticationService
 {
     public class WebkeyOrPasswordAuthenticationService : AuthenticationServiceBase, IAuthenticationService
     {
         private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private IConfigSource config;
         public WebkeyOrPasswordAuthenticationService(IConfigSource config)
             : base(config)
         {
+            this.config = config;
         }
 
         public string Authenticate(UUID principalID, string password, int lifetime)
         {
             AuthenticationData data = m_Database.Get(principalID);
+            IAuthenticationService svc;
+            Object[] args = new Object[] { config };
+            string result = String.Empty;
             if (data != null && data.Data != null)
             {
                 if (data.Data.ContainsKey("webLoginKey"))
                 {
-                    m_log.InfoFormat("[Authenticate]: Trying a web key authentication");
-                    if (new UUID(password) == UUID.Zero)
+                    svc = ServerUtils.LoadPlugin<IAuthenticationService>("OpenSim.Services.AuthenticationService.dll", "WebkeyAuthenticationService", args);
+                    result = svc.Authenticate(principalID, password, lifetime);
+                    if (result == String.Empty)
                     {
-                        m_log.InfoFormat("[Authenticate]: NULL_KEY is not a valid web_login_key");
-                    }
-                    else
-                    {
-                        string key = data.Data["webLoginKey"].ToString();
-                        m_log.DebugFormat("[WEB LOGIN AUTH]: got {0} for key in db vs {1}", key, password);
-                        if (key == password)
-                        {
-                            data.Data["webLoginKey"] = UUID.Zero.ToString();
-                            m_Database.Store(data);
-                            return GetToken(principalID, lifetime);
-                        }
+                        m_log.DebugFormat("[Authenticate]: Web Login failed for PrincipalID {0}", principalID);
                     }
                 }
                 if (data.Data.ContainsKey("passwordHash") && data.Data.ContainsKey("passwordSalt"))
                 {
-                    m_log.InfoFormat("[Authenticate]: Trying a password authentication");
-                    string hashed = Util.Md5Hash(password + ":" + data.Data["passwordSalt"].ToString());
-                    m_log.DebugFormat("[PASS AUTH]: got {0}; hashed = {1}; stored = {2}", password, hashed, data.Data["passwordHash"].ToString());
-                    if (data.Data["passwordHash"].ToString() == hashed)
+                    svc = ServerUtils.LoadPlugin<IAuthenticationService>("OpenSim.Services.AuthenticationService.dll", "PasswordAuthenticationService", args);
+                    result = svc.Authenticate(principalID, password, lifetime);
+                    if (result == String.Empty)
                     {
-                        return GetToken(principalID, lifetime);
+                        m_log.DebugFormat("[Authenticate]: Password login failed for PrincipalID {0}", principalID);
                     }
                 }
-                m_log.DebugFormat("[AUTH SERVICE]: Both password and webLoginKey-based login failed for PrincipalID {0}", principalID);
+                if (result == string.Empty)
+                {
+                    m_log.DebugFormat("[AUTH SERVICE]: Both password and webLoginKey-based login failed for PrincipalID {0}", principalID);
+                }
             }
             else
             {
                 m_log.DebugFormat("[AUTH SERVICE]: PrincipalID {0} or its data not found", principalID);
             }
-            return string.Empty;
+            return result;
         }
     }
 }
