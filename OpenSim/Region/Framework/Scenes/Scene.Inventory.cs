opensim/opensim

commit 3ba5eeb6c3b7c6381ca1e0ed87bce44049f96e37

Allow a null rotation to be passed in to RezObject so that we can control whether to use the serialized rotation or not.  Not used yet.

OpenSim/Region/Framework/Scenes/Scene.Inventory.cs
@@ -2016,14 +2016,14 @@ namespace OpenSim.Region.Framework.Scenes
         /// </summary>
         /// <param name="sourcePart"></param>
         /// <param name="item"></param>
-        /// <param name="pos"></param>
-        /// <param name="rot"></param>
-        /// <param name="vel"></param>
+        /// <param name="pos">The position of the rezzed object.</param>
+        /// <param name="rot">The rotation of the rezzed object.  If null, then the rotation stored with the object
+        /// will be used if it exists.</param>
+        /// <param name="vel">The velocity of the rezzed object.</param>
         /// <param name="param"></param>
         /// <returns>The SceneObjectGroup rezzed or null if rez was unsuccessful</returns>
         public virtual SceneObjectGroup RezObject(
-            SceneObjectPart sourcePart, TaskInventoryItem item,
-            Vector3 pos, Quaternion rot, Vector3 vel, int param)
+            SceneObjectPart sourcePart, TaskInventoryItem item, Vector3 pos, Quaternion? rot, Vector3 vel, int param)
         {
             if (null == item)
                 return null;
@@ -2041,8 +2041,14 @@ namespace OpenSim.Region.Framework.Scenes
                 if ((item.CurrentPermissions & (uint)PermissionMask.Copy) == 0)
                     sourcePart.Inventory.RemoveInventoryItem(item.ItemID);
             }
-                        
-            AddNewSceneObject(group, true, pos, rot, vel);
+                                    
+            AddNewSceneObject(group, true);
+            
+            group.AbsolutePosition = pos;
+            group.Velocity = vel;            
+            
+            if (rot != null)
+                group.UpdateGroupRotationR((Quaternion)rot);
             
             // We can only call this after adding the scene object, since the scene object references the scene
             // to find out if scripts should be activated at all.

commit 50aa93137d052c643a6ed44c32f0b4c5b32da79e

Fix a bug where physical objects rezzed with an initial velocity by script do not receive this velocity.
This is a minimal fix for the 0.7.1 release, pending a non copy/paste solution.
This hopefully addresses http://opensimulator.org/mantis/view.php?id=5457
The bug was introduced in commit 3ba5eeb

OpenSim/Region/Framework/Scenes/Scene.Inventory.cs
@@ -2039,6 +2039,12 @@ namespace OpenSim.Region.Framework.Scenes
             if (rot != null)
                 group.UpdateGroupRotationR((Quaternion)rot);
             
+            // TODO: This needs to be refactored with the similar code in 
+            // SceneGraph.AddNewSceneObject(SceneObjectGroup sceneObject, bool attachToBackup, Vector3 pos, Quaternion rot, Vector3 vel)
+            // possibly by allowing this method to take a null rotation.
+            if (group.RootPart.PhysActor != null && group.RootPart.PhysActor.IsPhysical && vel != Vector3.Zero)
+                group.RootPart.ApplyImpulse((vel * group.GetMass()), false);
+            
             // We can only call this after adding the scene object, since the scene object references the scene
             // to find out if scripts should be activated at all.
             group.CreateScriptInstances(param, true, DefaultScriptEngine, 3);