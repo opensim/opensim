accb4499fe61283040aed771cb4af38f9f5add6a
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2011-05-06 00:18:29 +0100

    change release flavour to post fixes

--- OpenSim/Framework/Servers/VersionInfo.cs
+++ OpenSim/Framework/Servers/VersionInfo.cs
@@ -30,7 +30,7 @@ namespace OpenSim
     public class VersionInfo
     {
         private const string VERSION_NUMBER = "0.7.1";
-        private const Flavour VERSION_FLAVOUR = Flavour.Release;
+        private const Flavour VERSION_FLAVOUR = Flavour.Post_Fixes;
 
         public enum Flavour
         {
