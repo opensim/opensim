f8f99f30cfb0cdd8fc0fafb73cef02d935ff464b
Diva Canto <diva@metaverseink.com>
2011-05-08 22:50:04 -0700

    The map is seriously broken. This doesn't fix it, but at least provides one more piece of data that seems to be required -- agent flags, which seem to be different in Viewer 2. WARNING: changes IClientAPI.

--- OpenSim/Region/ClientStack/LindenUDP/LLClientView.cs
+++ OpenSim/Region/ClientStack/LindenUDP/LLClientView.cs
@@ -8172,13 +8172,12 @@ namespace OpenSim.Region.ClientStack.LindenUDP
                     return true;
             }
             #endregion
-
             string mapName = Util.UTF8.GetString(map.NameData.Name, 0,
                                                      map.NameData.Name.Length - 1);
             RequestMapName handlerMapNameRequest = OnMapNameRequest;
             if (handlerMapNameRequest != null)
             {
-                handlerMapNameRequest(this, mapName);
+                handlerMapNameRequest(this, mapName, map.AgentData.Flags);
             }
             return true;
         }
