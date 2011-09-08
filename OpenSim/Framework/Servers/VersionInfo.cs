79f3de6ba8722b6b589da8e9034765b3d40395d9
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2011-09-08 22:16:42 +0100

    change version to rc1

--- OpenSim/Framework/Servers/VersionInfo.cs
+++ OpenSim/Framework/Servers/VersionInfo.cs
@@ -30,7 +30,7 @@ namespace OpenSim
     public class VersionInfo
     {
         private const string VERSION_NUMBER = "0.7.2";
-        private const Flavour VERSION_FLAVOUR = Flavour.Dev;
+        private const Flavour VERSION_FLAVOUR = Flavour.RC1;
 
         public enum Flavour
         {
