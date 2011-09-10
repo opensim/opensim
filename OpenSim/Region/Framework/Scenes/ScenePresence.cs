728fd0b1b8e1c80f6961ec06efb34727645fdc3e
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2011-09-10 01:09:17 +0100

    lock attachments when enumerating through them in ScenePresence.CopyTo().
    May have some effect on http://opensimulator.org/mantis/view.php?id=5644

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -3141,26 +3141,30 @@ namespace OpenSim.Region.Framework.Scenes
             catch { }
 
             // Attachment objects
-            if (m_attachments != null && m_attachments.Count > 0)
+            lock (m_attachments)
             {
-                cAgent.AttachmentObjects = new List<ISceneObject>();
-                cAgent.AttachmentObjectStates = new List<string>();
-//                IScriptModule se = m_scene.RequestModuleInterface<IScriptModule>();
-                m_InTransitScriptStates.Clear();
-                foreach (SceneObjectGroup sog in m_attachments)
+                if (m_attachments.Count > 0)
                 {
-                    // We need to make a copy and pass that copy
-                    // because of transfers withn the same sim
-                    ISceneObject clone = sog.CloneForNewScene();
-                    // Attachment module assumes that GroupPosition holds the offsets...!
-                    ((SceneObjectGroup)clone).RootPart.GroupPosition = sog.RootPart.AttachedPos;
-                    ((SceneObjectGroup)clone).IsAttachment = false;
-                    cAgent.AttachmentObjects.Add(clone);
-                    string state = sog.GetStateSnapshot();
-                    cAgent.AttachmentObjectStates.Add(state);
-                    m_InTransitScriptStates.Add(state);
-                    // Let's remove the scripts of the original object here
-                    sog.RemoveScriptInstances(true);
+                    cAgent.AttachmentObjects = new List<ISceneObject>();
+                    cAgent.AttachmentObjectStates = new List<string>();
+    //                IScriptModule se = m_scene.RequestModuleInterface<IScriptModule>();
+                    m_InTransitScriptStates.Clear();
+
+                    foreach (SceneObjectGroup sog in m_attachments)
+                    {
+                        // We need to make a copy and pass that copy
+                        // because of transfers withn the same sim
+                        ISceneObject clone = sog.CloneForNewScene();
+                        // Attachment module assumes that GroupPosition holds the offsets...!
+                        ((SceneObjectGroup)clone).RootPart.GroupPosition = sog.RootPart.AttachedPos;
+                        ((SceneObjectGroup)clone).IsAttachment = false;
+                        cAgent.AttachmentObjects.Add(clone);
+                        string state = sog.GetStateSnapshot();
+                        cAgent.AttachmentObjectStates.Add(state);
+                        m_InTransitScriptStates.Add(state);
+                        // Let's remove the scripts of the original object here
+                        sog.RemoveScriptInstances(true);
+                    }
                 }
             }
         }
