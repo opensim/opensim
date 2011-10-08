28f549bd64eb423ae43a5c23c1da5094e6269150
Pixel Tomsen <pixel.tomsen@gridnet.info>
2011-10-08 07:38:04 +0200

    osSetParcelDetails - PARCEL_DETAILS_CLAIMDATE
    this new flag set parcels claim date
    value 0 for this flag set current unixstamp
    
    http://opensimulator.org/mantis/view.php?id=5725

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/OSSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/OSSL_Api.cs
@@ -1356,27 +1356,34 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 string arg = rules.GetLSLStringItem(idx++);
                 switch (code)
                 {
-                    case 0:
-                      newLand.Name = arg;
-                      break;
+                    case ScriptBaseClass.PARCEL_DETAILS_NAME:
+                        newLand.Name = arg;
+                        break;
 
-                    case 1:
-                      newLand.Description = arg;
-                      break;
+                    case ScriptBaseClass.PARCEL_DETAILS_DESC:
+                        newLand.Description = arg;
+                        break;
 
-                    case 2:
-                      CheckThreatLevel(ThreatLevel.VeryHigh, functionName);
-                      if (UUID.TryParse(arg , out uuid))
-                          newLand.OwnerID = uuid;
-                      break;
+                    case ScriptBaseClass.PARCEL_DETAILS_OWNER:
+                        CheckThreatLevel(ThreatLevel.VeryHigh, functionName);
+                        if (UUID.TryParse(arg, out uuid))
+                            newLand.OwnerID = uuid;
+                        break;
 
-                    case 3:
-                      CheckThreatLevel(ThreatLevel.VeryHigh, functionName);
-                      if (UUID.TryParse(arg , out uuid))
-                          newLand.GroupID = uuid;
-                      break;
-                }
-            }
+                    case ScriptBaseClass.PARCEL_DETAILS_GROUP:
+                        CheckThreatLevel(ThreatLevel.VeryHigh, functionName);
+                        if (UUID.TryParse(arg, out uuid))
+                            newLand.GroupID = uuid;
+                        break;
+
+                    case ScriptBaseClass.PARCEL_DETAILS_CLAIMDATE:
+                        CheckThreatLevel(ThreatLevel.VeryHigh, functionName);
+                        newLand.ClaimDate = Convert.ToInt32(arg);
+                        if (newLand.ClaimDate == 0)
+                            newLand.ClaimDate = Util.UnixTimeSinceEpoch();
+                        break;
+                 }
+             }
 
             World.LandChannel.UpdateLandObject(newLand.LocalID,newLand);
         }
