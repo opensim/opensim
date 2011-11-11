dc200d7bb5a7246d56288d6437ee653ee9839800
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2011-11-11 19:38:36 +0000

    Add new ScenePresenceSitTests with a single sit/stand test

---
+++ OpenSim/Region/Framework/Scenes/Tests/ScenePresenceSitTests.cs
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Reflection;
+using Nini.Config;
+using NUnit.Framework;
+using OpenMetaverse;
+using OpenSim.Framework;
+using OpenSim.Framework.Communications;
+using OpenSim.Framework.Servers;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.CoreModules.ServiceConnectorsOut.Simulation;
+using OpenSim.Tests.Common;
+using OpenSim.Tests.Common.Mock;
+using System.Threading;
+
+namespace OpenSim.Region.Framework.Scenes.Tests
+{
+    [TestFixture]
+    public class ScenePresenceSitTests
+    {
+        private TestScene m_scene;
+//        private AvatarFactoryModule afm;
+//        private UserManagementModule umm;
+//        private AttachmentsModule am;
+
+        [SetUp]
+        public void Init()
+        {
+//            IConfigSource config = new IniConfigSource();
+//            config.AddConfig("NPC");
+//            config.Configs["NPC"].Set("Enabled", "true");
+//            config.AddConfig("Modules");
+//            config.Configs["Modules"].Set("InventoryAccessModule", "BasicInventoryAccessModule");
+//
+//            afm = new AvatarFactoryModule();
+//            umm = new UserManagementModule();
+//            am = new AttachmentsModule();
+
+            m_scene = SceneHelpers.SetupScene();
+//            SceneHelpers.SetupSceneModules(scene, config, afm, umm, am, new BasicInventoryAccessModule(), new NPCModule());
+        }
+
+        [Test]
+        public void TestSitAndStandWithNoSitTarget()
+        {
+            TestHelpers.InMethod();
+//            log4net.Config.XmlConfigurator.Configure();
+
+            ScenePresence sp = SceneHelpers.AddScenePresence(m_scene, TestHelpers.ParseTail(0x1));
+
+            // FIXME: To get this to work for now, we are going to place the npc right next to the target so that
+            // the autopilot doesn't trigger
+            Vector3 startPos = new Vector3(1, 1, 1);
+            sp.AbsolutePosition = startPos;
+
+            SceneObjectPart part = SceneHelpers.AddSceneObject(m_scene);
+
+            sp.HandleAgentRequestSit(sp.ControllingClient, sp.UUID, part.UUID, Vector3.Zero);
+
+            Assert.That(part.SitTargetAvatar, Is.EqualTo(UUID.Zero));
+            Assert.That(sp.ParentID, Is.EqualTo(part.LocalId));
+
+            // FIXME: This is different for live avatars - z position is adjusted.  This is half the height of the
+            // default avatar.
+            // Curiously, Vector3.ToString() will not display the last two places of the float.  For example,
+            // printing out npc.AbsolutePosition will give <0, 0, 0.8454993> not <0, 0, 0.845499337>
+            Assert.That(
+                sp.AbsolutePosition,
+                Is.EqualTo(part.AbsolutePosition + new Vector3(0, 0, 0.845499337f)));
+
+            sp.StandUp();
+
+            Assert.That(part.SitTargetAvatar, Is.EqualTo(UUID.Zero));
+            Assert.That(sp.ParentID, Is.EqualTo(0));
+        }
+    }
+}
