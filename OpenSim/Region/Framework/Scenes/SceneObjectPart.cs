61adf36339f15fcbcc16d96ff7089f03edf72c10
Dan Lake <dan.lake@intel.com>
2012-02-02 18:19:22 -0800

    Commenting out new event until I can fix OpenSim tests. Currently, testing objects does not create a Scene or EventManager so triggering events crashes some tests

--- OpenSim/Region/Framework/Scenes/SceneObjectPart.cs
+++ OpenSim/Region/Framework/Scenes/SceneObjectPart.cs
@@ -2729,9 +2729,9 @@ namespace OpenSim.Region.Framework.Scenes
             if (ParentGroup == null)
                 return;
 
-            // When running OpenSim tests, EventManager can be null. Maybe tests should create an EventManager.
-            if(ParentGroup.Scene.EventManager != null)
-                ParentGroup.Scene.EventManager.TriggerSceneObjectPartUpdated(this);
+            // When running OpenSim tests, Scene (and EventManager can be null).
+            // Need to fix tests before we can trigger this here
+            // ParentGroup.Scene.EventManager.TriggerSceneObjectPartUpdated(this);
 
             ParentGroup.QueueForUpdateCheck();
 
@@ -2765,9 +2765,9 @@ namespace OpenSim.Region.Framework.Scenes
             if (ParentGroup == null)
                 return;
 
-            // When running OpenSim tests, EventManager can be null. Maybe tests should create an EventManager.
-            if (ParentGroup.Scene.EventManager != null)
-                ParentGroup.Scene.EventManager.TriggerSceneObjectPartUpdated(this);
+            // When running OpenSim tests, Scene (and EventManager can be null).
+            // Need to fix tests before we can trigger this here
+            // ParentGroup.Scene.EventManager.TriggerSceneObjectPartUpdated(this);
 
             // This was pulled from SceneViewer. Attachments always receive full updates.
             // I could not verify if this is a requirement but this maintains existing behavior
