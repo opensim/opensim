13999dfc1df67222ffe3481ae4e45ca273ed5460
BlueWall <jamesh@bluewallgroup.com>
2012-02-04 21:52:55 -0500

    Set PhysPrimMax default to 0 so ini files won't be overridden

--- OpenSim/Framework/RegionInfo.cs
+++ OpenSim/Framework/RegionInfo.cs
@@ -609,7 +609,7 @@ namespace OpenSim.Framework
             //
             m_nonphysPrimMax = config.GetInt("NonphysicalPrimMax", 0);
             allKeys.Remove("NonphysicalPrimMax");
-            m_physPrimMax = config.GetInt("PhysicalPrimMax", 10);
+            m_physPrimMax = config.GetInt("PhysicalPrimMax", 0);
             allKeys.Remove("PhysicalPrimMax");
             m_clampPrimSize = config.GetBoolean("ClampPrimSize", false);
             allKeys.Remove("ClampPrimSize");
