12c32396665d5cf72235b074a91b25a5d1770b53
UbitUmarov <ajlduarte@sapo.pt>
2012-02-08 15:29:48 +0000

    changes needed to compile UbitODE, also changes project name to "Careminter". untested on linux

--- runprebuild.bat
+++ runprebuild.bat
@@ -1,4 +1,4 @@
 bin\Prebuild.exe /target nant
 bin\Prebuild.exe /target vs2008
-echo C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild opensim.sln > compile.bat
+echo C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Careminster.sln > compile.bat
 
--- runprebuild2010.bat
+++ runprebuild2010.bat
@@ -1,2 +1,2 @@
 bin\Prebuild.exe /target vs2010
-echo C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild OpenSim.sln > compile.bat
+echo C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Careminster.sln > compile.bat
