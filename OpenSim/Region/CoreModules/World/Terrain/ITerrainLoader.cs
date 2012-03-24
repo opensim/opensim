cf61c74e90324e07cb4b15f9c597fef00c047c75
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2012-03-24 02:16:44 +0000

    Give feedback when "terrain save-tile" is not successfully invoked.

--- OpenSim/Region/CoreModules/World/Terrain/ITerrainLoader.cs
+++ OpenSim/Region/CoreModules/World/Terrain/ITerrainLoader.cs
@@ -38,6 +38,21 @@ namespace OpenSim.Region.CoreModules.World.Terrain
         ITerrainChannel LoadStream(Stream stream);
         void SaveFile(string filename, ITerrainChannel map);
         void SaveStream(Stream stream, ITerrainChannel map);
+
+        /// <summary>
+        /// Save a number of map tiles to a single big image file.
+        /// </summary>
+        /// <remarks>
+        /// If the image file already exists then the tiles saved will replace those already in the file - other tiles
+        /// will be untouched.
+        /// </remarks>
+        /// <param name="filename">The terrain file to save</param>
+        /// <param name="offsetX">The map x co-ordinate at which to begin the save.</param>
+        /// <param name="offsetY">The may y co-ordinate at which to begin the save.</param>
+        /// <param name="fileWidth">The number of tiles to save along the X axis.</param>
+        /// <param name="fileHeight">The number of tiles to save along the Y axis.</param>
+        /// <param name="regionSizeX">The width of a map tile.</param>
+        /// <param name="regionSizeY">The height of a map tile.</param>
         void SaveFile(ITerrainChannel map, string filename, int offsetX, int offsetY, int fileWidth, int fileHeight, int regionSizeX, int regionSizeY);
     }
 }
