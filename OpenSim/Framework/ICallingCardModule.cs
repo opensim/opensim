5a82d939e68edf698ab111941c1f6ddcb8c91907
Melanie <melanie@t-data.com>
2012-03-29 00:34:28 +0100

    Adding the Avination calling card interface

---
+++ OpenSim/Framework/ICallingCardModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+using OpenSim.Framework;
+
+namespace OpenSim.Framework
+{
+    public interface ICallingCardModule
+    {
+        UUID CreateCallingCard(UUID userID, UUID creatorID, UUID folderID);
+    }
+}
