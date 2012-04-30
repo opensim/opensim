opensim/opensim

commit 6473674bbf6ce006512083902e8ff1796d8c8b22

Fixed: custom walking animations didn't stop when the avatar stopped walking.
This happened because the scripts were notified about control changes (e.g., the user stopped pressing the Forward key) when the animation was still WALK, so the script didn't stop the walking animation. Fixing this required: a) Update the movement animation *before* notifying the script; b) Add locking to prevent clashes with the Heartbeat thread (which also updates the animations); c) Handle the case of a user who stops walking just as the avatar is in the air: the avatar should STAND in that case, not WALK.

This reverts commit feef1dd73243cfdd5322632fb67e64cabc1ad4bc.

OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (c) Contributors, http://opensimulator.org/
  * See CONTRIBUTORS.TXT for a full list of copyright holders.
  *
@@ -45,6 +45,7 @@ using TeleportFlags = OpenSim.Framework.Constants.TeleportFlags;
 
 namespace OpenSim.Region.Framework.Scenes
 {
+    [Flags]
     enum ScriptControlled : uint
     {
         CONTROL_ZERO = 0,
@@ -1220,7 +1221,7 @@ namespace OpenSim.Region.Framework.Scenes
         {
 //            m_log.DebugFormat(
 //                "[SCENE PRESENCE]: In {0} received agent update from {1}, flags {2}",
-//                Scene.RegionInfo.RegionName, remoteClient.Name, agentData.ControlFlags);
+//                Scene.RegionInfo.RegionName, remoteClient.Name, (AgentManager.ControlFlags)agentData.ControlFlags);
 
             if (IsChildAgent)
             {
@@ -1320,14 +1321,8 @@ namespace OpenSim.Region.Framework.Scenes
                 }
             }
 
-            lock (scriptedcontrols)
-            {
-                if (scriptedcontrols.Count > 0)
-                {
-                    SendControlToScripts((uint)flags);
-                    flags = RemoveIgnoredControls(flags, IgnoredControls);
-                }
-            }
+            uint flagsForScripts = (uint)flags;
+            flags = RemoveIgnoredControls(flags, IgnoredControls);
 
             if ((flags & AgentManager.ControlFlags.AGENT_CONTROL_SIT_ON_GROUND) != 0)
                 HandleAgentSitOnGround();
@@ -1420,7 +1415,7 @@ namespace OpenSim.Region.Framework.Scenes
                                     MovementFlag |= (byte)nudgehack;
                                 }
 
-//                                m_log.DebugFormat("[SCENE PRESENCE]: Updating MovementFlag for {0} with {1}", Name, DCF);
+                                //m_log.DebugFormat("[SCENE PRESENCE]: Updating MovementFlag for {0} with {1}", Name, DCF);
                                 MovementFlag += (byte)(uint)DCF;
                                 update_movementflag = true;
                             }
@@ -1433,7 +1428,7 @@ namespace OpenSim.Region.Framework.Scenes
                                 && ((MovementFlag & (byte)nudgehack) == nudgehack))
                                 ) // This or is for Nudge forward
                             {
-//                                m_log.DebugFormat("[SCENE PRESENCE]: Updating MovementFlag for {0} with lack of {1}", Name, DCF);
+                                //m_log.DebugFormat("[SCENE PRESENCE]: Updating MovementFlag for {0} with lack of {1}", Name, DCF);
                                 MovementFlag -= ((byte)(uint)DCF);
                                 update_movementflag = true;
 
@@ -1514,8 +1509,18 @@ namespace OpenSim.Region.Framework.Scenes
 //                    }
 //                }
 
-//                if (update_movementflag && ParentID == 0)
-//                    Animator.UpdateMovementAnimations();
+                if (update_movementflag && ParentID == 0)
+                    Animator.UpdateMovementAnimations();
+
+                lock (scriptedcontrols)
+                {
+                    if (scriptedcontrols.Count > 0)
+                    {
+                        // Notify the scripts only after calling UpdateMovementAnimations(), so that if a script
+                        // (e.g., a walking script) checks which animation is active it will be the correct animation.
+                        SendControlToScripts(flagsForScripts);
+                    }
+                }
             }
 
             m_scene.EventManager.TriggerOnClientMovement(this);

OpenSim/Region/Framework/Scenes/Animation/ScenePresenceAnimator.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (c) Contributors, http://opensimulator.org/
  * See CONTRIBUTORS.TXT for a full list of copyright holders.
  *
@@ -278,6 +278,10 @@ namespace OpenSim.Region.Framework.Scenes.Animation
                     return "FALLDOWN";
                 }
 
+                // Check if the user has stopped walking just now
+                if (CurrentMovementAnimation == "WALK" && (move == Vector3.Zero))
+                    return "STAND";
+
                 return CurrentMovementAnimation;
             }
 
@@ -402,13 +406,16 @@ namespace OpenSim.Region.Framework.Scenes.Animation
         /// </summary>
         public void UpdateMovementAnimations()
         {
-            CurrentMovementAnimation = DetermineMovementAnimation();
+            lock (m_animations)
+            {
+                CurrentMovementAnimation = DetermineMovementAnimation();
 
-//            m_log.DebugFormat(
-//                "[SCENE PRESENCE ANIMATOR]: Determined animation {0} for {1} in UpdateMovementAnimations()",
-//                CurrentMovementAnimation, m_scenePresence.Name);
+//                m_log.DebugFormat(
+//                    "[SCENE PRESENCE ANIMATOR]: Determined animation {0} for {1} in UpdateMovementAnimations()",
+//                    CurrentMovementAnimation, m_scenePresence.Name);
 
-            TrySetMovementAnimation(CurrentMovementAnimation);
+                TrySetMovementAnimation(CurrentMovementAnimation);
+            }
         }
 
         public UUID[] GetAnimationArray()

commit fe8e835bfc027696979ff0fb3135e5b2718af9ed

Fixed: scripted controls didn't work if the avatar was sitting down
This fixes a bug introduced in 6473674bbf6ce006512083902e8ff1796d8c8b22

OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -1336,6 +1336,7 @@ namespace OpenSim.Region.Framework.Scenes
             PhysicsActor actor = PhysicsActor;
             if (actor == null)
             {
+                SafeSendControlsToScripts(flagsForScripts);
                 return;
             }
 
@@ -1512,18 +1513,23 @@ namespace OpenSim.Region.Framework.Scenes
                 if (update_movementflag && ParentID == 0)
                     Animator.UpdateMovementAnimations();
 
-                lock (scriptedcontrols)
-                {
-                    if (scriptedcontrols.Count > 0)
-                    {
-                        // Notify the scripts only after calling UpdateMovementAnimations(), so that if a script
-                        // (e.g., a walking script) checks which animation is active it will be the correct animation.
-                        SendControlToScripts(flagsForScripts);
-                    }
-                }
+                SafeSendControlsToScripts(flagsForScripts);
             }
 
             m_scene.EventManager.TriggerOnClientMovement(this);
+        }
+
+        private void SafeSendControlsToScripts(uint flagsForScripts)
+        {
+            lock (scriptedcontrols)
+            {
+                if (scriptedcontrols.Count > 0)
+                {
+                    // Notify the scripts only after calling UpdateMovementAnimations(), so that if a script
+                    // (e.g., a walking script) checks which animation is active it will be the correct animation.
+                    SendControlToScripts(flagsForScripts);
+                }
+            }
         }
 
         /// <summary>