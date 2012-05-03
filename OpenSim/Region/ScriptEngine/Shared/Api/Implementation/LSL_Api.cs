100e4ca67ea5f9eacc575ac65ce3ac7cd81eeb3d
Snoopy Pfeffer <snoopy.pfeffer@yahoo.com>
2012-05-03 19:00:09 +0200

    Fixes Mantis #5999. llSetLinkPrimitiveParams with PRIM_BUMP_SHINY did cause a runtime error.

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -7336,7 +7336,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                                 return;
                             face = (int)rules.GetLSLIntegerItem(idx++);
                             int shiny = (int)rules.GetLSLIntegerItem(idx++);
-                            Bumpiness bump = (Bumpiness)Convert.ToByte((int)rules.GetLSLIntegerItem(idx++));
+                            Bumpiness bump = (Bumpiness)(int)rules.GetLSLIntegerItem(idx++);
 
                             SetShiny(part, face, shiny, bump);
 
