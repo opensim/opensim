opensim/opensim

commit 67ebe80dd95ffa2c5e9b071497b472f9c99f00f8

Resolve some mono compiler warnings.

OpenSim/Region/CoreModules/World/Terrain/FileLoaders/Terragen.cs
@@ -65,7 +65,7 @@ namespace OpenSim.Region.CoreModules.World.Terrain.FileLoaders
             bool eof = false;
 
             int fileXPoints = 0;
-            int fileYPoints = 0;
+//            int fileYPoints = 0;
 
             // Terragen file
             while (eof == false)
@@ -75,7 +75,7 @@ namespace OpenSim.Region.CoreModules.World.Terrain.FileLoaders
                 {
                     case "SIZE":
                         fileXPoints = bs.ReadInt16() + 1;
-                        fileYPoints = fileXPoints;
+//                        fileYPoints = fileXPoints;
                         bs.ReadInt16();
                         break;
                     case "XPTS":
@@ -83,7 +83,8 @@ namespace OpenSim.Region.CoreModules.World.Terrain.FileLoaders
                         bs.ReadInt16();
                         break;
                     case "YPTS":
-                        fileYPoints = bs.ReadInt16();
+//                        fileYPoints = bs.ReadInt16();
+                        bs.ReadInt16();
                         bs.ReadInt16();
                         break;
                     case "ALTW":
@@ -164,10 +165,8 @@ namespace OpenSim.Region.CoreModules.World.Terrain.FileLoaders
             bool eof = false;
             if (Encoding.ASCII.GetString(bs.ReadBytes(16)) == "TERRAGENTERRAIN ")
             {
-
-                int fileWidth = w;
-                int fileHeight = h;
-
+//                int fileWidth = w;
+//                int fileHeight = h;
 
                 // Terragen file
                 while (eof == false)
@@ -176,17 +175,22 @@ namespace OpenSim.Region.CoreModules.World.Terrain.FileLoaders
                     switch (tmp)
                     {
                         case "SIZE":
-                            int sztmp = bs.ReadInt16() + 1;
-                            fileWidth = sztmp;
-                            fileHeight = sztmp;
+//                            int sztmp = bs.ReadInt16() + 1;
+//                            fileWidth = sztmp;
+//                            fileHeight = sztmp;
+                            bs.ReadInt16();
+                            bs.ReadInt16();
+                            bs.ReadInt16();
                             bs.ReadInt16();
                             break;
                         case "XPTS":
-                            fileWidth = bs.ReadInt16();
+//                            fileWidth = bs.ReadInt16();
+                            bs.ReadInt16();
                             bs.ReadInt16();
                             break;
                         case "YPTS":
-                            fileHeight = bs.ReadInt16();
+//                            fileHeight = bs.ReadInt16();
+                            bs.ReadInt16();
                             bs.ReadInt16();
                             break;
                         case "ALTW":

OpenSim/Region/CoreModules/Avatar/Attachments/AttachmentsModule.cs
@@ -48,7 +48,6 @@ namespace OpenSim.Region.CoreModules.Avatar.Attachments
         private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         
         private Scene m_scene;
-        private IDialogModule m_dialogModule;
         private IInventoryAccessModule m_invAccessModule;
 
         /// <summary>
@@ -89,7 +88,6 @@ namespace OpenSim.Region.CoreModules.Avatar.Attachments
         
         public void RegionLoaded(Scene scene)
         {
-            m_dialogModule = m_scene.RequestModuleInterface<IDialogModule>();
             m_invAccessModule = m_scene.RequestModuleInterface<IInventoryAccessModule>();
         }
         

OpenSim/Region/CoreModules/Avatar/Friends/HGFriendsModule.cs
@@ -415,12 +415,13 @@ namespace OpenSim.Region.CoreModules.Avatar.Friends
 
         protected override void StoreBackwards(UUID friendID, UUID agentID)
         {
-            Boolean agentIsLocal = true;
-            Boolean friendIsLocal = true;
+            bool agentIsLocal = true;
+//            bool friendIsLocal = true;
+
             if (UserManagementModule != null)
             {
                 agentIsLocal = UserManagementModule.IsLocalGridUser(agentID);
-                friendIsLocal = UserManagementModule.IsLocalGridUser(friendID);
+//                friendIsLocal = UserManagementModule.IsLocalGridUser(friendID);
             }
 
             // Is the requester a local user?
@@ -507,7 +508,11 @@ namespace OpenSim.Region.CoreModules.Avatar.Friends
                             {
                                 friendUUI = finfo.Friend;
                                 theFriendUUID = friendUUI;
-                                UUID utmp = UUID.Zero; String url = String.Empty; String first = String.Empty, last = String.Empty, tmp = String.Empty;
+                                UUID utmp = UUID.Zero;
+                                string url = String.Empty;
+                                string first = String.Empty;
+                                string last = String.Empty;
+
                                 // If it's confirming the friendship, we already have the full UUI with the secret
                                 if (Util.ParseUniversalUserIdentifier(theFriendUUID, out utmp, out url, out first, out last, out secret))
                                 {

OpenSim/Region/CoreModules/Framework/EntityTransfer/EntityTransferModule.cs
@@ -1240,7 +1240,7 @@ namespace OpenSim.Region.CoreModules.Framework.EntityTransfer
 
             return agent;
         }
-
+         
         private void CrossAgentToNewRegionCompleted(IAsyncResult iar)
         {
             CrossAgentToNewRegionDelegate icon = (CrossAgentToNewRegionDelegate)iar.AsyncState;

OpenSim/Region/CoreModules/Framework/EntityTransfer/HGEntityTransferModule.cs
@@ -336,7 +336,7 @@ namespace OpenSim.Region.CoreModules.Framework.EntityTransfer
 
                 // Let's find out if this is a foreign user or a local user
                 IUserManagement uMan = m_scene.RequestModuleInterface<IUserManagement>();
-                UserAccount account = m_scene.UserAccountService.GetUserAccount(m_scene.RegionInfo.ScopeID, obj.AgentId);
+//                UserAccount account = m_scene.UserAccountService.GetUserAccount(m_scene.RegionInfo.ScopeID, obj.AgentId);
                 if (uMan != null && uMan.IsLocalGridUser(obj.AgentId))
                 {
                     // local grid user

OpenSim/Region/CoreModules/Framework/InventoryAccess/HGInventoryAccessModule.cs
@@ -364,8 +364,7 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
                     {
                         m_log.DebugFormat("[HG INVENTORY ACCESS MODULE]: Changing root inventory for user {0}", client.Name);
                         InventoryCollection content = m_Scene.InventoryService.GetFolderContent(client.AgentId, root.ID);
-                        List<UUID> fids = new List<UUID>();
-                        List<UUID> iids = new List<UUID>();
+
                         List<InventoryFolderBase> keep = new List<InventoryFolderBase>();
 
                         foreach (InventoryFolderBase f in content.Folders)

OpenSim/Region/CoreModules/ServiceConnectorsIn/Hypergrid/HypergridServiceInConnectorModule.cs
@@ -122,7 +122,8 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsIn.Hypergrid
                 ISimulationService simService = scene.RequestModuleInterface<ISimulationService>();
                 IFriendsSimConnector friendsConn = scene.RequestModuleInterface<IFriendsSimConnector>();
                 Object[] args = new Object[] { m_Config };
-                IFriendsService friendsService = ServerUtils.LoadPlugin<IFriendsService>(m_LocalServiceDll, args);
+//                IFriendsService friendsService = ServerUtils.LoadPlugin<IFriendsService>(m_LocalServiceDll, args)
+                ServerUtils.LoadPlugin<IFriendsService>(m_LocalServiceDll, args);
 
                 m_HypergridHandler = new GatekeeperServiceInConnector(m_Config, MainServer.Instance, simService);
 

OpenSim/Region/CoreModules/ServiceConnectorsOut/Asset/HGAssetBroker.cs
@@ -149,7 +149,7 @@ namespace OpenSim.Region.CoreModules.ServiceConnectorsOut.Asset
             
             m_aScene = scene;
 
-            scene.RegisterModuleInterface<IAssetService>(this);
+            m_aScene.RegisterModuleInterface<IAssetService>(this);
         }
 
         public void RemoveRegion(Scene scene)

commit 6adc810eaaf067e25ac88b023a20543577a2d5fd

Stop accidentally reading 4 Int16s instead of 2 in SIZE section of Terragen file when loaded from a stream.
Fixes a bug introduced 2 weeks ago in 67ebe80
Thanks to Plugh for pointing this out.

OpenSim/Region/CoreModules/World/Terrain/FileLoaders/Terragen.cs
@@ -180,8 +180,6 @@ namespace OpenSim.Region.CoreModules.World.Terrain.FileLoaders
 //                            fileHeight = sztmp;
                             bs.ReadInt16();
                             bs.ReadInt16();
-                            bs.ReadInt16();
-                            bs.ReadInt16();
                             break;
                         case "XPTS":
 //                            fileWidth = bs.ReadInt16();