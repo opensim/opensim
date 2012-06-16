opensim/opensim

commit c6e375291a8fa3dbdcbd25cfb64bf0d536707fb0

Don't include time to transmit response back to requester when assessing slow handling of requests.
This is to avoid logging a 'slow' request when the source of delay is the viewer in processing a response.
This is not something we can do much about on the server end - it's server-side delay that we're interested in.
To ensure consistency, this commit also had to refactor and simplify inbound non-poll network request handling, though there should be no functional change.
IOSHttpResponse no longer exposes the Send() method, only classes in OpenSim.Framework.Servers.HttpServer should be doing this.
Only the GetTextureHandler was sending its own response.  Now it leaves this to BaseHttpServer, like all other core handlers.

OpenSim/Framework/Servers/HttpServer/PollServiceWorkerThread.cs
@@ -90,15 +90,16 @@ namespace OpenSim.Framework.Servers.HttpServer
                         }
 
                         Hashtable responsedata = req.PollServiceArgs.GetEvents(req.RequestID, req.PollServiceArgs.Id, str.ReadToEnd());
-                        m_server.DoHTTPGruntWork(responsedata,
-                                                 new OSHttpResponse(new HttpResponse(req.HttpContext, req.Request),req.HttpContext));
+                        DoHTTPGruntWork(m_server, req, responsedata);
                     }
                     else
                     {
                         if ((Environment.TickCount - req.RequestTime) > m_timeout)
                         {
-                            m_server.DoHTTPGruntWork(req.PollServiceArgs.NoEvents(req.RequestID, req.PollServiceArgs.Id),
-                                                     new OSHttpResponse(new HttpResponse(req.HttpContext, req.Request),req.HttpContext));
+                            DoHTTPGruntWork(
+                                m_server,
+                                req,
+                                req.PollServiceArgs.NoEvents(req.RequestID, req.PollServiceArgs.Id));
                         }
                         else
                         {
@@ -118,6 +119,48 @@ namespace OpenSim.Framework.Servers.HttpServer
         internal void Enqueue(PollServiceHttpRequest pPollServiceHttpRequest)
         {
             m_request.Enqueue(pPollServiceHttpRequest);
+        }
+
+        /// <summary>
+        /// FIXME: This should be part of BaseHttpServer
+        /// </summary>
+        internal static void DoHTTPGruntWork(BaseHttpServer server, PollServiceHttpRequest req, Hashtable responsedata)
+        {
+            OSHttpResponse response
+                = new OSHttpResponse(new HttpResponse(req.HttpContext, req.Request), req.HttpContext);
+
+            byte[] buffer
+                = server.DoHTTPGruntWork(
+                    responsedata, new OSHttpResponse(new HttpResponse(req.HttpContext, req.Request), req.HttpContext));
+
+            response.SendChunked = false;
+            response.ContentLength64 = buffer.Length;
+            response.ContentEncoding = Encoding.UTF8;
+
+            try
+            {
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                m_log.Warn(string.Format("[POLL SERVICE WORKER THREAD]: Error ", ex));
+            }
+            finally
+            {
+                //response.OutputStream.Close();
+                try
+                {
+                    response.OutputStream.Flush();
+                    response.Send();
+
+                    //if (!response.KeepAlive && response.ReuseContext)
+                    //    response.FreeContext();
+                }
+                catch (Exception e)
+                {
+                    m_log.Warn(String.Format("[POLL SERVICE WORKER THREAD]: Error ", e));
+                }
+            }
         }
     }
 }

OpenSim/ApplicationPlugins/Rest/Inventory/RestHandler.cs
@@ -539,7 +539,6 @@ namespace OpenSim.ApplicationPlugins.Rest.Inventory
         /// path has not already been registered, the method is added to the active
         /// handler table.
         /// </summary>
-
         public void AddStreamHandler(string httpMethod, string path, RestMethod method)
         {
             if (!IsEnabled)

OpenSim/Capabilities/Handlers/GetTexture/GetTextureHandler.cs
@@ -77,7 +77,6 @@ namespace OpenSim.Capabilities.Handlers
             {
                 m_log.Error("[GETTEXTURE]: Cannot fetch texture " + textureStr + " without an asset service");
                 httpResponse.StatusCode = (int)System.Net.HttpStatusCode.NotFound;
-                return null;
             }
 
             UUID textureID;
@@ -115,7 +114,6 @@ namespace OpenSim.Capabilities.Handlers
 //                "[GETTEXTURE]: For texture {0} sending back response {1}, data length {2}",
 //                textureID, httpResponse.StatusCode, httpResponse.ContentLength);
 
-            httpResponse.Send();
             return null;
         }
 

OpenSim/Framework/Servers/HttpServer/Interfaces/IOSHttpResponse.cs
@@ -128,11 +128,5 @@ namespace OpenSim.Framework.Servers.HttpServer
         /// <param name="value">string containing the header field
         /// value</param>
         void AddHeader(string key, string value);
-
-        /// <summary>
-        /// Send the response back to the remote client
-        /// </summary>
-        void Send();
     }
 }
-

OpenSim/Framework/Servers/HttpServer/OSHttpResponse.cs
@@ -321,13 +321,12 @@ namespace OpenSim.Framework.Servers.HttpServer
         {
             _httpResponse.Body.Flush();
             _httpResponse.Send();
-            
         }
+
         public void FreeContext()
         {
             if (_httpClientContext != null)
                 _httpClientContext.Close();
         }
-        
     }
 }

OpenSim/Framework/Servers/HttpServer/PollServiceRequestManager.cs
@@ -140,9 +140,8 @@ namespace OpenSim.Framework.Servers.HttpServer
             foreach (object o in m_requests)
             {
                 PollServiceHttpRequest req = (PollServiceHttpRequest) o;
-                m_server.DoHTTPGruntWork(
-                    req.PollServiceArgs.NoEvents(req.RequestID, req.PollServiceArgs.Id),
-                    new OSHttpResponse(new HttpResponse(req.HttpContext, req.Request), req.HttpContext));
+                PollServiceWorkerThread.DoHTTPGruntWork(
+                    m_server, req, req.PollServiceArgs.NoEvents(req.RequestID, req.PollServiceArgs.Id));
             }
 
             m_requests.Clear();
@@ -151,6 +150,7 @@ namespace OpenSim.Framework.Servers.HttpServer
             {
                 t.Abort();
             }
+            
             m_running = false;
         }
     }

commit ed513fc7bee7945a71ad9cb66d83ac68335a7774

Fix bug introduced in commit c6e3752 (13 Jun 2012) where poll responses would always return OK even if some other status code had been set

OpenSim/Framework/Servers/HttpServer/PollServiceWorkerThread.cs
@@ -129,9 +129,7 @@ namespace OpenSim.Framework.Servers.HttpServer
             OSHttpResponse response
                 = new OSHttpResponse(new HttpResponse(req.HttpContext, req.Request), req.HttpContext);
 
-            byte[] buffer
-                = server.DoHTTPGruntWork(
-                    responsedata, new OSHttpResponse(new HttpResponse(req.HttpContext, req.Request), req.HttpContext));
+            byte[] buffer = server.DoHTTPGruntWork(responsedata, response);
 
             response.SendChunked = false;
             response.ContentLength64 = buffer.Length;