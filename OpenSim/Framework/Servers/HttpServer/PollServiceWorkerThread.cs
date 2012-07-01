d3f1fc79e50edb9ef851bdedfa83e9f5c13b4519
UbitUmarov <ajlduarte@sapo.pt>
2012-07-01 01:33:20 +0100

    *TO CHECK/REVIEW/REVERT/TEST whatever* pollService new requests get enqueued to unified requests queue directly. Retries get into that every 100ms. 3 working threads as before plus another that only does retries timming.

--- OpenSim/Framework/Servers/HttpServer/PollServiceWorkerThread.cs
+++ OpenSim/Framework/Servers/HttpServer/PollServiceWorkerThread.cs
@@ -25,6 +25,8 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/* Ubit work moved to PollServiceRequestManager
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -128,3 +130,4 @@ namespace OpenSim.Framework.Servers.HttpServer
         }
     }
 }
+*/
