opensim/opensim

commit db91044593fc2592c7abb21034aeea8965febbd7

Thanks Neil Canham for fixing bulk inventory updates, no sending BulkInventoryUpdate after accepting inventory items.

OpenSim/Region/CoreModules/Avatar/Inventory/Transfer/InventoryTransferModule.cs
@@ -278,7 +278,18 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
                 else
                 {
                     if (m_TransferModule != null)
-                        m_TransferModule.SendInstantMessage(im, delegate(bool success) {});
+                        m_TransferModule.SendInstantMessage(im, delegate(bool success) {
+                            // Send BulkUpdateInventory
+                            IInventoryService invService = scene.InventoryService;
+                            UUID inventoryEntityID = new UUID(im.imSessionID); // The inventory item /folder, back from it's trip
+
+                            InventoryFolderBase folder = new InventoryFolderBase(inventoryEntityID, client.AgentId);
+                            folder = invService.GetFolder(folder);
+
+                            ScenePresence fromUser = scene.GetScenePresence(new UUID(im.fromAgentID));
+
+                            fromUser.ControllingClient.SendBulkUpdateInventory(folder);
+                        });
                 }
             }
             else if (im.dialog == (byte) InstantMessageDialog.InventoryDeclined)

OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -3927,7 +3927,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 GridInstantMessage msg = new GridInstantMessage(World,
                         m_host.UUID, m_host.Name+", an object owned by "+
                         resolveName(m_host.OwnerID)+",", destId,
-                        (byte)InstantMessageDialog.InventoryOffered,
+                        (byte)InstantMessageDialog.TaskInventoryOffered,
                         false, objName+"\n"+m_host.Name+" is located at "+
                         World.RegionInfo.RegionName+" "+
                         m_host.AbsolutePosition.ToString(),

commit ecb759c1e5b0ea3e1c81310acf3fe31561d2be3a

Fix regression where llGiveInventory() had stopped asking non-owner receivers to accept/decline.
This appears to be a regression from back in commit db91044 (Mon Aug 22 2011) where we started to send TaskInventoryOffered msg dialog rather than InventoryOffered dialog.
This is probably correct, but failed because the bucket was too large and because we wouldn't have handled the TaskInventoryDeclined option anyway.
This patch handles both of these and make llGiveInventoryList() use TaskInventoryOffered as well
Fixes http://opensimulator.org/mantis/view.php?id=6089

OpenSim/Region/CoreModules/Avatar/InstantMessage/MessageTransferModule.cs
@@ -137,13 +137,15 @@ namespace OpenSim.Region.CoreModules.Avatar.InstantMessage
             foreach (Scene scene in m_Scenes)
             {
 //                m_log.DebugFormat(
-//                    "[INSTANT MESSAGE]: Looking for root agent {0} in {1}", 
+//                    "[INSTANT MESSAGE]: Looking for root agent {0} in {1}",
 //                    toAgentID.ToString(), scene.RegionInfo.RegionName);
+
                 ScenePresence sp = scene.GetScenePresence(toAgentID);
                 if (sp != null && !sp.IsChildAgent)
                 {
                     // Local message
-//                    m_log.DebugFormat("[INSTANT MESSAGE]: Delivering IM to root agent {0} {1}", user.Name, toAgentID);
+                    m_log.DebugFormat("[INSTANT MESSAGE]: Delivering IM to root agent {0} {1}", sp.Name, toAgentID);
+
                     sp.ControllingClient.SendInstantMessage(im);
 
                     // Message sent
@@ -155,13 +157,15 @@ namespace OpenSim.Region.CoreModules.Avatar.InstantMessage
             // try child avatar second
             foreach (Scene scene in m_Scenes)
             {
-//                m_log.DebugFormat(
-//                    "[INSTANT MESSAGE]: Looking for child of {0} in {1}", toAgentID, scene.RegionInfo.RegionName);
+                m_log.DebugFormat(
+                    "[INSTANT MESSAGE]: Looking for child of {0} in {1}", toAgentID, scene.RegionInfo.RegionName);
+
                 ScenePresence sp = scene.GetScenePresence(toAgentID);
                 if (sp != null)
                 {
                     // Local message
-//                    m_log.DebugFormat("[INSTANT MESSAGE]: Delivering IM to child agent {0} {1}", user.Name, toAgentID);
+                    m_log.DebugFormat("[INSTANT MESSAGE]: Delivering IM to child agent {0} {1}", sp.Name, toAgentID);
+
                     sp.ControllingClient.SendInstantMessage(im);
 
                     // Message sent
@@ -170,10 +174,9 @@ namespace OpenSim.Region.CoreModules.Avatar.InstantMessage
                 }
             }
 
-//            m_log.DebugFormat("[INSTANT MESSAGE]: Delivering IM to {0} via XMLRPC", im.toAgentID);
-            SendGridInstantMessageViaXMLRPC(im, result);
+            m_log.DebugFormat("[INSTANT MESSAGE]: Delivering IM to {0} via XMLRPC", im.toAgentID);
 
-            return;
+            SendGridInstantMessageViaXMLRPC(im, result);
         }
 
         private void HandleUndeliveredMessage(GridInstantMessage im, MessageResultNotification result)

OpenSim/Region/CoreModules/Avatar/Inventory/Transfer/InventoryTransferModule.cs
@@ -297,7 +297,9 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
                         });
                 }
             }
-            else if (im.dialog == (byte) InstantMessageDialog.InventoryDeclined)
+            else if (
+                im.dialog == (byte)InstantMessageDialog.InventoryDeclined
+                || im.dialog == (byte)InstantMessageDialog.TaskInventoryDeclined)
             {
                 // Here, the recipient is local and we can assume that the
                 // inventory is loaded. Courtesy of the above bulk update,

OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -3988,22 +3988,23 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 if (agentItem == null)
                     return;
 
-                byte[] bucket = new byte[17];
-                bucket[0] = (byte)assetType;
-                byte[] objBytes = agentItem.ID.GetBytes();
-                Array.Copy(objBytes, 0, bucket, 1, 16);
-
-                GridInstantMessage msg = new GridInstantMessage(World,
-                        m_host.UUID, m_host.Name+", an object owned by "+
-                        resolveName(m_host.OwnerID)+",", destId,
-                        (byte)InstantMessageDialog.TaskInventoryOffered,
-                        false, objName+"\n"+m_host.Name+" is located at "+
-                        World.RegionInfo.RegionName+" "+
-                        m_host.AbsolutePosition.ToString(),
-                        agentItem.ID, true, m_host.AbsolutePosition,
-                        bucket);
                 if (m_TransferModule != null)
+                {
+                    byte[] bucket = new byte[] { (byte)assetType };
+                    
+                    GridInstantMessage msg = new GridInstantMessage(World,
+                            m_host.UUID, m_host.Name + ", an object owned by " +
+                            resolveName(m_host.OwnerID) + ",", destId,
+                            (byte)InstantMessageDialog.TaskInventoryOffered,
+                            false, objName + "\n" + m_host.Name + " is located at " +
+                            World.RegionInfo.RegionName + " " +
+                            m_host.AbsolutePosition.ToString(),
+                            agentItem.ID, true, m_host.AbsolutePosition,
+                            bucket);
+
                     m_TransferModule.SendInstantMessage(msg, delegate(bool success) {});
+                }
+
                 ScriptSleep(3000);
             }
         }
@@ -6410,23 +6411,22 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             if (folderID == UUID.Zero)
                 return;
 
-            byte[] bucket = new byte[17];
-            bucket[0] = (byte)AssetType.Folder;
-            byte[] objBytes = folderID.GetBytes();
-            Array.Copy(objBytes, 0, bucket, 1, 16);
-
-            GridInstantMessage msg = new GridInstantMessage(World,
-                    m_host.UUID, m_host.Name + ", an object owned by " +
-                    resolveName(m_host.OwnerID) + ",", destID,
-                    (byte)InstantMessageDialog.InventoryOffered,
-                    false, category + "\n" + m_host.Name + " is located at " +
-                    World.RegionInfo.RegionName + " " +
-                    m_host.AbsolutePosition.ToString(),
-                    folderID, true, m_host.AbsolutePosition,
-                    bucket);
-
             if (m_TransferModule != null)
+            {
+                byte[] bucket = new byte[] { (byte)AssetType.Folder };
+    
+                GridInstantMessage msg = new GridInstantMessage(World,
+                        m_host.UUID, m_host.Name + ", an object owned by " +
+                        resolveName(m_host.OwnerID) + ",", destID,
+                        (byte)InstantMessageDialog.TaskInventoryOffered,
+                        false, category + "\n" + m_host.Name + " is located at " +
+                        World.RegionInfo.RegionName + " " +
+                        m_host.AbsolutePosition.ToString(),
+                        folderID, true, m_host.AbsolutePosition,
+                        bucket);
+
                 m_TransferModule.SendInstantMessage(msg, delegate(bool success) {});
+            }
         }
 
         public void llSetVehicleType(int type)