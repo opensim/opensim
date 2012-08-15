bbac8f76faff387756bfa55dea83fa3bd589ef79
Melanie <melanie@t-data.com>
2012-08-16 01:18:32 +0200

    Remove AreUpdatesSuspended flag because it does nothing

--- OpenSim/Region/Framework/Scenes/SceneGraph.cs
+++ OpenSim/Region/Framework/Scenes/SceneGraph.cs
@@ -1793,7 +1793,6 @@ namespace OpenSim.Region.Framework.Scenes
 
             try
             {
-                parentGroup.AreUpdatesSuspended = true;
 
                 List<SceneObjectGroup> childGroups = new List<SceneObjectGroup>();
 
@@ -1850,7 +1849,6 @@ namespace OpenSim.Region.Framework.Scenes
                         SceneObjectGroupsByLocalPartID[part.LocalId] = parentGroup;
                 }
 
-                parentGroup.AreUpdatesSuspended = false;
                 parentGroup.HasGroupChanged = true;
                 parentGroup.ProcessBackup(m_parentScene.SimulationDataService, true);
                 parentGroup.ScheduleGroupForFullUpdate();
@@ -1896,7 +1894,6 @@ namespace OpenSim.Region.Framework.Scenes
                             SceneObjectGroup group = part.ParentGroup;
                             if (!affectedGroups.Contains(group))
                             {
-                                group.AreUpdatesSuspended = true;
                                 affectedGroups.Add(group);
                             }
                         }
@@ -1922,7 +1919,6 @@ namespace OpenSim.Region.Framework.Scenes
                     // However, editing linked parts and unlinking may be different
                     //
                     SceneObjectGroup group = root.ParentGroup;
-                    group.AreUpdatesSuspended = true;
                     
                     List<SceneObjectPart> newSet = new List<SceneObjectPart>(group.Parts);
                     int numChildren = newSet.Count;
@@ -1945,7 +1941,6 @@ namespace OpenSim.Region.Framework.Scenes
                             group.DelinkFromGroup(p, sendEventsToRemainder);
                             if (numChildren > 2)
                             {
-                                p.ParentGroup.AreUpdatesSuspended = true;
                             }
                             else
                             {
@@ -1980,7 +1975,6 @@ namespace OpenSim.Region.Framework.Scenes
                             foreach (SceneObjectPart newChild in newSet)
                                 newChild.ClearUpdateSchedule();
 
-                        newRoot.ParentGroup.AreUpdatesSuspended = true;
                         LinkObjects(newRoot, newSet);
                         if (!affectedGroups.Contains(newRoot.ParentGroup))
                             affectedGroups.Add(newRoot.ParentGroup);
@@ -1998,7 +1992,6 @@ namespace OpenSim.Region.Framework.Scenes
                     m_parentScene.SimulationDataService.RemoveObject(g.UUID, m_parentScene.RegionInfo.RegionID);
                     g.TriggerScriptChangedEvent(Changed.LINK);
                     g.HasGroupChanged = true; // Persist
-                    g.AreUpdatesSuspended = false;
                     g.ScheduleGroupForFullUpdate();
                 }
             }
--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -1775,13 +1775,11 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 try
                 {
-                    parts[0].ParentGroup.AreUpdatesSuspended = true;
                     foreach (SceneObjectPart part in parts)
                         SetAlpha(part, alpha, face);
                 }
                 finally
                 {
-                    parts[0].ParentGroup.AreUpdatesSuspended = false;
                 }
             }
         }
@@ -1962,13 +1960,11 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 try
                 {
-                    parts[0].ParentGroup.AreUpdatesSuspended = true;
                     foreach (SceneObjectPart part in parts)
                         SetTexture(part, texture, face);
                 }
                 finally
                 {
-                    parts[0].ParentGroup.AreUpdatesSuspended = false;
                 }
             }
             ScriptSleep(200);
@@ -3931,13 +3927,11 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 try
                 {
-                    parts[0].ParentGroup.AreUpdatesSuspended = true;
                     foreach (SceneObjectPart part in parts)
                         part.SetFaceColor(new Vector3((float)color.x, (float)color.y, (float)color.z), face);
                 }
                 finally
                 {
-                    parts[0].ParentGroup.AreUpdatesSuspended = false;
                 }
             }
         }
@@ -4055,7 +4049,6 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 {
                     try
                     {
-                        parts[0].ParentGroup.AreUpdatesSuspended = true;
                         foreach (SceneObjectPart part in parts)
                         {
                             parentPrim.DelinkFromGroup(part.LocalId, true);
@@ -4063,7 +4056,6 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                     }
                     finally
                     {
-                        parts[0].ParentGroup.AreUpdatesSuspended = false;
                     }
                 }
 
@@ -4078,7 +4070,6 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
 
                     try
                     {
-                        parts[0].ParentGroup.AreUpdatesSuspended = true;
                         foreach (SceneObjectPart part in parts)
                         {
                             part.ClearUpdateSchedule();
@@ -4087,7 +4078,6 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                     }
                     finally
                     {
-                        parts[0].ParentGroup.AreUpdatesSuspended = false;
                     }                    
                     
                     
@@ -6302,7 +6292,6 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             {
                 try
                 {
-                    parts[0].ParentGroup.AreUpdatesSuspended = true;
                     foreach (var part in parts)
                     {
                         SetTextureAnim(part, mode, face, sizex, sizey, start, length, rate);
@@ -6310,7 +6299,6 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 }
                 finally
                 {
-                    parts[0].ParentGroup.AreUpdatesSuspended = false;
                 }
             }
         }
