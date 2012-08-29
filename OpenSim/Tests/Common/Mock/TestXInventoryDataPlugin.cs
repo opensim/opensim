opensim/opensim

commit a0d178b284050df64d0eb5b9728565fd72615c22

Following on from f8a89a79, do not allow more than one 'type' folder (e.g. calling cards) to be created in the base "My Inventory" user folder.
This is to accomodate situations where viewers will create more than one 'type' subfolder (e.g. calling cards)
But at the same time to prevent multiple such 'system' folders (those in the base "My Inventory" user folder).
This also makes GetFolderForType() only return a folder in the base "My Inventory" folder, if such a type folder exists

OpenSim/Services/InventoryService/XInventoryService.cs
@@ -229,10 +229,28 @@ namespace OpenSim.Services.InventoryService
         public virtual InventoryFolderBase GetFolderForType(UUID principalID, AssetType type)
         {
 //            m_log.DebugFormat("[XINVENTORY SERVICE]: Getting folder type {0} for user {1}", type, principalID);
+
+            InventoryFolderBase rootFolder = GetRootFolder(principalID);
+
+            if (rootFolder == null)
+            {
+                m_log.WarnFormat(
+                    "[XINVENTORY]: Found no root folder for {0} in GetFolderForType() when looking for {1}",
+                    principalID, type);
+
+                return null;
+            }
+            
+            return GetSystemFolderForType(rootFolder, type);
+        }
+
+        private InventoryFolderBase GetSystemFolderForType(InventoryFolderBase rootFolder, AssetType type)
+        {
+//            m_log.DebugFormat("[XINVENTORY SERVICE]: Getting folder type {0} for user {1}", type, principalID);
             
             XInventoryFolder[] folders = m_Database.GetFolders(
-                    new string[] { "agentID", "type"},
-                    new string[] { principalID.ToString(), ((int)type).ToString() });
+                    new string[] { "agentID", "parentFolderID", "type"},
+                    new string[] { rootFolder.Owner.ToString(), rootFolder.ID.ToString(), ((int)type).ToString() });
 
             if (folders.Length == 0)
             {
@@ -308,23 +326,38 @@ namespace OpenSim.Services.InventoryService
             if (check != null)
                 return false;
 
-            if (folder.Type == (short)AssetType.Folder
-                || folder.Type == (short)AssetType.Unknown
-                || folder.Type == (short)AssetType.OutfitFolder
-                || folder.Type == (short)AssetType.CallingCard
-                || GetFolderForType(folder.Owner, (AssetType)(folder.Type)) == null)
+            if (folder.Type != (short)AssetType.Folder || folder.Type != (short)AssetType.Unknown)
             {
-                XInventoryFolder xFolder = ConvertFromOpenSim(folder);
-                return m_Database.StoreFolder(xFolder);
-            }
-            else
-            {
-                m_log.WarnFormat(
-                    "[XINVENTORY]: Folder of type {0} already exists when tried to add {1} to {2} for {3}",
-                    folder.Type, folder.Name, folder.ParentID, folder.Owner);
+                InventoryFolderBase rootFolder = GetRootFolder(folder.Owner);
+
+                if (rootFolder == null)
+                {
+                    m_log.WarnFormat(
+                        "[XINVENTORY]: Found no root folder for {0} in AddFolder() when looking for {1}",
+                        folder.Owner, folder.Type);
+
+                    return false;
+                }
+
+                // Check we're not trying to add this as a system folder.
+                if (folder.ParentID == rootFolder.ID)
+                {
+                    InventoryFolderBase existingSystemFolder
+                        = GetSystemFolderForType(rootFolder, (AssetType)folder.Type);
+
+                    if (existingSystemFolder != null)
+                    {
+                        m_log.WarnFormat(
+                            "[XINVENTORY]: System folder of type {0} already exists when tried to add {1} to {2} for {3}",
+                            folder.Type, folder.Name, folder.ParentID, folder.Owner);
+    
+                        return false;
+                    }
+                }
             }
 
-            return false;
+            XInventoryFolder xFolder = ConvertFromOpenSim(folder);
+            return m_Database.StoreFolder(xFolder);
         }
 
         public virtual bool UpdateFolder(InventoryFolderBase folder)

commit 7ea832d47c827ad9ef8eb0ce24702fbee585b1ee

Fix regression introduced in a0d178b2 (Sat Aug 25 02:00:17 2012) where folders with asset type of 'Folder' and 'Unknown' were accidentally treated as system folders.
This prevented more than one additional ordinary folder from being created in the base "My Inventory" user folder.
Added regression test for this case.
Switched tests to use XInventoryService with mostly implemented TestXInventoryDataPlugin rather than InventoryService
Disabled TestLoadIarV0_1SameNameCreator() since this has not been working for a very long time (ever since XInventoryService) started being used
since it doesnt' preserve creator data in the same way as InventoryService did and so effectively lost the OSPAs.
However, nobody noticed/complained about this issue and OSPAs have been superseded by HG like creator information via the --home save oar/iar switch.

OpenSim/Data/IXInventoryData.cs
@@ -40,6 +40,11 @@ namespace OpenSim.Data
         public UUID folderID;
         public UUID agentID;
         public UUID parentFolderID;
+
+        public XInventoryFolder Clone()
+        {
+            return (XInventoryFolder)MemberwiseClone();
+        }
     }
 
     public class XInventoryItem
@@ -64,6 +69,11 @@ namespace OpenSim.Data
         public UUID avatarID;
         public UUID parentFolderID;
         public int inventoryGroupPermissions;
+
+        public XInventoryItem Clone()
+        {
+            return (XInventoryItem)MemberwiseClone();
+        }
     }
 
     public interface IXInventoryData

OpenSim/Framework/InventoryFolderBase.cs
@@ -73,32 +73,26 @@ namespace OpenSim.Framework
         {
         }
 
-        public InventoryFolderBase(UUID id)
+        public InventoryFolderBase(UUID id) : this()
         {
             ID = id;
         }
 
-        public InventoryFolderBase(UUID id, UUID owner)
+        public InventoryFolderBase(UUID id, UUID owner) : this(id)
         {
-            ID = id;
             Owner = owner;
         }
 
-        public InventoryFolderBase(UUID id, string name, UUID owner, UUID parent)
+        public InventoryFolderBase(UUID id, string name, UUID owner, UUID parent) : this(id, owner)
         {
-            ID = id;
             Name = name;
-            Owner = owner;
             ParentID = parent;
         }
 
-        public InventoryFolderBase(UUID id, string name, UUID owner, short type, UUID parent, ushort version)
+        public InventoryFolderBase(
+            UUID id, string name, UUID owner, short type, UUID parent, ushort version) : this(id, name, owner, parent)
         {
-            ID = id;
-            Name = name;
-            Owner = owner;
             Type = type;
-            ParentID = parent;
             Version = version;
         }
     }

OpenSim/Framework/Serialization/External/OspResolver.cs
@@ -65,9 +65,14 @@ namespace OpenSim.Framework.Serialization
 
             UserAccount account = userService.GetUserAccount(UUID.Zero, userId);
             if (account != null)
+            {
                 return MakeOspa(account.FirstName, account.LastName);
+            }
 //            else
+//            {
 //                m_log.WarnFormat("[OSP RESOLVER]: No user account for {0}", userId);
+//                System.Console.WriteLine("[OSP RESOLVER]: No user account for {0}", userId);
+//            }
 
             return null;
         }
@@ -79,10 +84,13 @@ namespace OpenSim.Framework.Serialization
         /// <returns></returns>
         public static string MakeOspa(string firstName, string lastName)
         {
-//            m_log.DebugFormat("[OSP RESOLVER]: Making OSPA for {0} {1}", firstName, lastName);
+            string ospa
+                = OSPA_PREFIX + OSPA_NAME_KEY + OSPA_PAIR_SEPARATOR + firstName + OSPA_NAME_VALUE_SEPARATOR + lastName;
+
+//            m_log.DebugFormat("[OSP RESOLVER]: Made OSPA {0} for {1} {2}", ospa, firstName, lastName);
+//            System.Console.WriteLine("[OSP RESOLVER]: Made OSPA {0} for {1} {2}", ospa, firstName, lastName);
             
-            return 
-                OSPA_PREFIX + OSPA_NAME_KEY + OSPA_PAIR_SEPARATOR + firstName + OSPA_NAME_VALUE_SEPARATOR + lastName;
+            return ospa;
         }
         
         /// <summary>

OpenSim/Services/InventoryService/XInventoryService.cs
@@ -94,6 +94,7 @@ namespace OpenSim.Services.InventoryService
 
             m_Database = LoadPlugin<IXInventoryData>(dllName,
                     new Object[] {connString, String.Empty});
+
             if (m_Database == null)
                 throw new Exception("Could not find a storage interface in the given module");
         }
@@ -326,7 +327,7 @@ namespace OpenSim.Services.InventoryService
             if (check != null)
                 return false;
 
-            if (folder.Type != (short)AssetType.Folder || folder.Type != (short)AssetType.Unknown)
+            if (folder.Type != (short)AssetType.Folder && folder.Type != (short)AssetType.Unknown)
             {
                 InventoryFolderBase rootFolder = GetRootFolder(folder.Owner);
 

OpenSim/Region/CoreModules/Avatar/Inventory/Archiver/Tests/InventoryArchiverTests.cs
@@ -350,38 +350,38 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Archiver.Tests
             Assert.That(sog1.RootPart.CreatorID, Is.EqualTo(m_uaLL1.PrincipalID));            
         }
         
-        /// <summary>
-        /// Test loading a V0.1 OpenSim Inventory Archive (subject to change since there is no fixed format yet) where
-        /// an account exists with the same name as the creator, though not the same id.
-        /// </summary>
-        [Test]
-        public void TestLoadIarV0_1SameNameCreator()
-        {
-            TestHelpers.InMethod();
-//            log4net.Config.XmlConfigurator.Configure();
-
-            UserAccountHelpers.CreateUserWithInventory(m_scene, m_uaMT, "meowfood");
-            UserAccountHelpers.CreateUserWithInventory(m_scene, m_uaLL2, "hampshire");
-            
-            m_archiverModule.DearchiveInventory(m_uaMT.FirstName, m_uaMT.LastName, "/", "meowfood", m_iarStream);            
-            InventoryItemBase foundItem1
-                = InventoryArchiveUtils.FindItemByPath(m_scene.InventoryService, m_uaMT.PrincipalID, m_item1Name);
-
-            Assert.That(
-                foundItem1.CreatorId, Is.EqualTo(m_uaLL2.PrincipalID.ToString()), 
-                "Loaded item non-uuid creator doesn't match original");            
-            Assert.That(
-                foundItem1.CreatorIdAsUuid, Is.EqualTo(m_uaLL2.PrincipalID), 
-                "Loaded item uuid creator doesn't match original");
-            Assert.That(foundItem1.Owner, Is.EqualTo(m_uaMT.PrincipalID),
-                "Loaded item owner doesn't match inventory reciever");
-            
-            AssetBase asset1 = m_scene.AssetService.Get(foundItem1.AssetID.ToString());            
-            string xmlData = Utils.BytesToString(asset1.Data);
-            SceneObjectGroup sog1 = SceneObjectSerializer.FromOriginalXmlFormat(xmlData);
-            
-            Assert.That(sog1.RootPart.CreatorID, Is.EqualTo(m_uaLL2.PrincipalID));
-        }
+//        /// <summary>
+//        /// Test loading a V0.1 OpenSim Inventory Archive (subject to change since there is no fixed format yet) where
+//        /// an account exists with the same name as the creator, though not the same id.
+//        /// </summary>
+//        [Test]
+//        public void TestLoadIarV0_1SameNameCreator()
+//        {
+//            TestHelpers.InMethod();
+//            TestHelpers.EnableLogging();
+//
+//            UserAccountHelpers.CreateUserWithInventory(m_scene, m_uaMT, "meowfood");
+//            UserAccountHelpers.CreateUserWithInventory(m_scene, m_uaLL2, "hampshire");
+//            
+//            m_archiverModule.DearchiveInventory(m_uaMT.FirstName, m_uaMT.LastName, "/", "meowfood", m_iarStream);            
+//            InventoryItemBase foundItem1
+//                = InventoryArchiveUtils.FindItemByPath(m_scene.InventoryService, m_uaMT.PrincipalID, m_item1Name);
+//
+//            Assert.That(
+//                foundItem1.CreatorId, Is.EqualTo(m_uaLL2.PrincipalID.ToString()), 
+//                "Loaded item non-uuid creator doesn't match original");            
+//            Assert.That(
+//                foundItem1.CreatorIdAsUuid, Is.EqualTo(m_uaLL2.PrincipalID), 
+//                "Loaded item uuid creator doesn't match original");
+//            Assert.That(foundItem1.Owner, Is.EqualTo(m_uaMT.PrincipalID),
+//                "Loaded item owner doesn't match inventory reciever");
+//            
+//            AssetBase asset1 = m_scene.AssetService.Get(foundItem1.AssetID.ToString());            
+//            string xmlData = Utils.BytesToString(asset1.Data);
+//            SceneObjectGroup sog1 = SceneObjectSerializer.FromOriginalXmlFormat(xmlData);
+//            
+//            Assert.That(sog1.RootPart.CreatorID, Is.EqualTo(m_uaLL2.PrincipalID));
+//        }
 
         /// <summary>
         /// Test loading a V0.1 OpenSim Inventory Archive (subject to change since there is no fixed format yet) where

OpenSim/Region/Framework/Scenes/Tests/UserInventoryTests.cs
@@ -50,8 +50,40 @@ using OpenSim.Tests.Common.Mock;
 namespace OpenSim.Region.Framework.Tests
 {
     [TestFixture]
-    public class UserInventoryTests
+    public class UserInventoryTests : OpenSimTestCase
     {
+        [Test]
+        public void TestCreateInventoryFolders()
+        {
+            TestHelpers.InMethod();
+//            TestHelpers.EnableLogging();
+
+            // For this test both folders will have the same name which is legal in SL user inventories.
+            string foldersName = "f1";
+
+            Scene scene = new SceneHelpers().SetupScene();
+            UserAccount user1 = UserAccountHelpers.CreateUserWithInventory(scene, TestHelpers.ParseTail(1001));
+
+            UserInventoryHelpers.CreateInventoryFolder(scene.InventoryService, user1.PrincipalID, foldersName);
+
+            List<InventoryFolderBase> oneFolder
+                = UserInventoryHelpers.GetInventoryFolders(scene.InventoryService, user1.PrincipalID, foldersName);
+
+            Assert.That(oneFolder.Count, Is.EqualTo(1));
+            InventoryFolderBase firstRetrievedFolder = oneFolder[0];
+            Assert.That(firstRetrievedFolder.Name, Is.EqualTo(foldersName));
+
+            UserInventoryHelpers.CreateInventoryFolder(scene.InventoryService, user1.PrincipalID, foldersName);
+
+            List<InventoryFolderBase> twoFolders
+                = UserInventoryHelpers.GetInventoryFolders(scene.InventoryService, user1.PrincipalID, foldersName);
+
+            Assert.That(twoFolders.Count, Is.EqualTo(2));
+            Assert.That(twoFolders[0].Name, Is.EqualTo(foldersName));
+            Assert.That(twoFolders[1].Name, Is.EqualTo(foldersName));
+            Assert.That(twoFolders[0].ID, Is.Not.EqualTo(twoFolders[1].ID));
+        }
+
         [Test]
         public void TestGiveInventoryItem()
         {
@@ -83,7 +115,7 @@ namespace OpenSim.Region.Framework.Tests
         public void TestGiveInventoryFolder()
         {
             TestHelpers.InMethod();
-//            log4net.Config.XmlConfigurator.Configure();
+//            TestHelpers.EnableLogging();
             
             Scene scene = new SceneHelpers().SetupScene();
             UserAccount user1 = UserAccountHelpers.CreateUserWithInventory(scene, TestHelpers.ParseTail(1001));

OpenSim/Tests/Common/Helpers/SceneHelpers.cs
@@ -245,7 +245,7 @@ namespace OpenSim.Tests.Common
             config.AddConfig("Modules");
             config.AddConfig("InventoryService");
             config.Configs["Modules"].Set("InventoryServices", "LocalInventoryServicesConnector");
-            config.Configs["InventoryService"].Set("LocalServiceModule", "OpenSim.Services.InventoryService.dll:InventoryService");
+            config.Configs["InventoryService"].Set("LocalServiceModule", "OpenSim.Services.InventoryService.dll:XInventoryService");
             config.Configs["InventoryService"].Set("StorageProvider", "OpenSim.Tests.Common.dll");
 
             LocalInventoryServicesConnector inventoryService = new LocalInventoryServicesConnector();

OpenSim/Tests/Common/Helpers/UserInventoryHelpers.cs
@@ -199,7 +199,9 @@ namespace OpenSim.Tests.Common
             string[] components = path.Split(new string[] { PATH_DELIMITER }, 2, StringSplitOptions.None);
 
             InventoryFolderBase newFolder 
-                = new InventoryFolderBase(UUID.Random(), components[0], parentFolder.Owner, parentFolder.ID);
+                = new InventoryFolderBase(
+                    UUID.Random(), components[0], parentFolder.Owner, (short)AssetType.Unknown, parentFolder.ID, 0);
+            
             inventoryService.AddFolder(newFolder);
 
             if (components.Length > 1)

OpenSim/Tests/Common/Mock/TestXInventoryDataPlugin.cs
@@ -1,0 +1,131 @@
+/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using log4net;
+using OpenMetaverse;
+using OpenSim.Framework;
+using OpenSim.Data;
+
+namespace OpenSim.Tests.Common.Mock
+{
+    public class TestXInventoryDataPlugin : IXInventoryData
+    {
+        private Dictionary<UUID, XInventoryFolder> m_allFolders = new Dictionary<UUID, XInventoryFolder>();
+        private Dictionary<UUID, XInventoryItem> m_allItems = new Dictionary<UUID, XInventoryItem>();
+
+        public TestXInventoryDataPlugin(string conn, string realm) {}
+
+        public XInventoryItem[] GetItems(string[] fields, string[] vals)
+        {
+            List<XInventoryItem> origItems = Get<XInventoryItem>(fields, vals, m_allItems.Values.ToList());
+
+            return origItems.Select(i => i.Clone()).ToArray();
+        }
+
+        public XInventoryFolder[] GetFolders(string[] fields, string[] vals)
+        {
+            List<XInventoryFolder> origFolders
+                = Get<XInventoryFolder>(fields, vals, m_allFolders.Values.ToList());
+
+            return origFolders.Select(f => f.Clone()).ToArray();
+        }
+
+        private List<T> Get<T>(string[] fields, string[] vals, List<T> inputEntities)
+        {
+            List<T> entities = inputEntities;
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                entities
+                    = entities.Where(
+                        e =>
+                        {
+                            FieldInfo fi = typeof(T).GetField(fields[i]);
+                            if (fi == null)
+                                throw new NotImplementedException(string.Format("No field {0} for val {1}", fields[i], vals[i]));
+
+                            return fi.GetValue(e).ToString() == vals[i];
+                        }
+                    ).ToList();
+            }
+
+            return entities;
+        }
+
+        public bool StoreFolder(XInventoryFolder folder)
+        {
+            m_allFolders[folder.folderID] = folder.Clone();
+
+//            Console.WriteLine("Added folder {0} {1}", folder.folderName, folder.folderID);
+
+            return true;
+        }
+
+        public bool StoreItem(XInventoryItem item)
+        {
+            m_allItems[item.inventoryID] = item.Clone();
+
+//            Console.WriteLine("Added item {0} {1}, creator {2}, owner {3}", item.inventoryName, item.inventoryID, item.creatorID, item.avatarID);
+
+            return true;
+        }
+
+        public bool DeleteFolders(string field, string val)
+        {
+            return DeleteFolders(new string[] { field }, new string[] { val });
+        }
+
+        public bool DeleteFolders(string[] fields, string[] vals)
+        {
+            XInventoryFolder[] foldersToDelete = GetFolders(fields, vals);
+            Array.ForEach(foldersToDelete, f => m_allFolders.Remove(f.folderID));
+
+            return true;
+        }
+
+        public bool DeleteItems(string field, string val)
+        {
+            return DeleteItems(new string[] { field }, new string[] { val });
+        }
+
+        public bool DeleteItems(string[] fields, string[] vals)
+        {
+            XInventoryItem[] itemsToDelete = GetItems(fields, vals);
+            Array.ForEach(itemsToDelete, i => m_allItems.Remove(i.inventoryID));
+
+            return true;
+        }
+
+        public bool MoveItem(string id, string newParent) { throw new NotImplementedException(); }
+        public XInventoryItem[] GetActiveGestures(UUID principalID) { throw new NotImplementedException(); }
+        public int GetAssetPermissions(UUID principalID, UUID assetID) { throw new NotImplementedException(); }
+    }
+}

OpenSim/Tests/Common/TestHelpers.cs
@@ -95,6 +95,7 @@ namespace OpenSim.Tests.Common
         public static void EnableLogging()
         {
             log4net.Config.XmlConfigurator.Configure(EnableLoggingConfigStream);
+            EnableLoggingConfigStream.Position = 0;
         }
 
         /// <summary>