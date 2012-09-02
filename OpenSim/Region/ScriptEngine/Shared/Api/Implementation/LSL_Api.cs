fb211c64fd9d335f4879549023870b1e28416f74
SignpostMarv <github@signpostmarv.name>
2012-09-02 13:47:18 +0100

    formatting

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -2083,7 +2083,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
         public void llSetRot(LSL_Rotation rot)
         {
             m_host.AddScriptLPS(1);
-                SetRot(m_host, rot);
+            SetRot(m_host, rot);
             ScriptSleep(200);
         }
 
@@ -7280,7 +7280,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                             if (remain < 1)
                                 return null;
 
-                                SetRot(part, rules.GetQuaternionItem(idx++));
+                            SetRot(part, rules.GetQuaternionItem(idx++));
 
                             break;
 
