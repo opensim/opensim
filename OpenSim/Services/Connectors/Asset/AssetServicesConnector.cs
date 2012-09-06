3257f4d5d21116e9fb8e08f3277b3521d38bc11c
Melanie <melanie@t-data.com>
2012-09-06 17:31:41 +0200

    Revert "Change string concatenation to Path.Combine to eliminate extra slashes."
    This reverts commit b9d026666dbabb210cc013d29eb79fe214594dd9.

--- OpenSim/Services/Connectors/Asset/AssetServicesConnector.cs
+++ OpenSim/Services/Connectors/Asset/AssetServicesConnector.cs
@@ -181,7 +181,7 @@ namespace OpenSim.Services.Connectors
 
         public AssetBase Get(string id)
         {
-            string uri = Path.Combine(MapServer(id), Path.Combine("assets", id));
+            string uri = MapServer(id) + "/assets/" + id;
 
             AssetBase asset = null;
             if (m_Cache != null)
@@ -218,7 +218,7 @@ namespace OpenSim.Services.Connectors
                     return fullAsset.Metadata;
             }
 
-            string uri = Path.Combine(MapServer(id), Path.Combine("assets", id));
+            string uri = MapServer(id) + "/assets/" + id + "/metadata";
 
             AssetMetadata asset = SynchronousRestObjectRequester.
                     MakeRequest<int, AssetMetadata>("GET", uri, 0);
@@ -260,7 +260,7 @@ namespace OpenSim.Services.Connectors
 
         public bool Get(string id, Object sender, AssetRetrieved handler)
         {
-            string uri = Path.Combine(MapServer(id), Path.Combine("assets", id));
+            string uri = MapServer(id) + "/assets/" + id;
 
             AssetBase asset = null;
             if (m_Cache != null)
@@ -379,9 +379,7 @@ namespace OpenSim.Services.Connectors
                 return asset.ID;
             }
 
-            string uri = Path.Combine(MapServer(asset.FullID.ToString()), "/assets/");
-            if (!uri.EndsWith("/"))
-                uri += "/";
+            string uri = MapServer(asset.FullID.ToString()) + "/assets/";
 
             string newID = string.Empty;
             try
@@ -458,7 +456,7 @@ namespace OpenSim.Services.Connectors
             }
             asset.Data = data;
 
-            string uri = Path.Combine(MapServer(id), Path.Combine("assets", id));
+            string uri = MapServer(id) + "/assets/" + id;
 
             if (SynchronousRestObjectRequester.
                     MakeRequest<AssetBase, bool>("POST", uri, asset))
@@ -473,7 +471,7 @@ namespace OpenSim.Services.Connectors
 
         public bool Delete(string id)
         {
-            string uri = Path.Combine(MapServer(id), Path.Combine("assets", id));
+            string uri = MapServer(id) + "/assets/" + id;
 
             if (SynchronousRestObjectRequester.
                     MakeRequest<int, bool>("DELETE", uri, 0))
