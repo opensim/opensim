0b9bf236dd336c66542215bbc8f4a1eb85c646ee
Diva Canto <diva@metaverseink.com>
2012-10-01 15:55:24 -0700

    On more virtual method

--- OpenSim/Services/Interfaces/IGridUserService.cs
+++ OpenSim/Services/Interfaces/IGridUserService.cs
@@ -80,7 +80,7 @@ namespace OpenSim.Services.Interfaces
 
         }
 
-        public Dictionary<string, object> ToKeyValuePairs()
+        public virtual Dictionary<string, object> ToKeyValuePairs()
         {
             Dictionary<string, object> result = new Dictionary<string, object>();
             result["UserID"] = UserID;
