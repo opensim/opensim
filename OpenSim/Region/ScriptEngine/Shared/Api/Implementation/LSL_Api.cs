opensim/opensim

commit 16c9c1dff7bbf299efddd44e4f9aeeb7db38fff6

On receiving TaskInventoryAccepted with a destination folder in the binary bucket slot for RLV, notify the viewer about inventory folder updates.
The viewer would not see the folder move without this, either on accept or decline.
This commit also updates the TaskInventoryOffered message to better conform with the data LL uses
Changes are, agentID is prim owner rather than prim id, agent name is now simply object name rather than name with owner detail,
message is just folder name in single quotes, message is not timestamped.
However, folder is not renamed "still #RLV/~<name>".  Long term solution is probably not to do these operations server-side.
Notes will be added to http://opensimulator.org/mantis/view.php?id=6311

OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -3978,7 +3978,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                             World.RegionInfo.RegionName+" "+
                             m_host.AbsolutePosition.ToString(),
                             agentItem.ID, true, m_host.AbsolutePosition,
-                            bucket);
+                            bucket, true); // TODO: May actually send no timestamp
 
                     m_TransferModule.SendInstantMessage(msg, delegate(bool success) {});
                 }
@@ -6452,16 +6452,17 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             if (m_TransferModule != null)
             {
                 byte[] bucket = new byte[] { (byte)AssetType.Folder };
-    
+
+                Vector3 pos = m_host.AbsolutePosition;
+
                 GridInstantMessage msg = new GridInstantMessage(World,
-                        m_host.UUID, m_host.Name + ", an object owned by " +
-                        resolveName(m_host.OwnerID) + ",", destID,
+                        m_host.OwnerID, m_host.Name, destID,
                         (byte)InstantMessageDialog.TaskInventoryOffered,
-                        false, category + "\n" + m_host.Name + " is located at " +
-                        World.RegionInfo.RegionName + " " +
-                        m_host.AbsolutePosition.ToString(),
-                        folderID, true, m_host.AbsolutePosition,
-                        bucket);
+                        false, string.Format("'{0}'"),
+// We won't go so far as to add a SLURL, but this is the format used by LL as of 2012-10-06                                       
+// false, string.Format("'{0}'  ( http://slurl.com/secondlife/{1}/{2}/{3}/{4} )", category, World.Name, (int)pos.X, (int)pos.Y, (int)pos.Z),
+                        folderID, false, pos,
+                        bucket, false);
 
                 m_TransferModule.SendInstantMessage(msg, delegate(bool success) {});
             }

OpenSim/Framework/GridInstantMessage.cs
@@ -44,7 +44,6 @@ namespace OpenSim.Framework
         public Vector3 Position;
         public byte[] binaryBucket;
 
-
         public uint ParentEstateID;
         public Guid RegionID;
         public uint timestamp;
@@ -58,7 +57,7 @@ namespace OpenSim.Framework
                 string _fromAgentName, UUID _toAgentID,
                 byte _dialog, bool _fromGroup, string _message,
                 UUID _imSessionID, bool _offline, Vector3 _position,
-                byte[] _binaryBucket)
+                byte[] _binaryBucket, bool addTimestamp)
         {
             fromAgentID = _fromAgentID.Guid;
             fromAgentName = _fromAgentName;
@@ -79,7 +78,9 @@ namespace OpenSim.Framework
                 ParentEstateID = scene.RegionInfo.EstateSettings.ParentEstateID;
                 RegionID = scene.RegionInfo.RegionSettings.RegionUUID.Guid;
             }
-            timestamp = (uint)Util.UnixTimeSinceEpoch();
+
+            if (addTimestamp)
+                timestamp = (uint)Util.UnixTimeSinceEpoch();
         }
 
         public GridInstantMessage(IScene scene, UUID _fromAgentID,
@@ -87,7 +88,7 @@ namespace OpenSim.Framework
                 string _message, bool _offline,
                 Vector3 _position) : this(scene, _fromAgentID, _fromAgentName,
                 _toAgentID, _dialog, false, _message,
-                _fromAgentID ^ _toAgentID, _offline, _position, new byte[0])
+                _fromAgentID ^ _toAgentID, _offline, _position, new byte[0], true)
         {
         }
     }

OpenSim/Region/ClientStack/Linden/UDP/LLClientView.cs
@@ -5862,7 +5862,8 @@ namespace OpenSim.Region.ClientStack.LindenUDP
                         msgpack.MessageBlock.ID,
                         msgpack.MessageBlock.Offline != 0 ? true : false,
                         msgpack.MessageBlock.Position,
-                        msgpack.MessageBlock.BinaryBucket);
+                        msgpack.MessageBlock.BinaryBucket,
+                        true);
 
                 handlerInstantMessage(this, im);
             }

OpenSim/Region/CoreModules/Avatar/Friends/CallingCardModule.cs
@@ -141,7 +141,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Friends
                         client.FirstName+" "+client.LastName,
                         destID, (byte)211, false,
                         String.Empty,
-                        transactionID, false, new Vector3(), new byte[0]),
+                        transactionID, false, new Vector3(), new byte[0], true),
                         delegate(bool success) {} );
             }
         }

OpenSim/Region/CoreModules/Avatar/Inventory/Transfer/InventoryTransferModule.cs
@@ -297,6 +297,11 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
                         });
                 }
             }
+
+            // XXX: This code was placed here to try and accomdate RLV which moves given folders named #RLV/~<name>
+            // to a folder called name in #RLV.  However, this approach may not be ultimately correct - from analysis
+            // of Firestorm 4.2.2 on sending an InventoryOffered instead of TaskInventoryOffered (as was previously
+            // done), the viewer itself would appear to move and rename the folder, rather than the simulator doing it here.
             else if (im.dialog == (byte) InstantMessageDialog.TaskInventoryAccepted)
             {
                 UUID destinationFolderID = UUID.Zero;
@@ -308,6 +313,16 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
 
                 if (destinationFolderID != UUID.Zero)
                 {
+                    InventoryFolderBase destinationFolder = new InventoryFolderBase(destinationFolderID, client.AgentId);
+                    if (destinationFolder == null)
+                    {
+                        m_log.WarnFormat(
+                            "[INVENTORY TRANSFER]: TaskInventoryAccepted message from {0} in {1} specified folder {2} which does not exist",
+                            client.Name, scene.Name, destinationFolderID);
+
+                        return;
+                    }
+
                     IInventoryService invService = scene.InventoryService;
 
                     UUID inventoryID = new UUID(im.imSessionID); // The inventory item/folder, back from it's trip
@@ -315,9 +330,11 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
                     InventoryItemBase item = new InventoryItemBase(inventoryID, client.AgentId);
                     item = invService.GetItem(item);
                     InventoryFolderBase folder = null;
+                    UUID? previousParentFolderID = null;
 
                     if (item != null) // It's an item
                     {
+                        previousParentFolderID = item.Folder;
                         item.Folder = destinationFolderID;
 
                         invService.DeleteItems(item.Owner, new List<UUID>() { item.ID });
@@ -330,9 +347,21 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
 
                         if (folder != null) // It's a folder
                         {
+                            previousParentFolderID = folder.ParentID;
                             folder.ParentID = destinationFolderID;
                             invService.MoveFolder(folder);
                         }
+                    }
+
+                    // Tell client about updates to original parent and new parent (this should probably be factored with existing move item/folder code).
+                    if (previousParentFolderID != null)
+                    {
+                        InventoryFolderBase previousParentFolder
+                            = new InventoryFolderBase((UUID)previousParentFolderID, client.AgentId);
+                        previousParentFolder = invService.GetFolder(previousParentFolder);
+                        scene.SendInventoryUpdate(client, previousParentFolder, true, true);
+
+                        scene.SendInventoryUpdate(client, destinationFolder, true, true);
                     }
                 }
             }
@@ -354,9 +383,11 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
                 InventoryItemBase item = new InventoryItemBase(inventoryID, client.AgentId);
                 item = invService.GetItem(item);
                 InventoryFolderBase folder = null;
+                UUID? previousParentFolderID = null;
                 
                 if (item != null && trashFolder != null)
                 {
+                    previousParentFolderID = item.Folder;
                     item.Folder = trashFolder.ID;
 
                     // Diva comment: can't we just update this item???
@@ -372,6 +403,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
                     
                     if (folder != null & trashFolder != null)
                     {
+                        previousParentFolderID = folder.ParentID;
                         folder.ParentID = trashFolder.ID;
                         invService.MoveFolder(folder);
                     }
@@ -390,6 +422,16 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Transfer
                     
                     client.SendAgentAlertMessage("Unable to delete "+
                             "received inventory" + reason, false);
+                }
+                // Tell client about updates to original parent and new parent (this should probably be factored with existing move item/folder code).
+                else if (previousParentFolderID != null)
+                {
+                    InventoryFolderBase previousParentFolder
+                        = new InventoryFolderBase((UUID)previousParentFolderID, client.AgentId);
+                    previousParentFolder = invService.GetFolder(previousParentFolder);
+                    scene.SendInventoryUpdate(client, previousParentFolder, true, true);
+
+                    scene.SendInventoryUpdate(client, trashFolder, true, true);
                 }
 
                 ScenePresence user = scene.GetScenePresence(new UUID(im.toAgentID));

OpenSim/Region/CoreModules/Avatar/Lure/HGLureModule.cs
@@ -186,7 +186,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Lure
                     client.FirstName+" "+client.LastName, targetid,
                     (byte)InstantMessageDialog.RequestTeleport, false,
                     message, sessionID, false, presence.AbsolutePosition,
-                    new Byte[0]);
+                    new Byte[0], true);
             m.RegionID = client.Scene.RegionInfo.RegionID.Guid;
 
             m_log.DebugFormat("[HG LURE MODULE]: RequestTeleport sessionID={0}, regionID={1}, message={2}", m.imSessionID, m.RegionID, m.message);

OpenSim/Region/CoreModules/Avatar/Lure/LureModule.cs
@@ -169,7 +169,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Lure
                     client.FirstName+" "+client.LastName, targetid,
                     (byte)InstantMessageDialog.RequestTeleport, false,
                     message, dest, false, presence.AbsolutePosition,
-                    new Byte[0]);
+                    new Byte[0], true);
                     
             if (m_TransferModule != null)
             {

OpenSim/Region/CoreModules/Framework/EntityTransfer/EntityTransferModule.cs
@@ -1068,6 +1068,7 @@ namespace OpenSim.Region.CoreModules.Framework.EntityTransfer
             Scene initiatingScene)
         {
             Thread.Sleep(10000);
+            
             IMessageTransferModule im = initiatingScene.RequestModuleInterface<IMessageTransferModule>();
             if (im != null)
             {
@@ -1080,11 +1081,22 @@ namespace OpenSim.Region.CoreModules.Framework.EntityTransfer
                     (uint)(int)position.X,
                     (uint)(int)position.Y,
                     (uint)(int)position.Z);
-                GridInstantMessage m = new GridInstantMessage(initiatingScene, UUID.Zero,
-                "Region", agent.UUID,
-                (byte)InstantMessageDialog.GodLikeRequestTeleport, false,
-                "", gotoLocation, false, new Vector3(127, 0, 0),
-                new Byte[0]);
+
+                GridInstantMessage m
+                    = new GridInstantMessage(
+                        initiatingScene,
+                        UUID.Zero,
+                        "Region",
+                        agent.UUID,
+                        (byte)InstantMessageDialog.GodLikeRequestTeleport,
+                        false,
+                        "",
+                        gotoLocation,
+                        false,
+                        new Vector3(127, 0, 0),
+                        new Byte[0],
+                        false);
+
                 im.SendInstantMessage(m, delegate(bool success)
                 {
                     m_log.DebugFormat("[ENTITY TRANSFER MODULE]: Client Initiating Teleport sending IM success = {0}", success);

commit ab0294f0109416da0546e25797e7747b23071d33

Add missing category paremeter to llGiveInventoryList grid IM construction.
This was a regression from 16c9c1df Sat Oct 6 02:34:49 2012 +0100.
Should resolve http://opensimulator.org/mantis/view.php?id=6360

OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -6459,7 +6459,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 GridInstantMessage msg = new GridInstantMessage(World,
                         m_host.OwnerID, m_host.Name, destID,
                         (byte)InstantMessageDialog.TaskInventoryOffered,
-                        false, string.Format("'{0}'"),
+                        false, string.Format("'{0}'", category),
 // We won't go so far as to add a SLURL, but this is the format used by LL as of 2012-10-06                                       
 // false, string.Format("'{0}'  ( http://slurl.com/secondlife/{1}/{2}/{3}/{4} )", category, World.Name, (int)pos.X, (int)pos.Y, (int)pos.Z),
                         folderID, false, pos,