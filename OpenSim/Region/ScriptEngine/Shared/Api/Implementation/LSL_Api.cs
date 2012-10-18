ec044537d50b425178c4e081abcf325d28028480
SignpostMarv <github@signpostmarv.name>
2012-10-18 10:15:34 +0100

    ensuring that operand order in llGetLinkName is internally consistent

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -3828,7 +3828,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             }
 
             // simplest case, this prims link number
-            if (m_host.LinkNum == linknum)
+            if (linknum == m_host.LinkNum)
                 return m_host.Name;
 
             // Single prim
