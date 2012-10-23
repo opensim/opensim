b49f8a377b9ac542096a0bc8ad30b11942c27413
Robert Adams <Robert.Adams@intel.com>
2012-10-23 08:02:26 -0700

    BulletSim: minor change to insure avatar body recreation when shape changes.

--- OpenSim/Region/Physics/BulletSPlugin/BSShapeCollection.cs
+++ OpenSim/Region/Physics/BulletSPlugin/BSShapeCollection.cs
@@ -338,6 +338,7 @@ public class BSShapeCollection : IDisposable
             ret = GetReferenceToNativeShape(prim, shapeData, ShapeData.PhysicsShapeType.SHAPE_AVATAR,
                             ShapeData.FixedShapeKey.KEY_CAPSULE, shapeCallback);
             DetailLog("{0},BSShapeCollection.CreateGeom,avatarCapsule,shape={1}", prim.LocalID, prim.BSShape);
+            ret = true;
             haveShape = true;
         }
         // If the prim attributes are simple, this could be a simple Bullet native shape
@@ -411,14 +412,13 @@ public class BSShapeCollection : IDisposable
                             ShapeData.PhysicsShapeType shapeType, ShapeData.FixedShapeKey shapeKey,
                             ShapeDestructionCallback shapeCallback)
     {
+        // release any previous shape
+        DereferenceShape(prim.BSShape, true, shapeCallback);
 
         shapeData.Type = shapeType;
         // Bullet native objects are scaled by the Bullet engine so pass the size in
         prim.Scale = shapeData.Size;
         shapeData.Scale = shapeData.Size;
-
-        // release any previous shape
-        DereferenceShape(prim.BSShape, true, shapeCallback);
 
         BulletShape newShape = BuildPhysicalNativeShape(shapeType, shapeData, shapeKey);
 
