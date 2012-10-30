aeeed29d627f7d40bbcc160bf446a019b54e8b32
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2012-10-30 01:07:14 +0000

    correct ODEPrim.MeshAssetReveived -> MeshAssetReceived

--- OpenSim/Region/Physics/OdePlugin/ODEPrim.cs
+++ OpenSim/Region/Physics/OdePlugin/ODEPrim.cs
@@ -3335,7 +3335,6 @@ Console.WriteLine(" JointCreateFixed");
             m_material = pMaterial;
         }
 
-
         private void CheckMeshAsset()
         {
             if (_pbs.SculptEntry && !m_assetFailed && _pbs.SculptTexture != UUID.Zero)
@@ -3345,12 +3344,12 @@ Console.WriteLine(" JointCreateFixed");
                     {
                         RequestAssetDelegate assetProvider = _parent_scene.RequestAssetMethod;
                         if (assetProvider != null)
-                            assetProvider(_pbs.SculptTexture, MeshAssetReveived);
+                            assetProvider(_pbs.SculptTexture, MeshAssetReceived);
                     });
             }
         }
 
-        void MeshAssetReveived(AssetBase asset)
+        void MeshAssetReceived(AssetBase asset)
         {
             if (asset.Data != null && asset.Data.Length > 0)
             {
