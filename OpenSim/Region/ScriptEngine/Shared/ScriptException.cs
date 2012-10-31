566aaef1f4c7eade864d9e3d7b7b6ded8e0156a1
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2012-10-31 00:56:41 +0000

    Also add the additional ScriptException constructor necessary to get [Serializable] to work.

--- OpenSim/Region/ScriptEngine/Shared/ScriptException.cs
+++ OpenSim/Region/ScriptEngine/Shared/ScriptException.cs
@@ -26,6 +26,7 @@
  */
 
 using System;
+using System.Runtime.Serialization;
 
 namespace OpenSim.Region.ScriptEngine.Shared
 {
@@ -37,5 +38,7 @@ namespace OpenSim.Region.ScriptEngine.Shared
         public ScriptException(string message) : base(message) {}
 
         public ScriptException(string message, Exception innerException) : base(message, innerException) {}
+
+        public ScriptException(SerializationInfo info, StreamingContext context) :base(info, context) {}
     }
 }
