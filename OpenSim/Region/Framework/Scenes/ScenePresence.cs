f9051c22d8170bb2824d9a38540f2fdce4367f6e
teravus <teravus@gmail.com>
2012-12-19 22:14:20 -0500

    * Send an Unsolicited AgentDataUpdate packet upon Root Agent Status to synchronize with Agni.

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -925,6 +925,26 @@ namespace OpenSim.Region.Framework.Scenes
 
             m_scene.EventManager.TriggerSetRootAgentScene(m_uuid, m_scene);
 
+            UUID groupUUID = UUID.Zero;
+            string GroupName = string.Empty;
+            ulong groupPowers = 0;
+
+            // ----------------------------------
+            // Previous Agent Difference - AGNI sends an unsolicited AgentDataUpdate upon root agent status
+            if (gm != null)
+            {
+                groupUUID = ControllingClient.ActiveGroupId;
+                GroupRecord record = gm.GetGroupRecord(groupUUID);
+                if (record != null)
+                    GroupName = record.GroupName;
+                GroupMembershipData groupMembershipData = gm.GetMembershipData(groupUUID, m_uuid);
+                if (groupMembershipData != null)
+                    groupPowers = groupMembershipData.GroupPowers;
+            }
+            ControllingClient.SendAgentDataUpdate(m_uuid, groupUUID, Firstname, Lastname, groupPowers, GroupName,
+                                      Grouptitle);
+            // ------------------------------------
+
             if (ParentID == 0)
             {
                 // Moved this from SendInitialData to ensure that Appearance is initialized
