8c6984eac140ed48f10ac3f3db533d0c9b1d084a
Melanie <melanie@t-data.com>
2013-01-23 23:12:48 +0100

    Implement get version RemoteAdmin call

--- OpenSim/Framework/Servers/ServerBase.cs
+++ OpenSim/Framework/Servers/ServerBase.cs
@@ -531,7 +531,7 @@ namespace OpenSim.Framework.Servers
             }
         }
 
-        protected string GetVersionText()
+        public string GetVersionText()
         {
             return String.Format("Version: {0} (interface version {1})", m_version, VersionInfo.MajorInterfaceVersion);
         }
