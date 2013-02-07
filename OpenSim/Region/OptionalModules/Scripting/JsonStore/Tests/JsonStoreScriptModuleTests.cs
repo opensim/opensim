2e86978b609e3e2013a8f4c53f9afc9ed239d20b
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2013-02-07 23:30:03 +0000

    Add TestJsonDestoreStoreNotExists()
    This still returns true even if we ask to destroy a store that does not exist.
    Need to check that this is more appropriate behaviour.

--- OpenSim/Region/OptionalModules/Scripting/JsonStore/Tests/JsonStoreScriptModuleTests.cs
+++ OpenSim/Region/OptionalModules/Scripting/JsonStore/Tests/JsonStoreScriptModuleTests.cs
@@ -135,6 +135,20 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore.Tests
         }
 
         [Test]
+        public void TestJsonDestroyStoreNotExists()
+        {
+            TestHelpers.InMethod();
+//            TestHelpers.EnableLogging();
+
+            UUID fakeStoreId = TestHelpers.ParseTail(0x500);
+
+            int dsrv = (int)InvokeOp("JsonDestroyStore", fakeStoreId);
+
+            // XXX: Current returns 'true' even though no such store existed.  Need to ask if this is best behaviour.
+            Assert.That(dsrv, Is.EqualTo(1));
+        }
+
+        [Test]
         public void TestJsonGetValue()
         {
             TestHelpers.InMethod();
