opensim/opensim

commit e17392acbb46e1e48e169069a822f8b814762214

Enables script access to the per object dynamic attributes through the JsonStore script functions. Adds JsonAttachObjectStore to associate a store identifier with an object (scripts can only access the store in their host object, this could be extended but isn't necessary for now).
Note this opens a method to the DAMap OSDMap. This will be removed later, but
greatly simplifies the code for now.

The JsonStore and these scripts are disabled by default.

OpenSim/Region/OptionalModules/Scripting/JsonStore/JsonStore.cs
@@ -49,7 +49,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
         private static readonly ILog m_log =
             LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        private OSD m_ValueStore;
+        protected virtual OSD ValueStore { get; set; }
 
         protected class TakeValueCallbackClass
         {
@@ -108,17 +108,18 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
         /// 
         /// </summary>
         // -----------------------------------------------------------------
-        public JsonStore() : this("") {}
-        
-        public JsonStore(string value)
+        public JsonStore() 
         {
             m_TakeStore = new List<TakeValueCallbackClass>();
             m_ReadStore = new List<TakeValueCallbackClass>();
-           
+        }
+        
+        public JsonStore(string value)
+        {
             if (String.IsNullOrEmpty(value))
-                m_ValueStore = new OSDMap();
+                ValueStore = new OSDMap();
             else
-                m_ValueStore = OSDParser.DeserializeJson(value);
+                ValueStore = OSDParser.DeserializeJson(value);
         }
 
         // -----------------------------------------------------------------
@@ -129,7 +130,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
         public bool TestPath(string expr, bool useJson)
         {
             Stack<string> path = ParsePathExpression(expr);
-            OSD result = ProcessPathExpression(m_ValueStore,path);
+            OSD result = ProcessPathExpression(ValueStore,path);
 
             if (result == null)
                 return false;
@@ -148,7 +149,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
         public bool GetValue(string expr, out string value, bool useJson)
         {
             Stack<string> path = ParsePathExpression(expr);
-            OSD result = ProcessPathExpression(m_ValueStore,path);
+            OSD result = ProcessPathExpression(ValueStore,path);
             return ConvertOutputValue(result,out value,useJson); 
         }
      
@@ -184,7 +185,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             Stack<string> path = ParsePathExpression(expr);
             string pexpr = PathExpressionToKey(path);
 
-            OSD result = ProcessPathExpression(m_ValueStore,path);
+            OSD result = ProcessPathExpression(ValueStore,path);
             if (result == null)
             {
                 m_TakeStore.Add(new TakeValueCallbackClass(pexpr,useJson,cback));
@@ -215,7 +216,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             Stack<string> path = ParsePathExpression(expr);
             string pexpr = PathExpressionToKey(path);
 
-            OSD result = ProcessPathExpression(m_ValueStore,path);
+            OSD result = ProcessPathExpression(ValueStore,path);
             if (result == null)
             {
                 m_ReadStore.Add(new TakeValueCallbackClass(pexpr,useJson,cback));
@@ -245,7 +246,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             Stack<string> path = ParsePathExpression(expr);
             if (path.Count == 0)
             {
-                m_ValueStore = ovalue;
+                ValueStore = ovalue;
                 return true;
             }
 
@@ -254,7 +255,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             if (pexpr != "")
                 pexpr += ".";
 
-            OSD result = ProcessPathExpression(m_ValueStore,path);
+            OSD result = ProcessPathExpression(ValueStore,path);
             if (result == null)
                 return false;
 
@@ -522,4 +523,41 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             return pkey;
         }
     }
+
+    public class JsonObjectStore : JsonStore
+    {
+        private static readonly ILog m_log =
+            LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private Scene m_scene;
+        private UUID m_objectID;
+
+        protected override OSD ValueStore 
+        {
+            get
+            {
+                SceneObjectPart sop = m_scene.GetSceneObjectPart(m_objectID);
+                if (sop == null)
+                {
+                    // This is bad
+                    return null;
+                }
+                
+                return sop.DynAttrs.TopLevelMap;
+            }
+
+            // cannot set the top level
+            set
+            {
+                m_log.InfoFormat("[JsonStore] cannot set top level value in object store");
+            }
+        }
+
+        public JsonObjectStore(Scene scene, UUID oid) : base()
+        {
+            m_scene = scene;
+            m_objectID = oid;
+        }
+    }
+    
 }

OpenSim/Region/OptionalModules/Scripting/JsonStore/JsonStoreModule.cs
@@ -175,6 +175,34 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
         /// 
         /// </summary>
         // -----------------------------------------------------------------
+        public bool AttachObjectStore(UUID objectID)
+        {
+            if (! m_enabled) return false;
+
+            SceneObjectPart sop = m_scene.GetSceneObjectPart(objectID);
+            if (sop == null)
+            {
+                m_log.InfoFormat("[JsonStore] unable to attach to unknown object; {0}",objectID);
+                return false;
+            }
+
+            lock (m_JsonValueStore)
+            {
+                if (m_JsonValueStore.ContainsKey(objectID))
+                    return true;
+                
+                JsonStore map = new JsonObjectStore(m_scene,objectID);
+                m_JsonValueStore.Add(objectID,map);
+            }
+            
+            return true;
+        }
+        
+        // -----------------------------------------------------------------
+        /// <summary>
+        /// 
+        /// </summary>
+        // -----------------------------------------------------------------
         public bool CreateStore(string value, ref UUID result)
         {
             if (result == UUID.Zero)

OpenSim/Framework/DAMap.cs
@@ -73,6 +73,14 @@ namespace OpenSim.Framework
                 m_map = (OSDMap)OSDParser.DeserializeLLSDXml(rawXml);         
         }
         
+        // WARNING: this is temporary for experimentation only, it will be removed!!!!
+        public OSDMap TopLevelMap
+        {
+            get { return m_map; }
+            set { m_map = value; }
+        }
+        
+
         public void ReadXml(XmlReader reader)
         { 
             ReadXml(reader.ReadInnerXml());            

OpenSim/Region/Framework/Interfaces/IJsonStoreModule.cs
@@ -35,6 +35,7 @@ namespace OpenSim.Region.Framework.Interfaces
 
     public interface IJsonStoreModule
     {
+        bool AttachObjectStore(UUID objectID);
         bool CreateStore(string value, ref UUID result);
         bool DestroyStore(UUID storeID);
         bool TestStore(UUID storeID);

OpenSim/Region/OptionalModules/Scripting/JsonStore/JsonStoreScriptModule.cs
@@ -169,6 +169,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
                     m_comms.RegisterScriptInvocations(this);
 
                     // m_comms.RegisterScriptInvocation(this, "JsonCreateStore");
+                    // m_comms.RegisterScriptInvocation(this, "JsonAttachObjectStore");
                     // m_comms.RegisterScriptInvocation(this, "JsonDestroyStore");
                     // m_comms.RegisterScriptInvocation(this, "JsonTestStore");
 
@@ -214,6 +215,21 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
 #endregion
 
 #region ScriptInvocationInteface
+        // -----------------------------------------------------------------
+        /// <summary>
+        /// 
+        /// </summary>
+        // -----------------------------------------------------------------
+        [ScriptInvocation]
+        public UUID JsonAttachObjectStore(UUID hostID, UUID scriptID)
+        {
+            UUID uuid = UUID.Zero;
+            if (! m_store.AttachObjectStore(hostID))
+                GenerateRuntimeError("Failed to create Json store");
+            
+            return hostID;
+        }
+
         // -----------------------------------------------------------------
         /// <summary>
         /// 

commit c135c3224fcdc88a610b0d66da0c0dd6cd1211f9

Fix a recent regression in e17392a where JsonSetValue() stopped working (probably other functions as well).
Fix is to call through to the no-arg constructor from the string constructor in JsonStore, which I suspect was just forgotten.
This was actually picked up by the TestJsonSetValue() regression test failing
But this isn't being run on jenkins due to the .net version issue.
This commit also puts the full stack trace in logged messages and makes these error level messages instead of info

OpenSim/Region/OptionalModules/Scripting/JsonStore/JsonStore.cs
@@ -114,7 +114,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             m_ReadStore = new List<TakeValueCallbackClass>();
         }
         
-        public JsonStore(string value)
+        public JsonStore(string value) : this()
         {
             if (String.IsNullOrEmpty(value))
                 ValueStore = new OSDMap();

OpenSim/Region/OptionalModules/Scripting/JsonStore/JsonStoreModule.cs
@@ -93,12 +93,12 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             }
             catch (Exception e)
             {
-                m_log.ErrorFormat("[JsonStore] initialization error: {0}",e.Message);
+                m_log.Error("[JsonStore]: initialization error: {0}", e);
                 return;
             }
 
             if (m_enabled)
-                m_log.DebugFormat("[JsonStore] module is enabled");
+                m_log.DebugFormat("[JsonStore]: module is enabled");
         }
 
         // -----------------------------------------------------------------
@@ -182,7 +182,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             SceneObjectPart sop = m_scene.GetSceneObjectPart(objectID);
             if (sop == null)
             {
-                m_log.InfoFormat("[JsonStore] unable to attach to unknown object; {0}",objectID);
+                m_log.ErrorFormat("[JsonStore] unable to attach to unknown object; {0}", objectID);
                 return false;
             }
 
@@ -219,7 +219,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             }
             catch (Exception e)
             {
-                m_log.InfoFormat("[JsonStore] Unable to initialize store from {0}; {1}",value,e.Message);
+                m_log.Error(string.Format("[JsonStore]: Unable to initialize store from {0}", value), e);
                 return false;
             }
 
@@ -283,7 +283,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             }
             catch (Exception e)
             {
-                m_log.InfoFormat("[JsonStore] Path test failed for {0} in {1}; {2}",path,storeID,e.Message);
+                m_log.Error(string.Format("[JsonStore]: Path test failed for {0} in {1}", path, storeID), e);
             }
 
             return false;
@@ -316,7 +316,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             }
             catch (Exception e)
             {
-                m_log.InfoFormat("[JsonStore] Unable to assign {0} to {1} in {2}; {3}",value,path,storeID,e.Message);
+                m_log.Error(string.Format("[JsonStore]: Unable to assign {0} to {1} in {2}", value, path, storeID), e);
             }
 
             return false;
@@ -349,7 +349,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             }
             catch (Exception e)
             {
-                m_log.InfoFormat("[JsonStore] Unable to remove {0} in {1}; {2}",path,storeID,e.Message);
+                m_log.Error(string.Format("[JsonStore]: Unable to remove {0} in {1}", path, storeID), e);
             }
 
             return false;
@@ -382,7 +382,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             }
             catch (Exception e)
             {
-                m_log.InfoFormat("[JsonStore] unable to retrieve value; {0}",e.Message);
+                m_log.Error("[JsonStore]: unable to retrieve value", e);
             }
             
             return false;
@@ -421,7 +421,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             }
             catch (Exception e)
             {
-                m_log.InfoFormat("[JsonStore] unable to retrieve value; {0}",e.ToString());
+                m_log.Error("[JsonStore] unable to retrieve value", e);
             }
             
             cback(String.Empty);
@@ -460,7 +460,7 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore
             }
             catch (Exception e)
             {
-                m_log.InfoFormat("[JsonStore] unable to retrieve value; {0}",e.ToString());
+                m_log.Error("[JsonStore]: unable to retrieve value", e);
             }
             
             cback(String.Empty);

OpenSim/Region/OptionalModules/Scripting/JsonStore/Tests/JsonStoreScriptModuleTests.cs
@@ -184,13 +184,13 @@ namespace OpenSim.Region.OptionalModules.Scripting.JsonStore.Tests
             TestHelpers.InMethod();
 //            TestHelpers.EnableLogging();
 
-            UUID storeId = (UUID)InvokeOp("JsonCreateStore", "{}"); 
+            UUID storeId = (UUID)InvokeOp("JsonCreateStore", "{ }"); 
 
-            int result = (int)InvokeOp("JsonSetValue", storeId, "Hello", "World");
+            int result = (int)InvokeOp("JsonSetValue", storeId, "Fun", "Times");
             Assert.That(result, Is.EqualTo(1));
 
-            string value = (string)InvokeOp("JsonGetValue", storeId, "Hello");
-            Assert.That(value, Is.EqualTo("World"));
+            string value = (string)InvokeOp("JsonGetValue", storeId, "Fun");
+            Assert.That(value, Is.EqualTo("Times"));
         }
 
         public object DummyTestMethod(object o1, object o2, object o3, object o4, object o5) { return null; }