53122fad400e2d5bac5f02a3e153b6e76c9112b3
dahlia <dahlia@nomail>
2013-04-17 23:10:02 -0700

    Thanks lkalif for a fix to SendRegionHandshake() which fixes a potential crash with Server-side baking enabled viewers

--- OpenSim/Region/ClientStack/Linden/UDP/LLClientView.cs
+++ OpenSim/Region/ClientStack/Linden/UDP/LLClientView.cs
@@ -791,10 +791,14 @@ namespace OpenSim.Region.ClientStack.LindenUDP
             handshake.RegionInfo3.ColoName = Utils.EmptyBytes;
             handshake.RegionInfo3.ProductName = Util.StringToBytes256(regionInfo.RegionType);
             handshake.RegionInfo3.ProductSKU = Utils.EmptyBytes;
-            handshake.RegionInfo4 = new RegionHandshakePacket.RegionInfo4Block[0];
-            
+
+            handshake.RegionInfo4 = new RegionHandshakePacket.RegionInfo4Block[1];
+            handshake.RegionInfo4[0].RegionFlagsExtended = args.regionFlags;
+            handshake.RegionInfo4[0].RegionProtocols = 0; // 1 here would indicate that SSB is supported
+
             OutPacket(handshake, ThrottleOutPacketType.Task);
         }
+
 
         public void MoveAgentIntoRegion(RegionInfo regInfo, Vector3 pos, Vector3 look)
         {
