90f03ccd42a045bb30f33ed866feb38677ed7780
Diva Canto <diva@metaverseink.com>
2013-05-06 11:47:55 -0700

    Added new method to Remote Admin for reloading the estate settings. This is meant to be called when some other program has changed the data on the backend.

--- OpenSim/ApplicationPlugins/RemoteController/RemoteAdminPlugin.cs
+++ OpenSim/ApplicationPlugins/RemoteController/RemoteAdminPlugin.cs
@@ -157,6 +157,7 @@ namespace OpenSim.ApplicationPlugins.RemoteController
                     availableMethods["admin_acl_add"] = (req, ep) => InvokeXmlRpcMethod(req, ep, XmlRpcAccessListAdd);
                     availableMethods["admin_acl_remove"] = (req, ep) => InvokeXmlRpcMethod(req, ep, XmlRpcAccessListRemove);
                     availableMethods["admin_acl_list"] = (req, ep) => InvokeXmlRpcMethod(req, ep, XmlRpcAccessListList);
+                    availableMethods["admin_estate_reload"] = (req, ep) => InvokeXmlRpcMethod(req, ep, XmlRpcEstateReload);
 
                     // Either enable full remote functionality or just selected features
                     string enabledMethods = m_config.GetString("enabled_methods", "all");
@@ -1760,6 +1761,22 @@ namespace OpenSim.ApplicationPlugins.RemoteController
             responseData["success"] = true;
 
             m_log.Info("[RADMIN]: Access List List Request complete");
+        }
+
+        private void XmlRpcEstateReload(XmlRpcRequest request, XmlRpcResponse response, IPEndPoint remoteClient)
+        {
+            m_log.Info("[RADMIN]: Received Estate Reload Request");
+
+            Hashtable responseData = (Hashtable)response.Value;
+            Hashtable requestData = (Hashtable)request.Params[0];
+
+            m_application.SceneManager.ForEachScene(s => 
+                s.RegionInfo.EstateSettings = m_application.EstateDataService.LoadEstateSettings(s.RegionInfo.RegionID, false)                
+            );
+
+            responseData["success"] = true;
+
+            m_log.Info("[RADMIN]: Estate Reload Request complete");
         }
 
         private void XmlRpcGetAgentsMethod(XmlRpcRequest request, XmlRpcResponse response, IPEndPoint remoteClient)
