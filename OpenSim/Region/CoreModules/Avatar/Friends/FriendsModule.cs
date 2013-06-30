8fa5d12fcb73ffcc7144009e0147febcfa3ce704
Diva Canto <diva@metaverseink.com>
2013-06-29 21:26:58 -0700

    One more debug mantis #6625

--- OpenSim/Region/CoreModules/Avatar/Friends/FriendsModule.cs
+++ OpenSim/Region/CoreModules/Avatar/Friends/FriendsModule.cs
@@ -292,6 +292,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Friends
                     friendsData = new UserFriendData();
                     friendsData.PrincipalID = agentID;
                     friendsData.Friends = GetFriendsFromService(client);
+                    m_log.DebugFormat("[FRIENDS MODULE]: User has {1} friends", friendsData.Friends.Length);
                     friendsData.Refcount = 1;
 
                     m_Friends[agentID] = friendsData;
