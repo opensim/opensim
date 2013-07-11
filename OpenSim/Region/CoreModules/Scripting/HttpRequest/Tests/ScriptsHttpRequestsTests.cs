opensim/opensim

commit 831e4c38506140e9ece2db4b96b4f0960a0276a8

Fix bug where outstanding llHTTPRequests for scripts were not being aborted when they were deleted.
This was because AsyncCommandManager was handing an item ID to IHttpRequestModule.StopHttpRequest() rather than the expected request ID.
This commit also makes the http request asynchronous using BeginGetResponse() rather than doing this by launching a new thread
so that we can more safely abort it via HttpWebRequest.Abort() rather than aborting the thread itself.
This also renames StopHttpRequest() to StopHttpRequestsForScript() since any outstanding requests are now aborted and/or removed.

OpenSim/Region/CoreModules/Scripting/HttpRequest/ScriptsHttpRequests.cs
@@ -28,12 +28,15 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Net.Security;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Security.Cryptography.X509Certificates;
+using log4net;
 using Nini.Config;
 using OpenMetaverse;
 using OpenSim.Framework;
@@ -250,18 +253,29 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
             return reqID;
         }
 
-        public void StopHttpRequest(uint m_localID, UUID m_itemID)
+        public void StopHttpRequestsForScript(UUID id)
         {
             if (m_pendingRequests != null)
             {
+                List<UUID> keysToRemove = null;
+
                 lock (HttpListLock)
                 {
-                    HttpRequestClass tmpReq;
-                    if (m_pendingRequests.TryGetValue(m_itemID, out tmpReq))
+                    foreach (HttpRequestClass req in m_pendingRequests.Values)
                     {
-                        tmpReq.Stop();
-                        m_pendingRequests.Remove(m_itemID);
+                        if (req.ItemID == id)
+                        {
+                            req.Stop();
+
+                            if (keysToRemove == null)
+                                keysToRemove = new List<UUID>();
+
+                            keysToRemove.Add(req.ReqID);
+                        }
                     }
+
+                    if (keysToRemove != null)
+                        keysToRemove.ForEach(keyToRemove => m_pendingRequests.Remove(keyToRemove));
                 }
             }
         }
@@ -362,6 +376,8 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
 
     public class HttpRequestClass: IServiceRequest
     {
+//        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         // Constants for parameters
         // public const int HTTP_BODY_MAXLENGTH = 2;
         // public const int HTTP_METHOD = 0;
@@ -419,12 +435,7 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
 
         public void Process()
         {
-            httpThread = new Thread(SendRequest);
-            httpThread.Name = "HttpRequestThread";
-            httpThread.Priority = ThreadPriority.BelowNormal;
-            httpThread.IsBackground = true;
-            _finished = false;
-            httpThread.Start();
+            SendRequest();
         }
 
         /*
@@ -435,10 +446,6 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
         public void SendRequest()
         {
             HttpWebResponse response = null;
-            StringBuilder sb = new StringBuilder();
-            byte[] buf = new byte[8192];
-            string tempString = null;
-            int count = 0;
 
             try
             {
@@ -497,11 +504,12 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
                     bstream.Close();
                 }
 
-                Request.Timeout = HttpTimeout;
                 try
                 {
-                    // execute the request
-                    response = (HttpWebResponse) Request.GetResponse();
+                    IAsyncResult result = (IAsyncResult)Request.BeginGetResponse(ResponseCallback, null);
+
+                    ThreadPool.RegisterWaitForSingleObject(
+                        result.AsyncWaitHandle, new WaitOrTimerCallback(TimeoutCallback), null, HttpTimeout, true);
                 }
                 catch (WebException e)
                 {
@@ -510,11 +518,31 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
                         throw;
                     }
                     response = (HttpWebResponse)e.Response;
+                    _finished = true;
                 }
+            }
+            catch (Exception e)
+            {
+                Status = (int)OSHttpStatusCode.ClientErrorJoker;
+                ResponseBody = e.Message;
+                _finished = true;
+            }
+        }
 
+        private void ResponseCallback(IAsyncResult ar)
+        {
+            HttpWebResponse response = null;
+
+            try
+            {
+                response = (HttpWebResponse)Request.EndGetResponse(ar);
                 Status = (int)response.StatusCode;
 
                 Stream resStream = response.GetResponseStream();
+                StringBuilder sb = new StringBuilder();
+                byte[] buf = new byte[8192];
+                string tempString = null;
+                int count = 0;
 
                 do
                 {
@@ -530,36 +558,40 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
                         // continue building the string
                         sb.Append(tempString);
                     }
-                } while (count > 0); // any more data to read?
+                } 
+                while (count > 0); // any more data to read?
 
-                ResponseBody = sb.ToString();
+                ResponseBody = sb.ToString();         
             }
             catch (Exception e)
             {
                 Status = (int)OSHttpStatusCode.ClientErrorJoker;
                 ResponseBody = e.Message;
 
-                _finished = true;
-                return;
+//                m_log.Debug(
+//                    string.Format("[SCRIPTS HTTP REQUESTS]: Exception on response to {0} for {1}  ", Url, ItemID), e);
             }
             finally
             {
                 if (response != null)
                     response.Close();
-            }
 
-            _finished = true;
+                _finished = true;
+            }
+        }
+
+        private void TimeoutCallback(object state, bool timedOut)
+        {
+            if (timedOut)
+                Request.Abort();
         }
 
         public void Stop()
         {
-            try
-            {
-                httpThread.Abort();
-            }
-            catch (Exception)
-            {
-            }
+//            m_log.DebugFormat("[SCRIPTS HTTP REQUESTS]: Stopping request to {0} for {1}  ", Url, ItemID);
+
+            if (Request != null)
+                Request.Abort();
         }
     }
 }

OpenSim/Region/Framework/Interfaces/IHttpRequests.cs
@@ -45,7 +45,13 @@ namespace OpenSim.Region.Framework.Interfaces
     {
         UUID MakeHttpRequest(string url, string parameters, string body);
         UUID StartHttpRequest(uint localID, UUID itemID, string url, List<string> parameters, Dictionary<string, string> headers, string body);
-        void StopHttpRequest(uint m_localID, UUID m_itemID);
+
+        /// <summary>
+        /// Stop and remove all http requests for the given script.
+        /// </summary>
+        /// <param name='id'></param>
+        void StopHttpRequestsForScript(UUID id);
+
         IServiceRequest GetNextCompletedRequest();
         void RemoveCompletedRequest(UUID id);
     }

OpenSim/Region/ScriptEngine/Shared/Api/Implementation/AsyncCommandManager.cs
@@ -28,7 +28,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Threading;
+using log4net;
 using OpenMetaverse;
 using OpenSim.Framework;
 using OpenSim.Framework.Monitoring;
@@ -45,6 +47,8 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
     /// </summary>
     public class AsyncCommandManager
     {
+//        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         private static Thread cmdHandlerThread;
         private static int cmdHandlerThreadCycleSleepms;
 
@@ -225,6 +229,8 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
         /// <param name="itemID"></param>
         public static void RemoveScript(IScriptEngine engine, uint localID, UUID itemID)
         {
+//            m_log.DebugFormat("[ASYNC COMMAND MANAGER]: Removing facilities for script {0}", itemID);
+
             // Remove a specific script
 
             // Remove dataserver events
@@ -236,7 +242,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             // Remove from: HttpRequest
             IHttpRequestModule iHttpReq = engine.World.RequestModuleInterface<IHttpRequestModule>();
             if (iHttpReq != null)
-                iHttpReq.StopHttpRequest(localID, itemID);
+                iHttpReq.StopHttpRequestsForScript(itemID);
 
             IWorldComm comms = engine.World.RequestModuleInterface<IWorldComm>();
             if (comms != null)

commit 44e9849ed1190dbc29ffa97fa5df286dc9794edb

Fix regression where llHTTPRequests which did not get an OK response returned 499 and the exception message in the http_response event rather than the actual response code and body.
This was a regression since commit 831e4c3 (Thu Apr 4 00:36:15 2013)
This commit also adds a regression test for this case, though this currently only works with Mono
This aims to address http://opensimulator.org/mantis/view.php?id=6704

OpenSim/Framework/Util.cs
@@ -141,6 +141,11 @@ namespace OpenSim.Framework
         public static FireAndForgetMethod DefaultFireAndForgetMethod = FireAndForgetMethod.SmartThreadPool;
         public static FireAndForgetMethod FireAndForgetMethod = DefaultFireAndForgetMethod;
 
+        public static bool IsPlatformMono
+        {
+            get { return Type.GetType("Mono.Runtime") != null; }
+        }
+
         /// <summary>
         /// Gets the name of the directory where the current running executable
         /// is located
@@ -1326,7 +1331,7 @@ namespace OpenSim.Framework
                     ru = "OSX/Mono";
                 else
                 {
-                    if (Type.GetType("Mono.Runtime") != null)
+                    if (IsPlatformMono)
                         ru = "Win/Mono";
                     else
                         ru = "Win/.NET";

OpenSim/Region/CoreModules/Scripting/HttpRequest/ScriptsHttpRequests.cs
@@ -419,7 +419,7 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
             get { return _reqID; }
             set { _reqID = value; }
         }
-        public HttpWebRequest Request;
+        public WebRequest Request;
         public string ResponseBody;
         public List<string> ResponseMetadata;
         public Dictionary<string, string> ResponseHeaders;
@@ -431,18 +431,11 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
             SendRequest();
         }
 
-        /*
-         * TODO: More work on the response codes.  Right now
-         * returning 200 for success or 499 for exception
-         */
-
         public void SendRequest()
         {
-            HttpWebResponse response = null;
-
             try
             {
-                Request = (HttpWebRequest) WebRequest.Create(Url);
+                Request = WebRequest.Create(Url);
                 Request.Method = HttpMethod;
                 Request.ContentType = HttpMIMEType;
 
@@ -480,14 +473,17 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
                     }
                 }
 
-                foreach (KeyValuePair<string, string> entry in ResponseHeaders)
-                    if (entry.Key.ToLower().Equals("user-agent"))
-                        Request.UserAgent = entry.Value;
-                    else
-                        Request.Headers[entry.Key] = entry.Value;
+                if (ResponseHeaders != null)
+                {
+                    foreach (KeyValuePair<string, string> entry in ResponseHeaders)
+                        if (entry.Key.ToLower().Equals("user-agent") && Request is HttpWebRequest)
+                            ((HttpWebRequest)Request).UserAgent = entry.Value;
+                        else
+                            Request.Headers[entry.Key] = entry.Value;
+                }
 
                 // Encode outbound data
-                if (OutboundBody.Length > 0)
+                if (OutboundBody != null && OutboundBody.Length > 0)
                 {
                     byte[] data = Util.UTF8.GetBytes(OutboundBody);
 
@@ -510,12 +506,19 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
                     {
                         throw;
                     }
-                    response = (HttpWebResponse)e.Response;
+
+                    HttpWebResponse response = (HttpWebResponse)e.Response;
+
+                    Status = (int)response.StatusCode;
+                    ResponseBody = response.StatusDescription;
                     _finished = true;
                 }
             }
             catch (Exception e)
             {
+//                m_log.Debug(
+//                    string.Format("[SCRIPTS HTTP REQUESTS]: Exception on request to {0} for {1}  ", Url, ItemID), e);
+
                 Status = (int)OSHttpStatusCode.ClientErrorJoker;
                 ResponseBody = e.Message;
                 _finished = true;
@@ -528,33 +531,27 @@ namespace OpenSim.Region.CoreModules.Scripting.HttpRequest
 
             try
             {
-                response = (HttpWebResponse)Request.EndGetResponse(ar);
+                try
+                {
+                    response = (HttpWebResponse)Request.EndGetResponse(ar);
+                }
+                catch (WebException e)
+                {
+                    if (e.Status != WebExceptionStatus.ProtocolError)
+                    {
+                        throw;
+                    }
+
+                    response = (HttpWebResponse)e.Response;
+                }
+
                 Status = (int)response.StatusCode;
 
-                Stream resStream = response.GetResponseStream();
-                StringBuilder sb = new StringBuilder();
-                byte[] buf = new byte[8192];
-                string tempString = null;
-                int count = 0;
-
-                do
+                using (Stream stream = response.GetResponseStream())
                 {
-                    // fill the buffer with data
-                    count = resStream.Read(buf, 0, buf.Length);
-
-                    // make sure we read some data
-                    if (count != 0)
-                    {
-                        // translate from bytes to ASCII text
-                        tempString = Util.UTF8.GetString(buf, 0, count);
-
-                        // continue building the string
-                        sb.Append(tempString);
-                    }
+                    StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                    ResponseBody = reader.ReadToEnd();
                 } 
-                while (count > 0); // any more data to read?
-
-                ResponseBody = sb.ToString();         
             }
             catch (Exception e)
             {

prebuild.xml
@@ -3102,6 +3102,7 @@
         <Match path="Avatar/Inventory/Transfer/Tests" pattern="*.cs" recurse="true"/>
         <Match path="Framework/InventoryAccess/Tests" pattern="*.cs" recurse="true"/>
         <Match path="Framework/UserManagement/Tests" pattern="*.cs" recurse="true"/>
+        <Match path="Scripting/HttpRequest/Tests" pattern="*.cs" recurse="true"/>
         <Match path="Scripting/VectorRender/Tests" pattern="*.cs" recurse="true"/>
         <Match path="World/Archiver/Tests" pattern="*.cs" recurse="true"/>
         <Match buildAction="EmbeddedResource" path="World/Archiver/Tests/Resources" pattern="*"/>

OpenSim/Region/CoreModules/Scripting/HttpRequest/Tests/ScriptsHttpRequestsTests.cs
@@ -1,0 +1,201 @@
+/*
+ * Copyright (c) Contributors, http://opensimulator.org/
+ * See CONTRIBUTORS.TXT for a full list of copyright holders.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSimulator Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using log4net.Config;
+using NUnit.Framework;
+using OpenMetaverse;
+using OpenMetaverse.Assets;
+using OpenSim.Framework;
+using OpenSim.Region.CoreModules.Scripting.HttpRequest;
+using OpenSim.Region.Framework.Scenes;
+using OpenSim.Tests.Common;
+using OpenSim.Tests.Common.Mock;
+
+namespace OpenSim.Region.CoreModules.Scripting.HttpRequest.Tests
+{
+    class TestWebRequestCreate : IWebRequestCreate
+    {       
+        public TestWebRequest NextRequest { get; set; }
+
+        public WebRequest Create(Uri uri)
+        {
+//            NextRequest.RequestUri = uri;
+
+            return NextRequest;
+
+//            return new TestWebRequest(new SerializationInfo(typeof(TestWebRequest), new FormatterConverter()), new StreamingContext());
+        }
+    }
+
+    class TestWebRequest : WebRequest
+    {
+        public override string ContentType { get; set; }
+        public override string Method { get; set; }
+
+        public Func<IAsyncResult, WebResponse> OnEndGetResponse { get; set; }
+
+        public TestWebRequest() : base() 
+        {
+//            Console.WriteLine("created");
+        }
+
+//        public TestWebRequest(SerializationInfo serializationInfo, StreamingContext streamingContext) 
+//            : base(serializationInfo, streamingContext) 
+//        {
+//            Console.WriteLine("created");
+//        }
+
+        public override IAsyncResult BeginGetResponse(AsyncCallback callback, object state)
+        {
+//            Console.WriteLine("bish");
+            TestAsyncResult tasr = new TestAsyncResult();
+            callback(tasr);
+
+            return tasr;
+        }
+
+        public override WebResponse EndGetResponse(IAsyncResult asyncResult)
+        {
+//            Console.WriteLine("bosh");
+            return OnEndGetResponse(asyncResult);
+        }
+    }
+
+    class TestHttpWebResponse : HttpWebResponse
+    {
+        public string Response { get; set; }
+
+        public TestHttpWebResponse(SerializationInfo serializationInfo, StreamingContext streamingContext) 
+            : base(serializationInfo, streamingContext) {}
+
+        public override Stream GetResponseStream()
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(Response));
+        }
+    }
+
+    class TestAsyncResult : IAsyncResult
+    {
+        WaitHandle m_wh = new ManualResetEvent(true);
+
+        object IAsyncResult.AsyncState 
+        {
+            get {
+                throw new System.NotImplementedException ();
+            }
+        }
+
+        WaitHandle IAsyncResult.AsyncWaitHandle 
+        {
+            get { return m_wh; }
+        }
+
+        bool IAsyncResult.CompletedSynchronously 
+        {
+            get { return false; }
+        }
+
+        bool IAsyncResult.IsCompleted 
+        {
+            get { return true; }
+        }
+    }
+
+    /// <summary>
+    /// Test script http request code.
+    /// </summary>
+    /// <remarks>
+    /// This class uses some very hacky workarounds in order to mock HttpWebResponse which are Mono dependent (though
+    /// alternative code can be written to make this work for Windows).  However, the value of being able to
+    /// regression test this kind of code is very high.
+    /// </remarks>
+    [TestFixture]
+    public class ScriptsHttpRequestsTests : OpenSimTestCase
+    {
+        /// <summary>
+        /// Test what happens when we get a 404 response from a call.
+        /// </summary>
+        [Test]
+        public void Test404Response()
+        {
+            TestHelpers.InMethod();
+            TestHelpers.EnableLogging();
+
+            if (!Util.IsPlatformMono)
+                Assert.Ignore("Ignoring test since can only currently run on Mono");           
+
+            string rawResponse = "boom";
+
+            TestWebRequestCreate twrc = new TestWebRequestCreate();
+
+            TestWebRequest twr = new TestWebRequest();
+            //twr.OnEndGetResponse += ar => new TestHttpWebResponse(null, new StreamingContext());
+            twr.OnEndGetResponse += ar => 
+            {
+                SerializationInfo si = new SerializationInfo(typeof(HttpWebResponse), new FormatterConverter());
+                StreamingContext sc = new StreamingContext();
+//                WebHeaderCollection headers = new WebHeaderCollection();
+//                si.AddValue("m_HttpResponseHeaders", headers);
+                si.AddValue("uri", new Uri("test://arrg"));
+//                si.AddValue("m_Certificate", null);
+                si.AddValue("version", HttpVersion.Version11);
+                si.AddValue("statusCode", HttpStatusCode.NotFound);
+                si.AddValue("contentLength", 0);
+                si.AddValue("method", "GET");
+                si.AddValue("statusDescription", "Not Found");
+                si.AddValue("contentType", null);
+                si.AddValue("cookieCollection", new CookieCollection());
+
+                TestHttpWebResponse thwr = new TestHttpWebResponse(si, sc);
+                thwr.Response = rawResponse;
+
+                throw new WebException("no message", null, WebExceptionStatus.ProtocolError, thwr);
+            };
+
+            twrc.NextRequest = twr;
+
+            WebRequest.RegisterPrefix("test", twrc);
+            HttpRequestClass hr = new HttpRequestClass();
+            hr.Url = "test://something";
+            hr.SendRequest();
+
+            while (!hr.Finished)
+                Thread.Sleep(100);
+
+            Assert.That(hr.Status, Is.EqualTo((int)HttpStatusCode.NotFound));
+            Assert.That(hr.ResponseBody, Is.EqualTo(rawResponse));
+        }
+    }
+}