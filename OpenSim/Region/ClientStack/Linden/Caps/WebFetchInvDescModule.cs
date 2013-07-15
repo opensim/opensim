687c1a420a6f3c254eb7104fee40a79fc6a39e1d
Diva Canto <diva@metaverseink.com>
2013-07-15 12:33:31 -0700

    Guard against null ref

--- OpenSim/Region/ClientStack/Linden/Caps/WebFetchInvDescModule.cs
+++ OpenSim/Region/ClientStack/Linden/Caps/WebFetchInvDescModule.cs
@@ -351,7 +351,8 @@ namespace OpenSim.Region.ClientStack.Linden
 
                 aPollRequest poolreq = m_queue.Dequeue();
 
-                poolreq.thepoll.Process(poolreq);
+                if (poolreq != null && poolreq.thepoll != null)
+                    poolreq.thepoll.Process(poolreq);
             }
         }
     }
