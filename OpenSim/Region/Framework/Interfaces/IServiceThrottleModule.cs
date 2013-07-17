a006caabbcfe6e2a2b4a6118d217d3bd9abd4d99
Diva Canto <diva@metaverseink.com>
2013-07-16 17:06:54 -0700

    Added IServiceThrottleModule.cs

---
+++ OpenSim/Region/Framework/Interfaces/IServiceThrottleModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenSim.Region.Framework.Interfaces
+{
+    public interface IServiceThrottleModule
+    {
+        void Enqueue(Action continuation);
+    }
+
+}
