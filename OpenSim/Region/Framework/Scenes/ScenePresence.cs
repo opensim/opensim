78649eb0999a5f97cc541c94ec98d1d06ed957cb
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2013-11-23 01:35:08 +0000

    Refix fix for sitting on non-root linked prims with explicit sit targets.
    I forgot that m_post is being set inconsistently between non-explicit and explicit ragets

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -2590,7 +2590,7 @@ namespace OpenSim.Region.Framework.Scenes
 
                     //Quaternion result = (sitTargetOrient * vq) * nq;
 
-                    m_pos = sitTargetPos + SIT_TARGET_ADJUSTMENT - part.GroupPosition;
+                    m_pos = sitTargetPos + SIT_TARGET_ADJUSTMENT + part.OffsetPosition;
                     Rotation = sitTargetOrient;
                     ParentPosition = part.AbsolutePosition;
                 }
