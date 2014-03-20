opensim/opensim

commit 346644016c529db6ed63aa866c7cf91cec1b635f

If HGSuitcaseInventoryService.GetRootFolder() fails to create a suitcase folder when required, then don't try to store the null and perform other operations on it.
Patch from http://opensimulator.org/mantis/view.php?id=6844
Thanks Kira.

OpenSim/Services/HypergridService/HGSuitcaseInventoryService.cs
@@ -213,16 +213,23 @@ namespace OpenSim.Services.HypergridService
                 // In the DB we tag it as type 100, but we use -1 (Unknown) outside
                 suitcase = CreateFolder(principalID, root.folderID, 100, "My Suitcase");
                 if (suitcase == null)
+                {
                     m_log.ErrorFormat("[HG SUITCASE INVENTORY SERVICE]: Unable to create suitcase folder");
-                m_Database.StoreFolder(suitcase);
+                }
+                else
+                {
+                    m_Database.StoreFolder(suitcase);
 
-                // Create System folders
-                CreateSystemFolders(principalID, suitcase.folderID);
+                    // Create System folders
+                    CreateSystemFolders(principalID, suitcase.folderID);
+
+                    SetAsNormalFolder(suitcase);
+
+                    return ConvertToOpenSim(suitcase);
+                }
             }
 
-            SetAsNormalFolder(suitcase);
-
-            return ConvertToOpenSim(suitcase);
+            return null;
         }
 
         protected void CreateSystemFolders(UUID principalID, UUID rootID)

commit 7f027552ecd604977faa1d23ff17e1b619cbeaa6

If we find an existing suitcase folder in HGSuitcaseInventoryService.GetRootFolder(), then return that as a response rather than null.
Fixes a regression from commit 346644016c529db6ed63aa866c7cf91cec1b635f (Fri Nov 15 23:10:59 2013)
This is actually the reverse of zadark's patch in http://opensimulator.org/mantis/view.php?id=6969
But I'm assuming that was a mistake since clearly we should be returning this data - this was original behaviour

OpenSim/Services/HypergridService/HGSuitcaseInventoryService.cs
@@ -228,6 +228,10 @@ namespace OpenSim.Services.HypergridService
                     return ConvertToOpenSim(suitcase);
                 }
             }
+            else
+            {
+                return ConvertToOpenSim(suitcase);
+            }
 
             return null;
         }