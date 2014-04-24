bc06db3df45d8dd61338c5b1426fd0161731f7cd
Oren Hurvitz <orenh@kitely.com>
2014-04-24 14:21:05 +0300

    - Created a standard function to send XML-RPC requests, which logs them like we do for other types of HTTP activity. - Changed OpenProfileClient to use the new XML-RPC sending function - Improved logging in WebUtil

--- OpenSim/Framework/Servers/HttpServer/BaseHttpServer.cs
+++ OpenSim/Framework/Servers/HttpServer/BaseHttpServer.cs
@@ -705,7 +705,7 @@ namespace OpenSim.Framework.Servers.HttpServer
                             }
                         }
 
-                        WebUtil.LogResponseDetail(output);
+                        WebUtil.LogResponseDetail(null, output);
                     }
 
                     if (!response.SendChunked && response.ContentLength64 <= 0)
