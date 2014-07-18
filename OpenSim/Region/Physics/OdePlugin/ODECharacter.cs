748fc66ef10591fb0cf3c82d6838561bf48ac197
UbitUmarov <ajlduarte@sapo.pt>
2014-07-18 13:19:24 +0100

    reduce avatar hover in basic ode plugin

--- OpenSim/Region/Physics/OdePlugin/ODECharacter.cs
+++ OpenSim/Region/Physics/OdePlugin/ODECharacter.cs
@@ -500,7 +500,7 @@ namespace OpenSim.Region.Physics.OdePlugin
             {
                 m_pidControllerActive = true;
 
-                m_tainted_CAPSULE_LENGTH = (size.Z * 1.15f) - CAPSULE_RADIUS * 2.0f;
+                m_tainted_CAPSULE_LENGTH = (size.Z) - CAPSULE_RADIUS * 2.0f;
 //                    m_log.Info("[ODE CHARACTER]: " + CAPSULE_LENGTH);
             }
             else
