36920adb9684b201ff9020dd825c448fb4e09613
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2014-07-21 23:58:30 +0100

    minor: convert tabs to spaces that got in on recent commit 4a9282e

--- OpenSim/Server/Handlers/Profiles/UserProfilesHandlers.cs
+++ OpenSim/Server/Handlers/Profiles/UserProfilesHandlers.cs
@@ -128,12 +128,12 @@ namespace OpenSim.Server.Handlers
             OSDMap request = (OSDMap)json["params"];
             UUID classifiedId = new UUID(request["classifiedId"].AsString());
             
-			if(Service.ClassifiedDelete(classifiedId))
-				return true;
+            if (Service.ClassifiedDelete(classifiedId))
+                return true;
 
-			response.Error.Code = ErrorCode.InternalError;
-			response.Error.Message = "data error removing record";
-			return false;
+            response.Error.Code = ErrorCode.InternalError;
+            response.Error.Message = "data error removing record";
+            return false;
         }
         
         public bool ClassifiedInfoRequest(OSDMap json, ref JsonRpcResponse response)
