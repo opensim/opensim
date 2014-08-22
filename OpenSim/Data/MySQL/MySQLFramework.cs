d899bdcb9bbf1b52f91daabacfb630059757fcea
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2014-08-22 19:52:03 +0100

    Remove lock in MySQLFramework.  This is not necessary as the connection is not shared.

--- OpenSim/Data/MySQL/MySQLFramework.cs
+++ OpenSim/Data/MySQL/MySQLFramework.cs
@@ -45,36 +45,27 @@ namespace OpenSim.Data.MySQL
                 System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         protected string m_connectionString;
-        protected object m_dbLock = new object();
 
         protected MySqlFramework(string connectionString)
         {
             m_connectionString = connectionString;
         }
 
-        //////////////////////////////////////////////////////////////
-        //
-        // All non queries are funneled through one connection
-        // to increase performance a little
-        //
         protected int ExecuteNonQuery(MySqlCommand cmd)
         {
-            lock (m_dbLock)
+            using (MySqlConnection dbcon = new MySqlConnection(m_connectionString))
             {
-                using (MySqlConnection dbcon = new MySqlConnection(m_connectionString))
-                {
-                    dbcon.Open();
-                    cmd.Connection = dbcon;
+                dbcon.Open();
+                cmd.Connection = dbcon;
 
-                    try
-                    {
-                        return cmd.ExecuteNonQuery();
-                    }
-                    catch (Exception e)
-                    {
-                        m_log.Error(e.Message, e);
-                        return 0;
-                    }
+                try
+                {
+                    return cmd.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    m_log.Error(e.Message, e);
+                    return 0;
                 }
             }
         }
