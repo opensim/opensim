a02dae566ce510e7f43c6324996e70442cba3f46
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2014-08-30 00:26:18 +0100

    Fix issue where moving a seated avatar would not adjust their subsequent stand position.
    Addresses http://opensimulator.org/mantis/view.php?id=7299

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -2631,7 +2631,7 @@ namespace OpenSim.Region.Framework.Scenes
 
 //                Vector3 standPositionAdjustment 
 //                    = part.SitTargetPosition + new Vector3(0.5f, 0f, m_sitAvatarHeight / 2f);
-                Vector3 adjustmentForSitPosition = part.SitTargetPosition * part.GetWorldRotation();
+                Vector3 adjustmentForSitPosition = (part.SitTargetPosition + OffsetPosition) * part.GetWorldRotation();
 
                 // XXX: This is based on the physics capsule sizes.  Need to find a better way to read this rather than
                 // hardcoding here.
