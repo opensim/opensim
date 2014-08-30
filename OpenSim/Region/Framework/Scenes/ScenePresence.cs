473c559478ab56872f0e9d1bea3971b849df0b15
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2014-08-30 01:09:33 +0100

    As per the LL grid, if an avatar is sitting then return it's velocity relative to the region rather than relative to its seat.
    Resolves http://opensimulator.org/mantis/view.php?id=7175

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -622,9 +622,14 @@ namespace OpenSim.Region.Framework.Scenes
 //                        "[SCENE PRESENCE]: Set velocity {0} for {1} in {2} via getting Velocity!",
 //                        m_velocity, Name, Scene.RegionInfo.RegionName);
                 }
+                else if (ParentPart != null)
+                {
+                    return ParentPart.ParentGroup.Velocity;
+                }
 
                 return m_velocity;
             }
+
             set
             {
                 if (PhysicsActor != null)
