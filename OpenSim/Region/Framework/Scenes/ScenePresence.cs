6d2e924f248941e34919b62fe383415d7e255370
Melanie Thielker <melanie@t-data.com>
2014-11-10 04:46:51 +0100

    Call the bake module each time we see a new attachment to see if the attachment needs textures

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -4583,6 +4583,10 @@ namespace OpenSim.Region.Framework.Scenes
 
                 m_attachments.Add(gobj);
             }
+
+            IBakedTextureModule bakedModule = m_scene.RequestModuleInterface<IBakedTextureModule>();
+            if (bakedModule != null)
+                bakedModule.UpdateMeshAvatar(m_uuid);
         }
 
         /// <summary>
