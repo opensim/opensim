4b8215c8933ecc358cd33b04f849ca3a40750fe2
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2015-01-20 00:18:25 +0000

    minor: comment out unusued logging objects in some pCampbot behaviour classes

--- OpenSim/Tools/pCampBot/Behaviours/TwitchyBehaviour.cs
+++ OpenSim/Tools/pCampBot/Behaviours/TwitchyBehaviour.cs
@@ -44,7 +44,7 @@ namespace pCampBot
     /// </summary>
     public class TwitchyBehaviour : AbstractBehaviour
     {
-        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+//        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         public TwitchyBehaviour() 
         { 
