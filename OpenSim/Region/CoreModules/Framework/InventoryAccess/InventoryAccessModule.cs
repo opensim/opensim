opensim/opensim

commit f132f642b23d9d0c336354a0bc4bb95c41023c34

On code section that rezzes single objects and attachments, reduce CPU use by reading asset XML a single time with a stream reader rather than multiple times.
Reading large XML documents (e.g. complex attachments) is CPU expensive - this must be done as few times as possible (preferably just once).
Reading these documents into XmlDocument is also more resource intensive than using XmlTextReader, as per Microsoft's own publication "Improve .NET Application Performance and Scalability"
Optimization of other cases will follow if this change is successful.

OpenSim/Region/CoreModules/Framework/InventoryAccess/InventoryAccessModule.cs
@@ -798,7 +798,7 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
             UUID RayTargetID, byte BypassRayCast, bool RayEndIsIntersection,
             bool RezSelected, bool RemoveItem, UUID fromTaskID, bool attachment)
         {
-            AssetBase rezAsset = m_Scene.AssetService.Get(assetID.ToString());
+            AssetBase rezAsset = m_Scene.AssetService.Get(assetID.ToString());           
 
             if (rezAsset == null)
             {
@@ -829,7 +829,9 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
             byte bRayEndIsIntersection = (byte)(RayEndIsIntersection ? 1 : 0);
             Vector3 pos;
 
-            bool single = m_Scene.GetObjectsToRez(rezAsset.Data, attachment, out objlist, out veclist, out bbox, out offsetHeight);
+            bool single 
+                = m_Scene.GetObjectsToRez(
+                    rezAsset.Data, attachment, out objlist, out veclist, out bbox, out offsetHeight);
 
             if (single)
             {

OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
@@ -902,6 +902,34 @@ namespace OpenSim.Region.Framework.Scenes
             }
         }
 
+        public void LoadScriptState(XmlTextReader reader)
+        {
+//            m_log.DebugFormat("[SCENE OBJECT GROUP]: Looking for script state for {0} in {1}", Name);
+
+            while (reader.ReadToFollowing("SavedScriptState"))
+            {
+//                m_log.DebugFormat("[SCENE OBJECT GROUP]: Loading script state for {0}", Name);
+
+                if (m_savedScriptState == null)
+                    m_savedScriptState = new Dictionary<UUID, string>();
+
+                string uuid = reader.GetAttribute("UUID");
+
+                if (uuid != null)
+                {
+//                    m_log.DebugFormat("[SCENE OBJECT GROUP]: Found state for item ID {0} in object {1}", uuid, Name);
+
+                    UUID itemid = new UUID(uuid);
+                    if (itemid != UUID.Zero)
+                        m_savedScriptState[itemid] = reader.ReadInnerXml();
+                }
+                else
+                {
+                    m_log.WarnFormat("[SCENE OBJECT GROUP]: SavedScriptState element had no UUID in object {0}", Name);
+                }
+            }
+        }
+
         /// <summary>
         /// Hooks this object up to the backup event so that it is persisted to the database when the update thread executes.
         /// </summary>

OpenSim/Region/CoreModules/Framework/InventoryAccess/Tests/InventoryAccessModuleTests.cs
@@ -111,6 +111,7 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess.Tests
             InventoryFolderBase objsFolder 
                 = InventoryArchiveUtils.FindFoldersByPath(m_scene.InventoryService, m_userId, "Objects")[0];
             item1.Folder = objsFolder.ID;
+            item1.Flags |= (uint)InventoryItemFlags.ObjectHasMultipleItems;
             m_scene.AddInventoryItem(item1);
             
             SceneObjectGroup so 

OpenSim/Region/Framework/Scenes/Scene.Inventory.cs
@@ -30,6 +30,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Timers;
 using System.Xml;
 using OpenMetaverse;
@@ -2192,60 +2193,84 @@ namespace OpenSim.Region.Framework.Scenes
         /// Returns one object if the asset is a regular object, and multiple objects for a coalesced object.
         /// </remarks>
         /// <param name="assetData">Asset data</param>
-        /// <param name="attachment">Whether the item is an attachment</param>
+        /// <param name="isAttachment">True if the object is an attachment.</param>
         /// <param name="objlist">The objects included in the asset</param>
         /// <param name="veclist">Relative positions of the objects</param>
         /// <param name="bbox">Bounding box of all the objects</param>
         /// <param name="offsetHeight">Offset in the Z axis from the centre of the bounding box
         /// to the centre of the root prim (relevant only when returning a single object)</param>
-        /// <returns>true = returning a single object; false = multiple objects</returns>
-        public bool GetObjectsToRez(byte[] assetData, bool attachment, out List<SceneObjectGroup> objlist, out List<Vector3> veclist,
+        /// <returns>
+        /// true if returning a single object or deserialization fails, false if returning the coalesced
+        /// list of objects
+        /// </returns>
+        public bool GetObjectsToRez(
+            byte[] assetData, bool isAttachment, out List<SceneObjectGroup> objlist, out List<Vector3> veclist,
             out Vector3 bbox, out float offsetHeight)
         {
             objlist = new List<SceneObjectGroup>();
             veclist = new List<Vector3>();
 
-            XmlDocument doc = new XmlDocument();
             string xmlData = Utils.BytesToString(assetData);
-            doc.LoadXml(xmlData);
-            XmlElement e = (XmlElement)doc.SelectSingleNode("/CoalescedObject");
 
-            if (e == null || attachment) // Single
+            try
             {
-                SceneObjectGroup g = SceneObjectSerializer.FromOriginalXmlFormat(xmlData);
-                objlist.Add(g);
-                veclist.Add(new Vector3(0, 0, 0));
-                bbox = g.GetAxisAlignedBoundingBox(out offsetHeight);
-                return true;
-            }
-            else
-            {
-                XmlElement coll = (XmlElement)e;
-                float bx = Convert.ToSingle(coll.GetAttribute("x"));
-                float by = Convert.ToSingle(coll.GetAttribute("y"));
-                float bz = Convert.ToSingle(coll.GetAttribute("z"));
-                bbox = new Vector3(bx, by, bz);
-                offsetHeight = 0;
-
-                XmlNodeList groups = e.SelectNodes("SceneObjectGroup");
-                foreach (XmlNode n in groups)
+                using (XmlTextReader reader = new XmlTextReader(xmlData, XmlNodeType.Element, null))
                 {
-                    SceneObjectGroup g = SceneObjectSerializer.FromOriginalXmlFormat(n.OuterXml);
-                    objlist.Add(g);
+                    reader.Read();
+                    bool isSingleObject = reader.Name != "CoalescedObject";
 
-                    XmlElement el = (XmlElement)n;
-                    string rawX = el.GetAttribute("offsetx");
-                    string rawY = el.GetAttribute("offsety");
-                    string rawZ = el.GetAttribute("offsetz");
+                    if (isSingleObject || isAttachment)
+                    {
+                        SceneObjectGroup g = SceneObjectSerializer.FromOriginalXmlFormat(reader);
+                        objlist.Add(g);
+                        veclist.Add(Vector3.Zero);
+                        bbox = g.GetAxisAlignedBoundingBox(out offsetHeight);
+                        return true;
+                    }
+                    else
+                    {                
+                        XmlDocument doc = new XmlDocument();
+                        doc.LoadXml(xmlData);
+                        XmlElement e = (XmlElement)doc.SelectSingleNode("/CoalescedObject");
+                        XmlElement coll = (XmlElement)e;
+                        float bx = Convert.ToSingle(coll.GetAttribute("x"));
+                        float by = Convert.ToSingle(coll.GetAttribute("y"));
+                        float bz = Convert.ToSingle(coll.GetAttribute("z"));
+                        bbox = new Vector3(bx, by, bz);
+                        offsetHeight = 0;
 
-                    float x = Convert.ToSingle(rawX);
-                    float y = Convert.ToSingle(rawY);
-                    float z = Convert.ToSingle(rawZ);
-                    veclist.Add(new Vector3(x, y, z));
+                        XmlNodeList groups = e.SelectNodes("SceneObjectGroup");
+                        foreach (XmlNode n in groups)
+                        {
+                            SceneObjectGroup g = SceneObjectSerializer.FromOriginalXmlFormat(n.OuterXml);
+                            objlist.Add(g);
+
+                            XmlElement el = (XmlElement)n;
+                            string rawX = el.GetAttribute("offsetx");
+                            string rawY = el.GetAttribute("offsety");
+                            string rawZ = el.GetAttribute("offsetz");
+
+                            float x = Convert.ToSingle(rawX);
+                            float y = Convert.ToSingle(rawY);
+                            float z = Convert.ToSingle(rawZ);
+                            veclist.Add(new Vector3(x, y, z));
+                        }
+
+                        return false;
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                m_log.Error(
+                    "[AGENT INVENTORY]: Deserialization of xml failed when looking for CoalescedObject tag.  Exception ", 
+                    e);
 
-            return false;
+                bbox = Vector3.Zero;
+                offsetHeight = 0;
+            }
+
+            return true;
         }
 
         /// <summary>

OpenSim/Region/Framework/Scenes/Serialization/SceneObjectSerializer.cs
@@ -59,57 +59,58 @@ namespace OpenSim.Region.Framework.Scenes.Serialization
         /// <returns>The scene object deserialized.  Null on failure.</returns>
         public static SceneObjectGroup FromOriginalXmlFormat(string xmlData)
         {
+            using (XmlTextReader reader = new XmlTextReader(xmlData, XmlNodeType.Element, null))
+                return FromOriginalXmlFormat(reader);
+        }
+
+        /// <summary>
+        /// Deserialize a scene object from the original xml format
+        /// </summary>
+        /// <param name="xmlData"></param>
+        /// <returns>The scene object deserialized.  Null on failure.</returns>
+        public static SceneObjectGroup FromOriginalXmlFormat(XmlTextReader reader)
+        {
             //m_log.DebugFormat("[SOG]: Starting deserialization of SOG");
             //int time = System.Environment.TickCount;
 
+            SceneObjectGroup sceneObject = null;
+
             try
             {
-                StringReader  sr;
-                XmlTextReader reader;
-                XmlNodeList   parts;
-                XmlDocument   doc;
                 int           linkNum;
 
-                doc = new XmlDocument();
-                doc.LoadXml(xmlData);
-                parts = doc.GetElementsByTagName("RootPart");
+                reader.ReadToFollowing("RootPart");
+                reader.ReadToFollowing("SceneObjectPart");
+                sceneObject = new SceneObjectGroup(SceneObjectPart.FromXml(reader));
+                reader.ReadToFollowing("OtherParts");
 
-                if (parts.Count == 0)
-                    throw new Exception("Invalid Xml format - no root part");
-
-                sr = new StringReader(parts[0].InnerXml);
-                reader = new XmlTextReader(sr);
-                SceneObjectGroup sceneObject = new SceneObjectGroup(SceneObjectPart.FromXml(reader));
-                reader.Close();
-                sr.Close();
-
-                parts = doc.GetElementsByTagName("Part");
-
-                for (int i = 0; i < parts.Count; i++)
+                if (reader.ReadToDescendant("Part"))
                 {
-                    sr = new StringReader(parts[i].InnerXml);
-                    reader = new XmlTextReader(sr);
-                    SceneObjectPart part = SceneObjectPart.FromXml(reader);
-                    linkNum = part.LinkNum;
-                    sceneObject.AddPart(part);
-                    part.LinkNum = linkNum;
-                    part.TrimPermissions();
-                    reader.Close();
-                    sr.Close();
+                    do
+                    {
+                        if (reader.ReadToDescendant("SceneObjectPart"))
+                        {
+                            SceneObjectPart part = SceneObjectPart.FromXml(reader);
+                            linkNum = part.LinkNum;
+                            sceneObject.AddPart(part);
+                            part.LinkNum = linkNum;
+                            part.TrimPermissions();
+                        }
+                    }                    
+                    while (reader.ReadToNextSibling("Part"));
                 }
 
                 // Script state may, or may not, exist. Not having any, is NOT
                 // ever a problem.
-                sceneObject.LoadScriptState(doc);
-
-                return sceneObject;
+                sceneObject.LoadScriptState(reader);
             }
             catch (Exception e)
             {
-                m_log.ErrorFormat(
-                    "[SERIALIZER]: Deserialization of xml failed with {0}.  xml was {1}", e, xmlData);
+                m_log.ErrorFormat("[SERIALIZER]: Deserialization of xml failed.  Exception {0}", e);
                 return null;
             }
+
+            return sceneObject;
         }
 
         /// <summary>

commit d0a2ea0857c1342a42db31bce282c62c88c57e55

Fix regression where the stored state of every second script in an object rezzed from inventory (e.g. attachments) was no longer loaded.
Likely a regression since f132f642 (2014-08-28)
Relates to http://opensimulator.org/mantis/view.php?id=7278

OpenSim/Region/CoreModules/Framework/InventoryAccess/InventoryAccessModule.cs
@@ -297,6 +297,10 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
         {
             if (item != null && item.Owner == ownerID && asset != null)
             {
+//                m_log.DebugFormat(
+//                    "[INVENTORY ACCESS MODULE]: Updating item {0} {1} with new asset {2}", 
+//                    item.Name, item.ID, asset.ID);
+
                 item.AssetID = asset.FullID;
                 item.Description = asset.Description;
                 item.Name = asset.Name;
@@ -314,7 +318,6 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
                     (item == null || asset == null? "null item or asset" : "wrong owner"));
                 return false;
             }
-
         }
 
         public virtual List<InventoryItemBase> CopyToInventory(

OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
@@ -911,28 +911,36 @@ namespace OpenSim.Region.Framework.Scenes
 
         public void LoadScriptState(XmlReader reader)
         {
-//            m_log.DebugFormat("[SCENE OBJECT GROUP]: Looking for script state for {0} in {1}", Name);
+//            m_log.DebugFormat("[SCENE OBJECT GROUP]: Looking for script state for {0}", Name);
 
-            while (reader.ReadToFollowing("SavedScriptState"))
+            while (true)
             {
-//                m_log.DebugFormat("[SCENE OBJECT GROUP]: Loading script state for {0}", Name);
-
-                if (m_savedScriptState == null)
-                    m_savedScriptState = new Dictionary<UUID, string>();
-
-                string uuid = reader.GetAttribute("UUID");
-
-                if (uuid != null)
+                if (reader.Name == "SavedScriptState" && reader.NodeType == XmlNodeType.Element)
                 {
-//                    m_log.DebugFormat("[SCENE OBJECT GROUP]: Found state for item ID {0} in object {1}", uuid, Name);
+//                    m_log.DebugFormat("[SCENE OBJECT GROUP]: Loading script state for {0}", Name);
 
-                    UUID itemid = new UUID(uuid);
-                    if (itemid != UUID.Zero)
-                        m_savedScriptState[itemid] = reader.ReadInnerXml();
+                    if (m_savedScriptState == null)
+                        m_savedScriptState = new Dictionary<UUID, string>();
+
+                    string uuid = reader.GetAttribute("UUID");
+
+                    if (uuid != null)
+                    {
+//                        m_log.DebugFormat("[SCENE OBJECT GROUP]: Found state for item ID {0} in object {1}", uuid, Name);
+
+                        UUID itemid = new UUID(uuid);
+                        if (itemid != UUID.Zero)
+                            m_savedScriptState[itemid] = reader.ReadInnerXml();
+                    }
+                    else
+                    {
+                        m_log.WarnFormat("[SCENE OBJECT GROUP]: SavedScriptState element had no UUID in object {0}", Name);
+                    }
                 }
                 else
                 {
-                    m_log.WarnFormat("[SCENE OBJECT GROUP]: SavedScriptState element had no UUID in object {0}", Name);
+                    if (!reader.Read())
+                        break;
                 }
             }
         }

OpenSim/Region/Framework/Scenes/SceneObjectPartInventory.cs
@@ -400,6 +400,10 @@ namespace OpenSim.Region.Framework.Scenes
 
         private UUID RestoreSavedScriptState(UUID loadedID, UUID oldID, UUID newID)
         {
+//            m_log.DebugFormat(
+//                "[PRIM INVENTORY]: Restoring scripted state for item {0}, oldID {1}, loadedID {2}", 
+//                newID, oldID, loadedID);
+
             IScriptModule[] engines = m_part.ParentGroup.Scene.RequestModuleInterfaces<IScriptModule>();
             if (engines.Length == 0) // No engine at all
                 return oldID;
@@ -412,7 +416,7 @@ namespace OpenSim.Region.Framework.Scenes
                 XmlDocument doc = new XmlDocument();
 
                 doc.LoadXml(m_part.ParentGroup.m_savedScriptState[stateID]);
-                
+
                 ////////// CRUFT WARNING ///////////////////////////////////
                 //
                 // Old objects will have <ScriptState><State> ...
@@ -441,6 +445,8 @@ namespace OpenSim.Region.Framework.Scenes
 
                     // This created document has only the minimun data
                     // necessary for XEngine to parse it successfully
+
+//                    m_log.DebugFormat("[PRIM INVENTORY]: Adding legacy state {0} in {1}", stateID, newID);
 
                     m_part.ParentGroup.m_savedScriptState[stateID] = newDoc.OuterXml;
                 }

OpenSim/Region/ScriptEngine/Shared/Instance/ScriptInstance.cs
@@ -1043,6 +1043,9 @@ namespace OpenSim.Region.ScriptEngine.Shared.Instance
 
         public void SetVars(Dictionary<string, object> vars)
         {
+//            foreach (KeyValuePair<string, object> kvp in vars)
+//                m_log.DebugFormat("[SCRIPT INSTANCE]: Setting var {0}={1}", kvp.Key, kvp.Value);
+
             m_Script.SetVars(vars);
         }
 