opensim/opensim

commit faaf47a86f34f24dec0d7d51eda638d0df3db305

Prevent a race condition between the script engine backup thread and script removal by locking on the script's EventQueue and only proceeding if it's flagged as still running.
Relates to http://opensimulator.org/mantis/view.php?id=7407

OpenSim/Region/ScriptEngine/Shared/Instance/ScriptInstance.cs
@@ -564,7 +564,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Instance
             }
         }
 
-        public bool Stop(int timeout)
+        public bool Stop(int timeout, bool clearEventQueue = false)
         {
             if (DebugLevel >= 1)
                 m_log.DebugFormat(
@@ -575,6 +575,9 @@ namespace OpenSim.Region.ScriptEngine.Shared.Instance
 
             lock (EventQueue)
             {
+                if (clearEventQueue)
+                    ClearQueue();
+
                 if (!Running)
                     return true;
 
@@ -1065,45 +1068,52 @@ namespace OpenSim.Region.ScriptEngine.Shared.Instance
 
         public void SaveState()
         {
-            // If we're currently in an event, just tell it to save upon return
-            //
-            if (m_InEvent)
+            // We need to lock here to avoid any race with a thread that is removing this script.
+            lock (EventQueue)
             {
-                m_SaveState = true;
-                return;
-            }
+                if (!Running)
+                    return;
 
-//            m_log.DebugFormat(
-//                "[SCRIPT INSTANCE]: Saving state for script {0} (id {1}) in part {2} (id {3}) in object {4} in {5}",
-//                ScriptTask.Name, ScriptTask.ItemID, Part.Name, Part.UUID, Part.ParentGroup.Name, Engine.World.Name);
-
-            PluginData = AsyncCommandManager.GetSerializationData(Engine, ItemID);
-
-            string xml = ScriptSerializer.Serialize(this);
-
-            // Compare hash of the state we just just created with the state last written to disk
-            // If the state is different, update the disk file.
-            UUID hash = UUID.Parse(Utils.MD5String(xml));
-
-            if (hash != m_CurrentStateHash)
-            {
-                try
+                // If we're currently in an event, just tell it to save upon return
+                //
+                if (m_InEvent)
                 {
-                    using (FileStream fs = File.Create(Path.Combine(m_dataPath, ItemID.ToString() + ".state")))
+                    m_SaveState = true;
+                    return;
+                }
+
+    //            m_log.DebugFormat(
+    //                "[SCRIPT INSTANCE]: Saving state for script {0} (id {1}) in part {2} (id {3}) in object {4} in {5}",
+    //                ScriptTask.Name, ScriptTask.ItemID, Part.Name, Part.UUID, Part.ParentGroup.Name, Engine.World.Name);
+
+                PluginData = AsyncCommandManager.GetSerializationData(Engine, ItemID);
+
+                string xml = ScriptSerializer.Serialize(this);
+
+                // Compare hash of the state we just just created with the state last written to disk
+                // If the state is different, update the disk file.
+                UUID hash = UUID.Parse(Utils.MD5String(xml));
+
+                if (hash != m_CurrentStateHash)
+                {
+                    try
                     {
-                        Byte[] buf = Util.UTF8NoBomEncoding.GetBytes(xml);
-                        fs.Write(buf, 0, buf.Length);
+                        using (FileStream fs = File.Create(Path.Combine(m_dataPath, ItemID.ToString() + ".state")))
+                        {
+                            Byte[] buf = Util.UTF8NoBomEncoding.GetBytes(xml);
+                            fs.Write(buf, 0, buf.Length);
+                        }
                     }
+                    catch(Exception)
+                    {
+                        // m_log.Error("Unable to save xml\n"+e.ToString());
+                    }
+                    //if (!File.Exists(Path.Combine(Path.GetDirectoryName(assembly), ItemID.ToString() + ".state")))
+                    //{
+                    //    throw new Exception("Completed persistence save, but no file was created");
+                    //}
+                    m_CurrentStateHash = hash;
                 }
-                catch(Exception)
-                {
-                    // m_log.Error("Unable to save xml\n"+e.ToString());
-                }
-                //if (!File.Exists(Path.Combine(Path.GetDirectoryName(assembly), ItemID.ToString() + ".state")))
-                //{
-                //    throw new Exception("Completed persistence save, but no file was created");
-                //}
-                m_CurrentStateHash = hash;
             }
         }
 

OpenSim/Region/ScriptEngine/Interfaces/IScriptInstance.cs
@@ -178,8 +178,9 @@ namespace OpenSim.Region.ScriptEngine.Interfaces
         /// <param name="timeout"></param>
         /// How many milliseconds we will wait for an existing script event to finish before
         /// forcibly aborting that event.
+        /// <param name="clearEventQueue">If true then the event queue is also cleared</param>
         /// <returns>true if the script was successfully stopped, false otherwise</returns>
-        bool Stop(int timeout);
+        bool Stop(int timeout, bool clearEventQueue = false);
 
         void SetState(string state);
 

OpenSim/Region/ScriptEngine/XEngine/XEngine.cs
@@ -732,8 +732,7 @@ namespace OpenSim.Region.ScriptEngine.XEngine
 
                     // Clear the event queue and abort the instance thread
                     //
-                    instance.ClearQueue();
-                    instance.Stop(0);
+                    instance.Stop(0, true);
 
                     // Release events, timer, etc
                     //
@@ -858,8 +857,6 @@ namespace OpenSim.Region.ScriptEngine.XEngine
                         , e);
                 }
             }
-
-            instances.Clear();
 
             if (saveTime > 0)
                 m_ThreadPool.QueueWorkItem(new WorkItemCallback(this.DoBackup),
@@ -1442,6 +1439,7 @@ namespace OpenSim.Region.ScriptEngine.XEngine
                 instance = m_Scripts[itemID];
                 m_Scripts.Remove(itemID);
             }
+
 
             instance.ClearQueue();
 

commit b4e955d1c107784ccf17a739cd83b2603f665d0c

Avoid a possible race condition by serializing plugin data outside the EventQueue lock in ScriptInstance.SaveState()
This takes the AsyncCommandHandler.staticLock.
However, AsyncCommandHandler.DoOneCmdHandlerPass() already holds staticLock and may attempt to take the EventQueue lock via ScriptInstance.PostEvent() in XEngine.CheckListeners()
This is a regression from faaf47a (Fri Jan 16 2015) but not simply reverting that commit since it will reintroduce a race between script removal, backup and event queue manipulating code.

OpenSim/Region/ScriptEngine/Shared/Instance/ScriptInstance.cs
@@ -1022,9 +1022,18 @@ namespace OpenSim.Region.ScriptEngine.Shared.Instance
 
         public void SaveState()
         {
+            if (!Running)
+                return;
+
+            // We cannot call this inside the EventQueue lock since it will currently take AsyncCommandManager.staticLock.
+            // This may already be held by AsyncCommandManager.DoOneCmdHandlerPass() which in turn can take EventQueue
+            // lock via ScriptInstance.PostEvent().
+            PluginData = AsyncCommandManager.GetSerializationData(Engine, ItemID);
+
             // We need to lock here to avoid any race with a thread that is removing this script.
             lock (EventQueue)
             {
+                // Check again to avoid a race with a thread in Stop()
                 if (!Running)
                     return;
 
@@ -1039,8 +1048,6 @@ namespace OpenSim.Region.ScriptEngine.Shared.Instance
     //            m_log.DebugFormat(
     //                "[SCRIPT INSTANCE]: Saving state for script {0} (id {1}) in part {2} (id {3}) in object {4} in {5}",
     //                ScriptTask.Name, ScriptTask.ItemID, Part.Name, Part.UUID, Part.ParentGroup.Name, Engine.World.Name);
-
-                PluginData = AsyncCommandManager.GetSerializationData(Engine, ItemID);
 
                 string xml = ScriptSerializer.Serialize(this);
 