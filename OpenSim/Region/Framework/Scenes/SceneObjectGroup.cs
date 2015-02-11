dd6f560c05caafed740de037eb2b76fcf3924a4a
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2015-02-11 00:53:56 +0000

    If serialized scene object XML has a SavedScriptState with no UUID, then read past the innerXML instead of wrongly continously looping on the same element.
    Addresses http://opensimulator.org/mantis/view.php?id=7437

--- OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
+++ OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
@@ -924,13 +924,17 @@ namespace OpenSim.Region.Framework.Scenes
 
                     string uuid = reader.GetAttribute("UUID");
 
+                    // Even if there is no UUID attribute for some strange reason, we must always read the inner XML
+                    // so we don't continually keep checking the same SavedScriptedState element.
+                    string innerXml = reader.ReadInnerXml();
+
                     if (uuid != null)
                     {
 //                        m_log.DebugFormat("[SCENE OBJECT GROUP]: Found state for item ID {0} in object {1}", uuid, Name);
 
                         UUID itemid = new UUID(uuid);
                         if (itemid != UUID.Zero)
-                            m_savedScriptState[itemid] = reader.ReadInnerXml();
+                            m_savedScriptState[itemid] = innerXml;
                     }
                     else
                     {
