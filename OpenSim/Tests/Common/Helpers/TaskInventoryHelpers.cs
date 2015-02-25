opensim/opensim

commit 08606ae409b400b6f9b83006ed04826eede8a9c7

Make the IteratingUuidGatherer the only UuidGatherer.
This UUID gatherer provides a superset of the previous gatherer's functionality
as it also allows the caller to control gathering iterations for load purposes.

OpenSim/Region/Framework/Scenes/Tests/UuidGathererTests.cs
@@ -61,11 +61,11 @@ namespace OpenSim.Region.Framework.Scenes.Tests
                 = AssetHelpers.CreateAsset(corruptAssetUuid, AssetType.Notecard, "CORRUPT ASSET", UUID.Zero);
             m_assetService.Store(corruptAsset);
 
-            IDictionary<UUID, sbyte> foundAssetUuids = new Dictionary<UUID, sbyte>();
-            m_uuidGatherer.GatherAssetUuids(corruptAssetUuid, (sbyte)AssetType.Object, foundAssetUuids);
+            m_uuidGatherer.AddForInspection(corruptAssetUuid);
+            m_uuidGatherer.GatherAll();
 
             // We count the uuid as gathered even if the asset itself is corrupt.
-            Assert.That(foundAssetUuids.Count, Is.EqualTo(1));
+            Assert.That(m_uuidGatherer.GatheredUuids.Count, Is.EqualTo(1));
         }
         
         /// <summary>
@@ -77,38 +77,42 @@ namespace OpenSim.Region.Framework.Scenes.Tests
             TestHelpers.InMethod();
             
             UUID missingAssetUuid = UUID.Parse("00000000-0000-0000-0000-000000000666");
-            IDictionary<UUID, sbyte> foundAssetUuids = new Dictionary<UUID, sbyte>();
-            
-            m_uuidGatherer.GatherAssetUuids(missingAssetUuid, (sbyte)AssetType.Object, foundAssetUuids);
 
-            // We count the uuid as gathered even if the asset itself is missing.
-            Assert.That(foundAssetUuids.Count, Is.EqualTo(1));
+            m_uuidGatherer.AddForInspection(missingAssetUuid);
+            m_uuidGatherer.GatherAll();
+
+            Assert.That(m_uuidGatherer.GatheredUuids.Count, Is.EqualTo(0));
         }
 
         [Test]
         public void TestNotecardAsset()
         {
             TestHelpers.InMethod();
-//            log4net.Config.XmlConfigurator.Configure();
+            TestHelpers.EnableLogging();
 
             UUID ownerId = TestHelpers.ParseTail(0x10);
-            UUID soAssetId = TestHelpers.ParseTail(0x20);
+            UUID embeddedId = TestHelpers.ParseTail(0x20);
+            UUID missingEmbeddedId = TestHelpers.ParseTail(0x21);
             UUID ncAssetId = TestHelpers.ParseTail(0x30);
 
-            SceneObjectGroup so = SceneHelpers.CreateSceneObject(1, ownerId);
-            AssetBase soAsset = AssetHelpers.CreateAsset(soAssetId, so);
-            m_assetService.Store(soAsset);
-
-            AssetBase ncAsset = AssetHelpers.CreateNotecardAsset(ncAssetId, soAssetId.ToString());
+            AssetBase ncAsset 
+                = AssetHelpers.CreateNotecardAsset(
+                    ncAssetId, string.Format("Hello{0}World{1}", embeddedId, missingEmbeddedId));
             m_assetService.Store(ncAsset);
 
-            IDictionary<UUID, sbyte> foundAssetUuids = new Dictionary<UUID, sbyte>();
-            m_uuidGatherer.GatherAssetUuids(ncAssetId, (sbyte)AssetType.Notecard, foundAssetUuids);
+            AssetBase embeddedAsset = AssetHelpers.CreateNotecardAsset(embeddedId, "We'll meet again.");
+            m_assetService.Store(embeddedAsset);
 
-            // We count the uuid as gathered even if the asset itself is corrupt.
-            Assert.That(foundAssetUuids.Count, Is.EqualTo(2));
-            Assert.That(foundAssetUuids.ContainsKey(ncAssetId));
-            Assert.That(foundAssetUuids.ContainsKey(soAssetId));
+            m_uuidGatherer.AddForInspection(ncAssetId);
+            m_uuidGatherer.GatherAll();
+
+//            foreach (UUID key in m_uuidGatherer.GatheredUuids.Keys)
+//                System.Console.WriteLine("key : {0}", key);
+
+            Assert.That(m_uuidGatherer.GatheredUuids.Count, Is.EqualTo(3));
+            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(ncAssetId));
+            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(embeddedId));
+            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(missingEmbeddedId));
         }
     }
 }

OpenSim/Region/CoreModules/Asset/FlotsamAssetCache.cs
@@ -776,7 +776,6 @@ namespace OpenSim.Region.CoreModules.Asset
         {
             UuidGatherer gatherer = new UuidGatherer(m_AssetService);
 
-            Dictionary<UUID, sbyte> assetIdsToCheck = new Dictionary<UUID, sbyte>();
             Dictionary<UUID, bool> assetsFound = new Dictionary<UUID, bool>();
 
             foreach (Scene s in m_Scenes)
@@ -784,10 +783,11 @@ namespace OpenSim.Region.CoreModules.Asset
                 StampRegionStatusFile(s.RegionInfo.RegionID);
 
                 s.ForEachSOG(delegate(SceneObjectGroup e)
-                {                   
-                    gatherer.GatherAssetUuids(e, assetIdsToCheck);
+                {            
+                    gatherer.AddForInspection(e);
+                    gatherer.GatherAll();
 
-                    foreach (UUID assetID in assetIdsToCheck.Keys)
+                    foreach (UUID assetID in gatherer.GatheredUuids.Keys)
                     {
                         if (!assetsFound.ContainsKey(assetID))
                         {
@@ -800,7 +800,7 @@ namespace OpenSim.Region.CoreModules.Asset
                             else if (storeUncached)
                             {
                                 AssetBase cachedAsset = m_AssetService.Get(assetID.ToString());
-                                if (cachedAsset == null && assetIdsToCheck[assetID] != (sbyte)AssetType.Unknown)
+                                if (cachedAsset == null && gatherer.GatheredUuids[assetID] != (sbyte)AssetType.Unknown)
                                     assetsFound[assetID] = false;
                                 else
                                     assetsFound[assetID] = true;
@@ -810,11 +810,11 @@ namespace OpenSim.Region.CoreModules.Asset
                         {
                             m_log.DebugFormat(
                                 "[FLOTSAM ASSET CACHE]: Could not find asset {0}, type {1} referenced by object {2} at {3} in scene {4} when pre-caching all scene assets",
-                                    assetID, assetIdsToCheck[assetID], e.Name, e.AbsolutePosition, s.Name);
+                                assetID, gatherer.GatheredUuids[assetID], e.Name, e.AbsolutePosition, s.Name);
                         }
                     }
 
-                    assetIdsToCheck.Clear();
+                    gatherer.GatheredUuids.Clear();
                 });
             }
 

OpenSim/Region/CoreModules/Avatar/Inventory/Archiver/InventoryArchiveWriteRequest.cs
@@ -94,11 +94,6 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Archiver
         protected UUID m_id;
 
         /// <value>
-        /// Used to collect the uuids of the assets that we need to save into the archive
-        /// </value>
-        protected Dictionary<UUID, sbyte> m_assetUuids = new Dictionary<UUID, sbyte>();
-
-        /// <value>
         /// Used to collect the uuids of the users that we need to save into the archive
         /// </value>
         protected Dictionary<UUID, int> m_userUuids = new Dictionary<UUID, int>();
@@ -225,7 +220,7 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Archiver
 
             // Don't chase down link asset items as they actually point to their target item IDs rather than an asset
             if (SaveAssets && itemAssetType != AssetType.Link && itemAssetType != AssetType.LinkFolder)
-                m_assetGatherer.GatherAssetUuids(inventoryItem.AssetID, (sbyte)inventoryItem.AssetType, m_assetUuids);
+                m_assetGatherer.AddForInspection(inventoryItem.AssetID);
         }
 
         /// <summary>
@@ -422,12 +417,15 @@ namespace OpenSim.Region.CoreModules.Avatar.Inventory.Archiver
 
                 if (SaveAssets)
                 {
-                    m_log.DebugFormat("[INVENTORY ARCHIVER]: Saving {0} assets for items", m_assetUuids.Count);
+                    m_assetGatherer.GatherAll();
+
+                    m_log.DebugFormat(
+                        "[INVENTORY ARCHIVER]: Saving {0} assets for items", m_assetGatherer.GatheredUuids.Count);
 
                     AssetsRequest ar
                         = new AssetsRequest(
                             new AssetsArchiver(m_archiveWriter),
-                            m_assetUuids, m_scene.AssetService,
+                            m_assetGatherer.GatheredUuids, m_scene.AssetService,
                             m_scene.UserAccountService, m_scene.RegionInfo.ScopeID,
                             options, ReceivedAllAssets);
 

OpenSim/Region/CoreModules/Framework/EntityTransfer/HGEntityTransferModule.cs
@@ -605,8 +605,8 @@ namespace OpenSim.Region.CoreModules.Framework.EntityTransfer
     //                                so.Name, so.AttachedAvatar, url);
 
                                 IDictionary<UUID, sbyte> ids = new Dictionary<UUID, sbyte>();
-                                IteratingHGUuidGatherer uuidGatherer 
-                                    = new IteratingHGUuidGatherer(Scene.AssetService, url, ids);
+                                HGUuidGatherer uuidGatherer 
+                                    = new HGUuidGatherer(Scene.AssetService, url, ids);
                                 uuidGatherer.AddForInspection(so);
 
                                 while (!uuidGatherer.Complete)

OpenSim/Region/CoreModules/Framework/InventoryAccess/HGAssetMapper.cs
@@ -412,12 +412,13 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
 
             // The act of gathering UUIDs downloads some assets from the remote server
             // but not all...
-            Dictionary<UUID, sbyte> ids = new Dictionary<UUID, sbyte>();
             HGUuidGatherer uuidGatherer = new HGUuidGatherer(m_scene.AssetService, userAssetURL);
-            uuidGatherer.GatherAssetUuids(assetID, meta.Type, ids);
-            m_log.DebugFormat("[HG ASSET MAPPER]: Preparing to get {0} assets", ids.Count);
+            uuidGatherer.AddForInspection(assetID);
+            uuidGatherer.GatherAll();
+
+            m_log.DebugFormat("[HG ASSET MAPPER]: Preparing to get {0} assets", uuidGatherer.GatheredUuids.Count);
             bool success = true;
-            foreach (UUID uuid in ids.Keys)
+            foreach (UUID uuid in uuidGatherer.GatheredUuids.Keys)
                 if (FetchAsset(userAssetURL, uuid) == null)
                     success = false;
 
@@ -427,7 +428,6 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
             else
                 m_log.DebugFormat("[HG ASSET MAPPER]: Successfully got item {0} from asset server {1}", assetID, userAssetURL);
         }
-
 
         public void Post(UUID assetID, UUID ownerID, string userAssetURL)
         {
@@ -442,9 +442,8 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
                 return;
             }
 
-            Dictionary<UUID, sbyte> ids = new Dictionary<UUID, sbyte>();
             HGUuidGatherer uuidGatherer = new HGUuidGatherer(m_scene.AssetService, string.Empty);
-            uuidGatherer.GatherAssetUuids(asset.FullID, asset.Type, ids);
+            uuidGatherer.AddForInspection(asset.FullID);
 
             // Check which assets already exist in the destination server
 
@@ -452,16 +451,16 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
             if (!url.EndsWith("/") && !url.EndsWith("="))
                 url = url + "/";
 
-            string[] remoteAssetIDs = new string[ids.Count];
+            string[] remoteAssetIDs = new string[uuidGatherer.GatheredUuids.Count];
             int i = 0;
-            foreach (UUID id in ids.Keys)
+            foreach (UUID id in uuidGatherer.GatheredUuids.Keys)
                 remoteAssetIDs[i++] = url + id.ToString();
 
             bool[] exist = m_scene.AssetService.AssetsExist(remoteAssetIDs);
 
             var existSet = new HashSet<string>();
             i = 0;
-            foreach (UUID id in ids.Keys)
+            foreach (UUID id in uuidGatherer.GatheredUuids.Keys)
             {
                 if (exist[i])
                     existSet.Add(id.ToString());
@@ -472,7 +471,7 @@ namespace OpenSim.Region.CoreModules.Framework.InventoryAccess
 
             bool success = true;
 
-            foreach (UUID uuid in ids.Keys)
+            foreach (UUID uuid in uuidGatherer.GatheredUuids.Keys)
             {
                 if (!existSet.Contains(uuid.ToString()))
                 {

OpenSim/Region/CoreModules/World/Archiver/ArchiveWriteRequest.cs
@@ -219,7 +219,7 @@ namespace OpenSim.Region.CoreModules.World.Archiver
 
         private void ArchiveOneRegion(Scene scene, string regionDir, Dictionary<UUID, sbyte> assetUuids)
         {
-            m_log.InfoFormat("[ARCHIVER]: Writing region {0}", scene.RegionInfo.RegionName);
+            m_log.InfoFormat("[ARCHIVER]: Writing region {0}", scene.Name);
 
             EntityBase[] entities = scene.GetEntities();
             List<SceneObjectGroup> sceneObjects = new List<SceneObjectGroup>();
@@ -253,13 +253,13 @@ namespace OpenSim.Region.CoreModules.World.Archiver
 
             if (SaveAssets)
             {
-                UuidGatherer assetGatherer = new UuidGatherer(scene.AssetService);
+                UuidGatherer assetGatherer = new UuidGatherer(scene.AssetService, assetUuids);
                 int prevAssets = assetUuids.Count;
                     
                 foreach (SceneObjectGroup sceneObject in sceneObjects)
-                {
-                    assetGatherer.GatherAssetUuids(sceneObject, assetUuids);
-                }
+                    assetGatherer.AddForInspection(sceneObject);
+
+                assetGatherer.GatherAll();
 
                 m_log.DebugFormat(
                     "[ARCHIVER]: {0} scene objects to serialize requiring save of {1} assets",

OpenSim/Region/OptionalModules/Avatar/Appearance/AppearanceInfoModule.cs
@@ -426,10 +426,10 @@ namespace OpenSim.Region.OptionalModules.Avatar.Appearance
                                     cdl.AddRow("Assets", "");
                                     sb.Append(cdl.ToString());
 
-                                    Dictionary<UUID, sbyte> assetUuids = new Dictionary<UUID, sbyte>();
-                                    uuidGatherer.GatherAssetUuids(wi.AssetID, assetUuids);
+                                    uuidGatherer.AddForInspection(wi.AssetID);
+                                    uuidGatherer.GatherAll();
                                     string[] assetStrings 
-                                        = Array.ConvertAll<UUID, string>(assetUuids.Keys.ToArray(), u => u.ToString());
+                                        = Array.ConvertAll<UUID, string>(uuidGatherer.GatheredUuids.Keys.ToArray(), u => u.ToString());
 
                                     bool[] existChecks = scene.AssetService.AssetsExist(assetStrings);
 
@@ -440,7 +440,9 @@ namespace OpenSim.Region.OptionalModules.Avatar.Appearance
                                     cdt.AddColumn("Found", 5);
                                             
                                     for (int k = 0; k < existChecks.Length; k++)
-                                        cdt.AddRow((AssetType)assetUuids[new UUID(assetStrings[k])], assetStrings[k], existChecks[k] ? "yes" : "no");
+                                        cdt.AddRow(
+                                            (AssetType)uuidGatherer.GatheredUuids[new UUID(assetStrings[k])], 
+                                            assetStrings[k], existChecks[k] ? "yes" : "no");
 
                                     sb.Append(cdt.ToString());
                                     sb.Append("\n");

commit a03d893f2c431c220f44a6f7c1b94de7568bd6f8

Fix bug where the uuid gatherer was not inspecting UUIDs for items in an embedded object's inventory.
Added regression test for this case.
Likely a regression since 08606ae4 (Thu Jan 8 2015)
Relates to Mantises 7439, 7450 and possibly others.

OpenSim/Region/Framework/Scenes/UuidGatherer.cs
@@ -123,6 +123,8 @@ namespace OpenSim.Region.Framework.Scenes
             if (m_assetUuidsToInspect.Contains(uuid))
                 return false;
 
+//            m_log.DebugFormat("[UUID GATHERER]: Adding asset {0} for inspection", uuid);
+
             m_assetUuidsToInspect.Enqueue(uuid);
 
             return true;
@@ -238,7 +240,11 @@ namespace OpenSim.Region.Framework.Scenes
             if (Complete)
                 return false;
 
-            GetAssetUuids(m_assetUuidsToInspect.Dequeue());
+            UUID nextToInspect = m_assetUuidsToInspect.Dequeue();
+
+//            m_log.DebugFormat("[UUID GATHERER]: Inspecting asset {0}", nextToInspect);
+
+            GetAssetUuids(nextToInspect);
 
             return true;
         }
@@ -322,8 +328,6 @@ namespace OpenSim.Region.Framework.Scenes
             // Here, we want to collect uuids which require further asset fetches but mark the others as gathered
             try
             {               
-                GatheredUuids[assetUuid] = assetType;
-
                 if ((sbyte)AssetType.Bodypart == assetType 
                     || (sbyte)AssetType.Clothing == assetType
                     || (sbyte)AssetType.Gesture == assetType
@@ -334,11 +338,15 @@ namespace OpenSim.Region.Framework.Scenes
                 {
                     AddForInspection(assetUuid);
                 }
+                else
+                {
+                    GatheredUuids[assetUuid] = assetType;
+                }
             }
             catch (Exception)
             {
                 m_log.ErrorFormat(
-                    "[ITERATABLE UUID GATHERER]: Failed to gather uuids for asset id {0}, type {1}", 
+                    "[UUID GATHERER]: Failed to gather uuids for asset id {0}, type {1}", 
                     assetUuid, assetType);
                 throw;
             }

OpenSim/Region/Framework/Scenes/Tests/UuidGathererTests.cs
@@ -122,40 +122,37 @@ namespace OpenSim.Region.Framework.Scenes.Tests
         }
 
         [Test]
-        public void TestTaskItem()
+        public void TestTaskItems()
         {
             TestHelpers.InMethod();
-                        TestHelpers.EnableLogging();
+//                        TestHelpers.EnableLogging();
 
             UUID ownerId = TestHelpers.ParseTail(0x10);
-            UUID embeddedId = TestHelpers.ParseTail(0x20);
-            UUID secondLevelEmbeddedId = TestHelpers.ParseTail(0x21);
-            UUID missingEmbeddedId = TestHelpers.ParseTail(0x22);
-            UUID ncAssetId = TestHelpers.ParseTail(0x30);
 
-            AssetBase ncAsset 
-                = AssetHelpers.CreateNotecardAsset(
-                    ncAssetId, string.Format("Hello{0}World{1}", embeddedId, missingEmbeddedId));
-            m_assetService.Store(ncAsset);
+            SceneObjectGroup soL0 = SceneHelpers.CreateSceneObject(1, ownerId, "l0", 0x20);
+            SceneObjectGroup soL1 = SceneHelpers.CreateSceneObject(1, ownerId, "l1", 0x21);
+            SceneObjectGroup soL2 = SceneHelpers.CreateSceneObject(1, ownerId, "l2", 0x22);
 
-            AssetBase embeddedAsset 
-                = AssetHelpers.CreateNotecardAsset(embeddedId, string.Format("{0} We'll meet again.", secondLevelEmbeddedId));
-            m_assetService.Store(embeddedAsset);
+            TaskInventoryHelpers.AddScript(
+                m_assetService, soL2.RootPart, TestHelpers.ParseTail(0x33), TestHelpers.ParseTail(0x43), "l3-script", "gibberish");
 
-            AssetBase secondLevelEmbeddedAsset 
-                = AssetHelpers.CreateNotecardAsset(secondLevelEmbeddedId, "Don't know where, don't know when.");
-            m_assetService.Store(secondLevelEmbeddedAsset);
+            TaskInventoryHelpers.AddSceneObject(
+                m_assetService, soL1.RootPart, "l2-item", TestHelpers.ParseTail(0x32), soL2, TestHelpers.ParseTail(0x42));
+            TaskInventoryHelpers.AddSceneObject(
+                m_assetService, soL0.RootPart, "l1-item", TestHelpers.ParseTail(0x31), soL1, TestHelpers.ParseTail(0x41));
 
-            m_uuidGatherer.AddForInspection(ncAssetId);
+            m_uuidGatherer.AddForInspection(soL0);
             m_uuidGatherer.GatherAll();
 
-            //            foreach (UUID key in m_uuidGatherer.GatheredUuids.Keys)
-            //                System.Console.WriteLine("key : {0}", key);
+//                        foreach (UUID key in m_uuidGatherer.GatheredUuids.Keys)
+//                            System.Console.WriteLine("key : {0}", key);
 
-            Assert.That(m_uuidGatherer.GatheredUuids.Count, Is.EqualTo(3));
-            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(ncAssetId));
-            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(embeddedId));
-            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(secondLevelEmbeddedId));
+            // We expect to see the default prim texture and the assets of the contained task items
+            Assert.That(m_uuidGatherer.GatheredUuids.Count, Is.EqualTo(4));
+            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(new UUID(Constants.DefaultTexture)));
+            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(TestHelpers.ParseTail(0x41)));
+            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(TestHelpers.ParseTail(0x42)));
+            Assert.That(m_uuidGatherer.GatheredUuids.ContainsKey(TestHelpers.ParseTail(0x43)));
         }
     }
 }

OpenSim/Tests/Common/Helpers/TaskInventoryHelpers.cs
@@ -153,6 +153,37 @@ namespace OpenSim.Tests.Common
             return item;
         }
 
+        /// <summary>
+        /// Add a scene object item to the given part.
+        /// </summary>
+        /// <remarks>
+        /// TODO: Accept input for item and asset IDs to avoid mysterious script failures that try to use any of these
+        /// functions more than once in a test.
+        /// </remarks>
+        ///
+        /// <param name="assetService"></param>
+        /// <param name="sop"></param>
+        /// <param name="itemName"></param>
+        /// <param name="itemId"></param>
+        /// <param name="soToAdd"></param>
+        /// <param name="soAssetId"></param>
+        public static TaskInventoryItem AddSceneObject(
+            IAssetService assetService, SceneObjectPart sop, string itemName, UUID itemId, SceneObjectGroup soToAdd, UUID soAssetId)
+        {
+            AssetBase taskSceneObjectAsset = AssetHelpers.CreateAsset(soAssetId, soToAdd);
+            assetService.Store(taskSceneObjectAsset);
+            TaskInventoryItem taskSceneObjectItem
+                = new TaskInventoryItem
+            { Name = itemName,
+                AssetID = taskSceneObjectAsset.FullID,
+                ItemID = itemId,
+                OwnerID = soToAdd.OwnerID,
+                Type = (int)AssetType.Object,
+                InvType = (int)InventoryType.Object };
+            sop.Inventory.AddInventoryItem(taskSceneObjectItem, true);
+
+            return taskSceneObjectItem;
+        }
 
         /// <summary>
         /// Add a scene object item to the given part.
@@ -168,22 +199,12 @@ namespace OpenSim.Tests.Common
         /// <param name="id"></param>
         /// <param name="userId"></param>
         public static TaskInventoryItem AddSceneObject(
-            IAssetService assetService, SceneObjectPart sop, string itemName, UUID id, UUID userId)
+            IAssetService assetService, SceneObjectPart sop, string itemName, UUID itemId, UUID userId)
         {
-            SceneObjectGroup taskSceneObject = SceneHelpers.CreateSceneObject(1, UUID.Zero);
-            AssetBase taskSceneObjectAsset = AssetHelpers.CreateAsset(0x10, taskSceneObject);
-            assetService.Store(taskSceneObjectAsset);
-            TaskInventoryItem taskSceneObjectItem
-                = new TaskInventoryItem
-                    { Name = itemName,
-                      AssetID = taskSceneObjectAsset.FullID,
-                      ItemID = id,
-                      OwnerID = userId,
-                      Type = (int)AssetType.Object,
-                      InvType = (int)InventoryType.Object };
-            sop.Inventory.AddInventoryItem(taskSceneObjectItem, true);
+            SceneObjectGroup taskSceneObject = SceneHelpers.CreateSceneObject(1, userId);
 
-            return taskSceneObjectItem;
+            return TaskInventoryHelpers.AddSceneObject(
+                assetService, sop, itemName, itemId, taskSceneObject, TestHelpers.ParseTail(0x10));
         }
     }
 }