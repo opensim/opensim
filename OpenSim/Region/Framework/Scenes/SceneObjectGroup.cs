e171ae899d3b78072f368de357dadeeae8808f8d
BlueWall <jamesh@bluewallgroup.com>
2015-04-28 17:24:54 -0500

    Revert "Enable grab feature (Ctrl+Drag) for non-physical link-sets and add code to handle spin (Ctrl+Shift+Drag)"
    We found that linksets can move if the root prim is unscripted. Will revert this until a fix can be implemented.

--- OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
+++ OpenSim/Region/Framework/Scenes/SceneObjectGroup.cs
@@ -2672,26 +2672,20 @@ namespace OpenSim.Region.Framework.Scenes
                     }
                     else
                     {
-                        NonPhysicalGrabMovement(pos);
+                        //NonPhysicalGrabMovement(pos);
                     }
                 }
                 else
                 {
-                    NonPhysicalGrabMovement(pos);
+                    //NonPhysicalGrabMovement(pos);
                 }
             }
         }
 
-        /// <summary>
-        /// Apply possition for grabbing non-physical linksets (ctrl+drag)
-        /// </summary>
-        /// <param name="pos">New Position</param>
         public void NonPhysicalGrabMovement(Vector3 pos)
         {
-            if((m_rootPart.Flags & PrimFlags.Scripted) == 0)
-            {
-                UpdateGroupPosition(pos);
-            }
+            AbsolutePosition = pos;
+            m_rootPart.SendTerseUpdateToAllClients();
         }
 
         /// <summary>
@@ -2787,25 +2781,13 @@ namespace OpenSim.Region.Framework.Scenes
                     }
                     else
                     {
-                        NonPhysicalSpinMovement(newOrientation);
+                        //NonPhysicalSpinMovement(pos);
                     }
                 }
                 else
                 {
-                    NonPhysicalSpinMovement(newOrientation);
+                    //NonPhysicalSpinMovement(pos);
                 }
-            }
-        }
-
-        /// <summary>
-        /// Apply rotation for spinning non-physical linksets (ctrl+shift+drag)
-        /// </summary>
-        /// <param name="newOrientation">New Rotation</param>
-        private void NonPhysicalSpinMovement(Quaternion newOrientation)
-        {
-            if ((m_rootPart.Flags & PrimFlags.Scripted) == 0)
-            {
-                UpdateGroupRotationR(newOrientation);
             }
         }
 
