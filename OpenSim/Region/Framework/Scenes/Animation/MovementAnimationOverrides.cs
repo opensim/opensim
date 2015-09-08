31afc76fcf34a5ae58724c9e994454045862d9e6
UbitUmarov <ajlduarte@sapo.pt>
2015-09-08 16:51:42 +0100

    fix some compile

--- OpenSim/Region/Framework/Scenes/Animation/MovementAnimationOverrides.cs
+++ OpenSim/Region/Framework/Scenes/Animation/MovementAnimationOverrides.cs
@@ -40,7 +40,7 @@ using OpenSim.Framework.Client;
 using OpenSim.Region.Framework.Interfaces;
 using OpenSim.Region.Framework.Scenes.Animation;
 using OpenSim.Region.Framework.Scenes.Types;
-using OpenSim.Region.Physics.Manager;
+using OpenSim.Region.PhysicsModules.SharedBase;
 using GridRegion = OpenSim.Services.Interfaces.GridRegion;
 using OpenSim.Services.Interfaces;
 using TeleportFlags = OpenSim.Framework.Constants.TeleportFlags;
