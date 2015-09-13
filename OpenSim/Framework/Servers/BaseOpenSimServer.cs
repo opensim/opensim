f24414c063261d279490a73510af413da6d05d7b
Melanie Thielker <melanie@t-data.com>
2015-09-13 21:03:38 +0200

    Remove the last proprietary copyright headers, insert BSD ones.

--- OpenSim/Framework/Servers/BaseOpenSimServer.cs
+++ OpenSim/Framework/Servers/BaseOpenSimServer.cs
@@ -152,7 +152,7 @@ namespace OpenSim.Framework.Servers
         {
             m_log.Info("[STARTUP]: Beginning startup processing");
             
-            m_log.Info("[STARTUP]: Careminster version: " + m_version + Environment.NewLine);
+            m_log.Info("[STARTUP]: version: " + m_version + Environment.NewLine);
             // clr version potentially is more confusing than helpful, since it doesn't tell us if we're running under Mono/MS .NET and
             // the clr version number doesn't match the project version number under Mono.
             //m_log.Info("[STARTUP]: Virtual machine runtime version: " + Environment.Version + Environment.NewLine);
