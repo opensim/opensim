4c0ef5a0efe830747d38d2c9c47efa2ef17be530
UbitUmarov <ajlduarte@sapo.pt>
2015-09-28 12:37:12 +0100

    change version string

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -33,7 +33,7 @@ namespace OpenSim
         private const Flavour VERSION_FLAVOUR = Flavour.Dev;
 
         public enum Flavour
-        { 
+        {
             Unknown,
             Dev,
             RC1,
@@ -51,7 +51,7 @@ namespace OpenSim
 
         public static string GetVersionString(string versionNumber, Flavour flavour)
         {
-            string versionString = "OpenSimulator" + versionNumber + " " + flavour;
+            string versionString = "OpenSim " + versionNumber + " " + flavour;
             return versionString.PadRight(VERSIONINFO_VERSION_LENGTH);
         }
 
