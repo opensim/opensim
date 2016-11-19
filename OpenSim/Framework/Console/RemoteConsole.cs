08dee3fa34f72912cab3c5facdc79083e85b39ef
UbitUmarov <ajlduarte@sapo.pt>
2016-11-19 15:46:47 +0000

    fix pool parameters for httptests

--- OpenSim/Framework/Console/RemoteConsole.cs
+++ OpenSim/Framework/Console/RemoteConsole.cs
@@ -403,7 +403,7 @@ namespace OpenSim.Framework.Console
             string uri = "/ReadResponses/" + sessionID.ToString() + "/";
 
             m_Server.AddPollServiceHTTPHandler(
-                uri, new PollServiceEventArgs(null, uri, HasEvents, GetEvents, NoEvents, sessionID,25000)); // 25 secs timeout
+                uri, new PollServiceEventArgs(null, uri, HasEvents, GetEvents, NoEvents, null, sessionID,25000)); // 25 secs timeout
 
 			// Our reply is an XML document.
 			// TODO: Change this to Linq.Xml
