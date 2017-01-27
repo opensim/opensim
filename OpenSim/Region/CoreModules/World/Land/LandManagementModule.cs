d4111ff6707e59278d1391479d6513bb069b4c3d
UbitUmarov <ajlduarte@sapo.pt>
2017-01-27 20:59:56 +0000

    fix SimwideArea again

--- OpenSim/Region/CoreModules/World/Land/LandManagementModule.cs
+++ OpenSim/Region/CoreModules/World/Land/LandManagementModule.cs
@@ -1581,6 +1581,7 @@ namespace OpenSim.Region.CoreModules.World.Land
                         }
                     }
                 }
+                FinalizeLandPrimCountUpdate(); // update simarea information
             }
         }
 
