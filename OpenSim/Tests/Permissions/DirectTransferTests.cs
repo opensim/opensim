3c43cdbbcc2687e2f5d4319c5c3c3cbad4930fab
Diva Canto <diva@metaverseink.com>
2017-05-01 16:42:47 -0700

    Added some indirect transfer tests -- Take Copy by another avatar

--- OpenSim/Tests/Permissions/DirectTransferTests.cs
+++ OpenSim/Tests/Permissions/DirectTransferTests.cs
@@ -25,20 +25,10 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Threading;
-using Nini.Config;
 using NUnit.Framework;
 using OpenMetaverse;
 using OpenSim.Framework;
 using OpenSim.Region.Framework.Scenes;
-using OpenSim.Region.CoreModules.World.Permissions;
-using OpenSim.Region.CoreModules.Avatar.Inventory.Transfer;
-using OpenSim.Region.CoreModules.Framework.InventoryAccess;
-using OpenSim.Region.Framework.Scenes.Serialization;
-using OpenSim.Services.Interfaces;
 using OpenSim.Tests.Common;
 using PermissionMask = OpenSim.Framework.PermissionMask;
 
