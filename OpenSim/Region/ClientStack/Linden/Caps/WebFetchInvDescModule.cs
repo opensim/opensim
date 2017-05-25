b6c23fe91169ed7065280464d20bba658252e278
UbitUmarov <ajlduarte@sapo.pt>
2017-05-25 02:01:36 +0100

    make a few more threads background ones

--- OpenSim/Region/ClientStack/Linden/Caps/WebFetchInvDescModule.cs
+++ OpenSim/Region/ClientStack/Linden/Caps/WebFetchInvDescModule.cs
@@ -207,7 +207,7 @@ namespace OpenSim.Region.ClientStack.Linden
                     m_workerThreads[i] = WorkManager.StartThread(DoInventoryRequests,
                             String.Format("InventoryWorkerThread{0}", i),
                             ThreadPriority.Normal,
-                            false,
+                            true,
                             true,
                             null,
                             int.MaxValue);
