d90b68c2a637b822b826a0cf3c52991aa9ee2c97
UbitUmarov <ajlduarte@sapo.pt>
2017-05-26 00:05:35 +0100

    change wrong watchdog stopped thread check code, and don't log it

--- OpenSim/Region/Application/OpenSim.cs
+++ OpenSim/Region/Application/OpenSim.cs
@@ -511,8 +511,6 @@ namespace OpenSim
         private void WatchdogTimeoutHandler(Watchdog.ThreadWatchdogInfo twi)
         {
             int now = Environment.TickCount & Int32.MaxValue;
-            if(twi.Thread.ThreadState == System.Threading.ThreadState.Stopped)
-                return;
             m_log.ErrorFormat(
                 "[WATCHDOG]: Timeout detected for thread \"{0}\". ThreadState={1}. Last tick was {2}ms ago.  {3}",
                 twi.Thread.Name,
