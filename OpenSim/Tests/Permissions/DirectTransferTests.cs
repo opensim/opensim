8b6557e377f7bac1bdb38f3e6dd22c797417d09c
Diva Canto <diva@metaverseink.com>
2017-08-14 16:49:11 -0700

    Let's try giving Common a default constructor instead of the workaround

--- OpenSim/Tests/Permissions/DirectTransferTests.cs
+++ OpenSim/Tests/Permissions/DirectTransferTests.cs
@@ -44,13 +44,6 @@ namespace OpenSim.Tests.Permissions
         [SetUp]
         public void SetUp()
         {
-            // In case we're dealing with some older version of nunit
-            if (Common.TheInstance == null)
-            {
-                Common.TheInstance = new Common();
-                Common.TheInstance.SetUp();
-            }
-
             Common.TheInstance.DeleteObjectsFolders();
         }
 
