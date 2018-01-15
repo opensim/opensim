ae04a24e7a2b9fee4bfb4ac3ca894c06946fea17
UbitUmarov <ajlduarte@sapo.pt>
2018-01-15 18:57:15 +0000

    recover a lost null check

--- OpenSim/Region/PhysicsModules/ubOdeMeshing/Meshmerizer.cs
+++ OpenSim/Region/PhysicsModules/ubOdeMeshing/Meshmerizer.cs
@@ -340,7 +340,7 @@ namespace OpenSim.Region.PhysicsModule.ubODEMeshing
             faces = new List<Face>();
             OSD meshOsd = null;
 
-            if (primShape.SculptData.Length <= 0)
+            if (primShape.SculptData == null || primShape.SculptData.Length <= 0)
             {
 //                m_log.InfoFormat("[MESH]: asset data for {0} is zero length", primName);
                 return false;
@@ -363,6 +363,7 @@ namespace OpenSim.Region.PhysicsModule.ubODEMeshing
                 catch (Exception e)
                 {
                     m_log.Error("[MESH]: Exception deserializing mesh asset header:" + e.ToString());
+                    return false;
                 }
 
                 start = data.Position;
