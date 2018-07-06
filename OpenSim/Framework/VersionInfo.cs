8ac69a5d276e4c8c2ea550402fc6e67376784fe1
UbitUmarov <ajlduarte@sapo.pt>
2018-07-06 02:23:06 +0100

    just remove the test instead

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -57,7 +57,7 @@ namespace OpenSim
             return versionString.PadRight(VERSIONINFO_VERSION_LENGTH);
         }
 
-        public const int VERSIONINFO_VERSION_LENGTH = 30;
+        public const int VERSIONINFO_VERSION_LENGTH = 27;
 
         /// <value>
         /// This is the external interface version.  It is separate from the OpenSimulator project version.
