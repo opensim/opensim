6f4052561ec68f11982b9f7b9067990858717b9b
UbitUmarov <ajlduarte@sapo.pt>
2018-07-14 14:39:29 +0100

    send data as bin not string to http lower level

--- OpenSim/Region/ClientStack/Linden/Caps/WebFetchInvDescModule.cs
+++ OpenSim/Region/ClientStack/Linden/Caps/WebFetchInvDescModule.cs
@@ -394,9 +394,14 @@ namespace OpenSim.Region.ClientStack.Linden
                 response["int_response_code"] = 200;
                 response["content_type"] = "text/plain";
 
-                response["str_response_string"] = m_webFetchHandler.FetchInventoryDescendentsRequest(
-                        requestinfo.request["body"].ToString(), String.Empty, String.Empty, null, null);
+//                response["str_response_string"] = m_webFetchHandler.FetchInventoryDescendentsRequest(
+//                        requestinfo.request["body"].ToString(), String.Empty, String.Empty, null, null);
 
+                response["bin_response_data"] = System.Text.Encoding.UTF8.GetBytes(
+                        m_webFetchHandler.FetchInventoryDescendentsRequest(
+                                    requestinfo.request["body"].ToString(),
+                                    String.Empty, String.Empty, null, null)
+                        );
                 lock (responses)
                 {
                     lock(dropedResponses)
