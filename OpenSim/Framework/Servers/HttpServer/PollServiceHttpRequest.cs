39e82cb04349174bfda91dc2952aabf690cfb87e
Melanie <melanie@t-data.com>
2018-09-12 13:13:11 +0100

    Squelch "connection reset by peer" exceptions

--- OpenSim/Framework/Servers/HttpServer/PollServiceHttpRequest.cs
+++ OpenSim/Framework/Servers/HttpServer/PollServiceHttpRequest.cs
@@ -94,6 +94,13 @@ namespace OpenSim.Framework.Servers.HttpServer
                 response.Send();
                 buffer = null;
             }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                // This is "connection reset by peer", meaning the
+                // requesting server has given up. They need to
+                // fix their timeouts. We don't want to spam the
+                // log with this.
+            }
             catch (Exception ex)
             {
                 m_log.Warn("[POLL SERVICE WORKER THREAD]: Error ", ex);
