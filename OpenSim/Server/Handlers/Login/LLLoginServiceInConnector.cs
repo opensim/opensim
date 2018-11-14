35f5d4449bc96b6ee0eaaa128092a0c8035f79c3
UbitUmarov <ajlduarte@sapo.pt>
2018-11-14 17:33:26 +0000

    take login service out of basicDOSprot (too little, too late)

--- OpenSim/Server/Handlers/Login/LLLoginServiceInConnector.cs
+++ OpenSim/Server/Handlers/Login/LLLoginServiceInConnector.cs
@@ -106,9 +106,10 @@ namespace OpenSim.Server.Handlers.Login
         private void InitializeHandlers(IHttpServer server)
         {
             LLLoginHandlers loginHandlers = new LLLoginHandlers(m_LoginService, m_Proxy);
-            server.AddXmlRPCHandler("login_to_simulator",
-                new XmlRpcBasicDOSProtector(loginHandlers.HandleXMLRPCLogin,loginHandlers.HandleXMLRPCLoginBlocked,
-                    m_DosProtectionOptions).Process, false);
+//            server.AddXmlRPCHandler("login_to_simulator",
+//                new XmlRpcBasicDOSProtector(loginHandlers.HandleXMLRPCLogin, loginHandlers.HandleXMLRPCLoginBlocked,
+//                    m_DosProtectionOptions).Process, false);
+            server.AddXmlRPCHandler("login_to_simulator",loginHandlers.HandleXMLRPCLogin, false);
             server.AddXmlRPCHandler("set_login_level", loginHandlers.HandleXMLRPCSetLoginLevel, false);
             server.SetDefaultLLSDHandler(loginHandlers.HandleLLSDLogin);
             server.AddWebSocketHandler("/WebSocket/GridLogin", loginHandlers.HandleWebSocketLoginEvents);
