a1568697d0240d0b553b1ec2ca7c573f5382d318
Robert Adams <misterblue@misterblue.com>
2019-02-05 08:07:22 -0800

    Remove cut-and-paste typo in 6dof contstrain setup in XNA version of BulletSim. Could only effect operation of flexible linksets when using the C# version of the Bullet physics engine. Found by Tampa and reported in Mantis 7612.

--- OpenSim/Region/PhysicsModules/BulletS/BSAPIXNA.cs
+++ OpenSim/Region/PhysicsModules/BulletS/BSAPIXNA.cs
@@ -588,7 +588,7 @@ private sealed class BulletConstraintXNA : BulletConstraint
         IndexedVector3 frame2v = new IndexedVector3(pframe2.X, pframe2.Y, pframe2.Z);
         IndexedQuaternion frame2rot = new IndexedQuaternion(pframe2rot.X, pframe2rot.Y, pframe2rot.Z, pframe2rot.W);
         IndexedMatrix frame2 = IndexedMatrix.CreateFromQuaternion(frame2rot);
-        frame2._origin = frame1v;
+        frame2._origin = frame2v;
 
         Generic6DofConstraint consttr = new Generic6DofConstraint(body1, body2, ref frame1, ref frame2,
                                                                   puseLinearReferenceFrameA);
