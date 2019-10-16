c7714d63204d70ed49e5d633bb49e34fd0f5f7cc
UbitUmarov <ajlduarte@sapo.pt>
2019-10-16 13:05:30 +0100

    make OSSL parsing error warning more clear

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/OSSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/OSSL_Api.cs
@@ -357,6 +357,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                     else
                     {
                         string[] ids;
+                        bool error = false;
                         if (!string.IsNullOrWhiteSpace(ownerPerm))
                         {
                             ids = ownerPerm.Split(new char[] {','});
@@ -403,15 +404,16 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                                         }
                                         else
                                         {
-                                            m_log.WarnFormat("[OSSLENABLE]: error parsing line {0}", ownerPerm);
+                                            error = true;
                                         }
-
                                         break;
                                     }
                                 }
                             }
+                            if (error)
+                                m_log.WarnFormat("[OSSLENABLE]: error parsing line Allow_{0} = {1}", function, ownerPerm);
                         }
-
+                        error = false;
                         if (!string.IsNullOrWhiteSpace(creatorPerm))
                         {
                             ids = creatorPerm.Split(new char[] {','});
@@ -430,9 +432,11 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                                 }
                                 else
                                 {
-                                    m_log.WarnFormat("[OSSLENABLE]: error parsing line {0}", creatorPerm);
+                                    error = true;
                                 }
                             }
+                            if (error)
+                                m_log.WarnFormat("[OSSLENABLE]: error parsing line Creators_{0} = {1}", function, creatorPerm);
                         }
                         // both empty fallback as disabled
                     }
