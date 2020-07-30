3b06891252ba42cb9c7d80171dd914f770df7d35
UbitUmarov <ajlduarte@sapo.pt>
2020-07-30 18:10:06 +0100

    dont break asset cache folders spread more than needed

--- OpenSim/Region/CoreModules/Agent/TextureSender/J2KDecoderModule.cs
+++ OpenSim/Region/CoreModules/Agent/TextureSender/J2KDecoderModule.cs
@@ -334,7 +334,7 @@ namespace OpenSim.Region.CoreModules.Agent.TextureSender
 
             if (Cache != null)
             {
-                string assetID = "j2kCache_" + AssetId.ToString();
+                string assetID = "j2k" + AssetId.ToString();
 
                 AssetBase layerDecodeAsset = new AssetBase(assetID, assetID, (sbyte)AssetType.Notecard, m_CreatorID.ToString());
                 layerDecodeAsset.Local = true;
