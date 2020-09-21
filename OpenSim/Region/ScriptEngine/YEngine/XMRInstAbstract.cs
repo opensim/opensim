d2e7c711b4188106a89f28d54ada3cd76c697705
UbitUmarov <ajlduarte@sapo.pt>
2020-09-21 21:40:24 +0100

    actually make yExceptionMessage and yExceptionTypeName avaiable

--- OpenSim/Region/ScriptEngine/YEngine/XMRInstAbstract.cs
+++ OpenSim/Region/ScriptEngine/YEngine/XMRInstAbstract.cs
@@ -1198,8 +1198,18 @@ namespace OpenSim.Region.ScriptEngine.Yengine
             return ex.Message;
         }
 
+        public static string yExceptionMessage(Exception ex)
+        {
+            return ex.Message;
+        }
+
         // Return stack trace (no type or message, just stack trace lines: at ... \n)
         public string xmrExceptionStackTrace(Exception ex)
+        {
+            return XMRExceptionStackString(ex);
+        }
+
+        public string yExceptionStackTrace(Exception ex)
         {
             return XMRExceptionStackString(ex);
         }
@@ -1210,8 +1220,18 @@ namespace OpenSim.Region.ScriptEngine.Yengine
             return ((ScriptThrownException)ex).thrown;
         }
 
+        public static object yExceptionThrownValue(Exception ex)
+        {
+            return ((ScriptThrownException)ex).thrown;
+        }
+
         // Return exception's short type name, eg, NullReferenceException, ScriptThrownException, etc.
         public static string xmrExceptionTypeName(Exception ex)
+        {
+            return ex.GetType().Name;
+        }
+
+        public static string yExceptionTypeName(Exception ex)
         {
             return ex.GetType().Name;
         }
