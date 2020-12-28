e169b1b0748b106aca289541212adc1c91527bbc
UbitUmarov <ajlduarte@sapo.pt>
2020-12-28 21:05:26 +0000

    simplify xbakes service

--- OpenSim/Services/Interfaces/IBakedTextureService.cs
+++ OpenSim/Services/Interfaces/IBakedTextureService.cs
@@ -32,7 +32,7 @@ namespace OpenSim.Services.Interfaces
 {
     public interface IBakedTextureService
     {
-        string Get(string id);
+        byte[] Get(string id);
         void Store(string id, string data);
     }
 }
