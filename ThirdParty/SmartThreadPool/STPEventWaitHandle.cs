1db6a541bd4891e73e4e6d2e63f4ee487f549565
UbitUmarov <ajlduarte@sapo.pt>
2021-01-13 16:23:49 +0000

    more changes to smartthreadpool

--- ThirdParty/SmartThreadPool/STPEventWaitHandle.cs
+++ ThirdParty/SmartThreadPool/STPEventWaitHandle.cs
@@ -7,21 +7,25 @@ namespace Amib.Threading.Internal
     {
         public const int WaitTimeout = Timeout.Infinite;
 
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         internal static bool WaitAll(WaitHandle[] waitHandles, int millisecondsTimeout, bool exitContext)
         {
             return WaitHandle.WaitAll(waitHandles, millisecondsTimeout, exitContext);
         }
 
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         internal static int WaitAny(WaitHandle[] waitHandles)
         {
             return WaitHandle.WaitAny(waitHandles);
         }
 
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         internal static int WaitAny(WaitHandle[] waitHandles, int millisecondsTimeout, bool exitContext)
         {
             return WaitHandle.WaitAny(waitHandles, millisecondsTimeout, exitContext);
         }
 
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         internal static bool WaitOne(WaitHandle waitHandle, int millisecondsTimeout, bool exitContext)
         {
             return waitHandle.WaitOne(millisecondsTimeout, exitContext);
