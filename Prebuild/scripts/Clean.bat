4f6a8e800f9c53cf8feeec18aea423873064c9d1
mdicvkson <mike.dickson@utopiaskye.com>
2021-05-15 20:29:46 -0400

    Generate csproj files for the new Money Modules.

--- Prebuild/scripts/Clean.bat
+++
@@ -1,2 +0,0 @@
-cd ..
-Prebuild.exe /clean /yes /removedir obj /file prebuild.xml /build NET_2_0 /pause
--- Prebuild/scripts/autotools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-RUNTIME=`which mono`
-
-SCRIPTDIR=`dirname $0`
-${RUNTIME} ${SCRIPTDIR}/../Prebuild.exe /target autotools /file ${SCRIPTDIR}/../prebuild.xml /build NET_2_0
