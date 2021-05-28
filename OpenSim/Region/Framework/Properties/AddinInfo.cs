55241a69624780deb056dbbd810653e9c59e1142
mdicvkson <mike.dickson@utopiaskye.com>
2021-05-28 17:08:49 -0400

    Removed the AssembleInfo.cs files (again), They are generated from the soltution metadata.  Migrated the Addin info to an AddinInfo.cs file so its still includes in the assembly.

---
+++ OpenSim/Region/Framework/Properties/AddinInfo.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: AddinRoot("OpenSim.Region.Framework", OpenSim.VersionInfo.VersionNumber)]
---
+++ OpenSim/Region/PhysicsModules/BasicPhysics/AddinInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin("OpenSim.Region.PhysicsModule.BasicPhysics", OpenSim.VersionInfo.VersionNumber)]
+[assembly: AddinDependency("OpenSim.Region.Framework", OpenSim.VersionInfo.VersionNumber)]
+
