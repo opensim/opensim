4afc1e3909242c107f0076ea389c9a0cbabfefe1
UbitUmarov <ajlduarte@sapo.pt>
2021-08-13 21:36:55 +0100

    catch possible null refs

--- OpenSim/Framework/Monitoring/Stats/PercentageStat.cs
+++ OpenSim/Framework/Monitoring/Stats/PercentageStat.cs
@@ -45,16 +45,23 @@ namespace OpenSim.Framework.Monitoring
                 // Asking for an update here means that the updater cannot access this value without infinite recursion.
                 // XXX: A slightly messy but simple solution may be to flick a flag so we can tell if this is being
                 // called by the pull action and just return the value.
-                if (StatType == StatType.Pull)
-                    PullAction(this);
+                try
+                {
+                    if (StatType == StatType.Pull)
+                        PullAction(this);
 
-                long c = Consequent;
+                    long c = Consequent;
 
-                // Avoid any chance of a multi-threaded divide-by-zero
-                if (c == 0)
+                    // Avoid any chance of a multi-threaded divide-by-zero
+                    if (c == 0)
+                        return 0;
+
+                    return (double)Antecedent / c * 100;
+                }
+                catch
+                {
                     return 0;
-
-                return (double)Antecedent / c * 100;
+                }
             }
 
             set
