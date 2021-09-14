ef8f12396e590f3b68fffc3ef14a31f2eea25f3e
mdicvkson <mike.dickson@utopiaskye.com>
2021-09-14 12:55:36 -0400

    Bump version info to show how current we are relative to upstream core

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -29,7 +29,7 @@ namespace OpenSim
 {
     public class VersionInfo
     {
-        public const string VersionNumber = "0.9.2.2021.06";
+        public const string VersionNumber = "0.9.2.2021.08";
         public const string AssemblyVersionNumber = "0.9.2.*";
 
         public const Flavour VERSION_FLAVOUR = Flavour.Dev;
