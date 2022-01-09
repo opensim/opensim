70a788170de94561aacef5a45b813122942b146e
UbitUmarov <ajlduarte@sapo.pt>
2022-01-09 03:36:09 +0000

    undo a small change that was not suposed to happen now

--- OpenSim/Addons/Groups/GroupsModule.cs
+++ OpenSim/Addons/Groups/GroupsModule.cs
@@ -249,8 +249,8 @@ namespace OpenSim.Groups
             // There might be some problem with the thread we're generating this on but not
             //   doing the update at this time causes problems (Mantis #7920 and #7915)
             // TODO: move sending this update to a later time in the rootification of the client.
-            //if(!sp.m_haveGroupInformation)
-            //    SendAgentGroupDataUpdate(sp.ControllingClient, false);
+            if(!sp.m_haveGroupInformation)
+                SendAgentGroupDataUpdate(sp.ControllingClient, false);
         }
 
         private void OnMakeChild(ScenePresence sp)
