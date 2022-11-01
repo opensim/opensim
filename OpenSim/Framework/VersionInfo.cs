631d0aee00d77683ebe792b00698ae120841cd78
Mike Dickson <mike.dickson@utopiaskye.com>
2022-11-01 16:11:24 -0400

    Remove Ionic.Zip.dll, libzlib.net and log4net.dll from bin and instead pull from Nuget a replacement version that is also compatible with net6.  The Log4Net change is especially important because we want to use the logging interfaces in dotnet but wont be able to switch everything over day one. So we'll use the log4net logging provider and we can tackle it in chunks.

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -37,7 +37,7 @@ namespace OpenSim
 {
     public class VersionInfo
     {
-        public const string BuildNumber = "8309";
+        public const string BuildNumber = "8340";
         public const string VersionNumber = "0.9.2."+BuildNumber;
         public const string AssemblyVersionNumber = "0.9.2."+BuildNumber;
 
