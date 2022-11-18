6b4dfcde841277248861b1df9de9c3783b73297f
UbitUmarov <ajlduarte@sapo.pt>
2022-11-18 03:36:35 +0000

    mantis 9039: do not lose runtime permitions on llAttachToAvatarTemp

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -3853,6 +3853,9 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 if ((effectivePerms & (uint)PermissionMask.Transfer) == 0)
                     return;
 
+                UUID permsgranter = m_item.PermsGranter;
+                int permsmask = m_item.PermsMask;
+
                 grp.SetOwner(target.UUID, target.ControllingClient.ActiveGroupId);
 
                 if (World.Permissions.PropagatePermissions())
@@ -3865,6 +3868,9 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                     }
                     grp.InvalidateEffectivePerms();
                 }
+
+                m_item.PermsMask = permsmask;
+                m_item.PermsGranter = permsgranter;
 
                 grp.RootPart.ObjectSaleType = 0;
                 grp.RootPart.SalePrice = 10;
