60d4724284b496c029540374b32b28c1af0de3be
lickx <sinesurfer@gmail.com>
2022-12-23 12:34:58 +0100

    Mantis 9042 allow llStopAnimation with overridden animations
    Signed-off-by: UbitUmarov <ajlduarte@sapo.pt>

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -4167,11 +4167,15 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                 if (presence != null)
                 {
                     UUID animID = ScriptUtils.GetAssetIdFromKeyOrItemName(m_host, anim);
-
-                    if (animID.IsZero())
-                        presence.Animator.RemoveAnimation(anim);
-                    else
+                    if (animID.IsNotZero())
                         presence.Animator.RemoveAnimation(animID, true);
+                    else if (MovementAnimationsForLSL.TryGetValue(anim.ToUpper(), out string lslMovementAnimation))
+                    {
+                        if (presence.TryGetAnimationOverride(anim.ToUpper(), out UUID sitanimID))
+                            presence.Animator.RemoveAnimation(sitanimID, true);
+                    }
+                    else
+                        presence.Animator.RemoveAnimation(anim);
                 }
             }
         }
