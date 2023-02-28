49eabf2c0cb63f76cbda58adc2347971c5d76248
UbitUmarov <ajlduarte@sapo.pt>
2023-02-28 10:01:45 +0000

    missing file

--- OpenSim/Region/ScriptEngine/Shared/Api/Runtime/LSL_Stub.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Runtime/LSL_Stub.cs
@@ -662,10 +662,9 @@ namespace OpenSim.Region.ScriptEngine.Shared.ScriptBase
             return m_LSL_Functions.llGetLinkKey(linknum);
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public LSL_Key llObjectGetLinkKey(LSL_Key objectid, int linknum)
+        public LSL_Key llGetObjectLinkKey(LSL_Key objectid, int linknum)
         {
-            return m_LSL_Functions.llObjectGetLinkKey(objectid, linknum);
+            return m_LSL_Functions.llGetObjectLinkKey(objectid, linknum); ;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
