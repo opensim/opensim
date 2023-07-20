11b4195fe1b6939c93addfe5d005b7b5634aaca2
Mike Dickson <mike.dickson@utopiaskye.com>
2023-07-20 13:10:32 -0400

    Rework the Gloebit startup logic so its consistent with the other money modules and allows more than one money module to be present.  This will short-circuit some startup actions if the Gloebit module is not configured which is really more correct anyway.  It shouldn't be trying to start at all in that case.

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -39,7 +39,7 @@ namespace OpenSim
     {
         public const string VersionNumber = "0.9.2.2";
         public const string AssemblyVersionNumber = "0.9.2.2";
-        public const string Release = "8593";
+        public const string Release = "8600";
 
         public const Flavour VERSION_FLAVOUR = Flavour.Dev;
 
