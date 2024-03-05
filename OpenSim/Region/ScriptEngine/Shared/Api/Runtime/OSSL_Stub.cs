f770fc8a0e5bcc2b83559109c7f6957cfefaff45
UbitUmarov <ajlduarte@sapo.pt>
2024-03-05 18:52:48 +0000

    missing file

--- OpenSim/Region/ScriptEngine/Shared/Api/Runtime/OSSL_Stub.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Runtime/OSSL_Stub.cs
@@ -1461,16 +1461,15 @@ namespace OpenSim.Region.ScriptEngine.Shared.ScriptBase
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public LSL_Key osGetLinkInventoryKey(LSL_Integer linkNumber, LSL_String name)
+        public LSL_Key osGetLinkInventoryItemKey(LSL_Integer linkNumber, LSL_String name)
         {
-            return m_OSSL_Functions.osGetLinkInventoryKey(linkNumber, name);
-
+            return m_OSSL_Functions.osGetLinkInventoryItemKey(linkNumber, name);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public LSL_List osGetInventoryKeys(LSL_Integer type)
+        public LSL_List osGetInventoryItemKeys(LSL_Integer type)
         {
-            return m_OSSL_Functions.osGetInventoryKeys(type);
+            return m_OSSL_Functions.osGetInventoryItemKeys(type);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
