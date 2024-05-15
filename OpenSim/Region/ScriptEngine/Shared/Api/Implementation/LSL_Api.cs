a79f5d58f60b982fa1845e29922cf9662da905c2
UbitUmarov <ajlduarte@sapo.pt>
2024-05-15 19:05:32 +0100

    add missing dependency for pgsql connector

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -12059,7 +12059,6 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
         //  these functions, so the string to integer
         //  conversion returns '0' if an invalid
         //  character is encountered during conversion.
-        //  Only full bytes are considered, partial are counted as 0
         //  </para>
         //  <para>
         //  References
@@ -12193,7 +12192,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
         //  be returned.
         //  If fewer than 6 characters are supplied, then
         //  the answer will reflect a partial
-        //  accumulation.
+        //  accumulation of full bytes
         //  <para>
         //  The 6-bit segments are
         //  extracted left-to-right in big-endian mode,
