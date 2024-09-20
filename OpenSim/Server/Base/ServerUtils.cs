3823940205c9a1205e184e0686bc9c60d5c46f39
UbitUmarov <ajlduarte@sapo.pt>
2024-09-21 00:41:25 +0100

    add GetLocalRegionByName to GridServicesConnector plus cosmetics

--- OpenSim/Server/Base/ServerUtils.cs
+++ OpenSim/Server/Base/ServerUtils.cs
@@ -543,19 +543,21 @@ namespace OpenSim.Server.Base
 
         public static Dictionary<string, object> ParseXmlResponse(string data)
         {
-            try
+            if(!string.IsNullOrEmpty(data))
             {
-                using XmlReader xr = XmlReader.Create(new StringReader(data), 
-                    ParseXmlStringResponseXmlReaderSettings, ParseXmlResponseXmlParserContext);
-                if (!xr.ReadToFollowing("ServerResponse"))
-                    return new Dictionary<string, object>();
-                return ScanXmlResponse(xr);
+                try
+                {
+                    using XmlReader xr = XmlReader.Create(new StringReader(data), 
+                        ParseXmlStringResponseXmlReaderSettings, ParseXmlResponseXmlParserContext);
+                    if (xr.ReadToFollowing("ServerResponse"))
+                        return ScanXmlResponse(xr);
+                }
+                catch (Exception e)
+                {
+                    m_log.Debug($"[serverUtils.ParseXmlResponse]: failed error: {e.Message}\n --string:\n{data}\n");
+                }
             }
-            catch (Exception e)
-            {
-                m_log.DebugFormat("[serverUtils.ParseXmlResponse]: failed error: {0}\n --string:\n{1}\n", e.Message, data);
-            }
-            return new Dictionary<string, object>();
+            return [];
         }
 
         private static readonly XmlReaderSettings ParseXmlStreamResponseXmlReaderSettings = new()
