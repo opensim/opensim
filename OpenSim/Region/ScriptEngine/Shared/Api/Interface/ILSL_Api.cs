f79e04d3077ae51cabaaee42b42f28e5f829800d
UbitUmarov <ajlduarte@sapo.pt>
2025-08-14 22:24:31 +0100

    add stub for llMapBeacon

--- OpenSim/Region/ScriptEngine/Shared/Api/Interface/ILSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Interface/ILSL_Api.cs
@@ -284,6 +284,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api.Interfaces
               void llMakeFire(int particles, double scale, double vel, double lifetime, double arc, string texture, LSL_Vector offset);
               void llMakeFountain(int particles, double scale, double vel, double lifetime, double arc, int bounce, string texture, LSL_Vector offset, double bounce_offset);
               void llMakeSmoke(int particles, double scale, double vel, double lifetime, double arc, string texture, LSL_Vector offset);
+              void llMapBeacon(string simname, LSL_Vector pos, LSL_List loptions);
               void llMapDestination(string simname, LSL_Vector pos, LSL_Vector look_at);
         LSL_String llMD5String(string src, int nonce);
         LSL_String llSHA1String(string src);
